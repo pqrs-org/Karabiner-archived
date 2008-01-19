34847074548d8917bb152b7559ab79ed1c028583
Takayama Fumihiko <tekezo@pqrs.org>
2008-01-19 14:25:00 +0900

    add make-source

---
+++ make-source.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+name="$(basename $(pwd))-$(cat version)"
+tmpdir=$(mktemp -d "/tmp/$name.XXXXXX")
+workdir="$tmpdir/$name"
+
+# copy
+rsync -a . $workdir
+
+# cleanup
+(cd $workdir; make clean)
+rm -rf $workdir/.hg*
+find $workdir -name '.DS_Store' -print0 | xargs -0 rm -f
+
+# archive
+tar czf $name.tar.gz -C $tmpdir $name
+
+# purge tmpdir
+rm -rf $tmpdir
