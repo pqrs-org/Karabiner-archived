f9cc1e9e0ed63ec1fbef3c1870a3b7085d9ee687
Takayama Fumihiko <tekezo@pqrs.org>
2008-02-02 17:34:49 +0900

    changes for Tiger

--- prefpane/AdminAction.h
+++ prefpane/AdminAction.h
@@ -1,6 +1,8 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
 #import <Cocoa/Cocoa.h>
+#import <Security/Authorization.h>
+#import <Security/AuthorizationTags.h>
 
 @interface AdminAction : NSObject
 {
