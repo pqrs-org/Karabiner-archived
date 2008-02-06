a3e8bb5fe626fb53ad77eb46088900d546906bc1
Takayama Fumihiko <tekezo@pqrs.org>
2008-02-07 01:44:29 +0900

    add util

---
+++ util/xml2html.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/ruby
+
+require 'cgi'
+
+file = "%s/../files/prefpane/sysctl.xml" % File.dirname($0)
+open(file) do |f|
+  while l = f.gets
+    if /<list>/ =~ l then
+      print "<ul>"
+    end
+    if /<\/list>/ =~ l then
+      print "</ul>"
+    end
+    if /<item>/ =~ l then
+      print "<li>"
+    end
+    if /<\/item>/ =~ l then
+      print "</li>"
+    end
+    if /<name>(.+)<\/name>/ =~ l then
+      print CGI.escapeHTML($1)
+    end
+  end
+  print "\n"
+end
+
+
