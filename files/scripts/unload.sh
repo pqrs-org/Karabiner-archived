ed8714bb8154b176f334354ad5a7841884bb1e70
tekezo <devnull@localhost>
2008-03-14 10:35:13 +0900

    update unload.sh

--- files/scripts/unload.sh
+++ files/scripts/unload.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 
-kext="/Library/org.pqrs/KeyRemap4MacBook/KeyRemap4MacBook.kext"
-[ -d $kext ] && kextunload "$kext"
-
-# old version
-kext="/Applications/KeyRemap4MacBook/KeyRemap4MacBook.kext"
-[ -d $kext ] && kextunload "$kext"
-
+/sbin/kextunload -b org.pqrs.driver.KeyRemap4MacBook
 exit 0
