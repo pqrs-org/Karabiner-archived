86badfbc4a652f84b24e8d7eb2abda9bc26c92fc
Takayama Fumihiko <tekezo@pqrs.org>
2008-04-20 23:19:25 +0900

    handle package version @ src/kext, prefpane

--- pkginfo/setversion.sh
+++ pkginfo/setversion.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 basedir=`dirname $0`
-version=$(cat $basedir/../version)
-sed "s|PKGVERSION|$version|g" Info.plist.tmpl > Info.plist
+version=$(cat "$basedir/../version")
+
+for f in `find $basedir/.. -name 'Info.plist.tmpl'`; do
+    if [ -f "$f" ]; then
+        sed "s|PKGVERSION|$version|g" $f > `dirname $f`/`basename $f .tmpl`
+    fi
+done
