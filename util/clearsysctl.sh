ce7f7069b61b061c81a3392acd8dc832f257b01f
Takayama Fumihiko <tekezo@pqrs.org>
2008-04-25 21:45:49 +0900

    purge util/clearsysctl.sh

--- util/clearsysctl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
-
-for key in `sysctl keyremap4macbook 2>&1 | grep -oE '^keyremap4macbook.(remap|option).[^:]+'`; do
-  sudo sysctl -w $key=0
-done
