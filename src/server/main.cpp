8381511e43c6b26a6ed45e84b3e308c00a3adfb3
tekezo <devnull@localhost>
2008-04-26 15:19:45 +0900

    add verifyUser @ KeyRemap4MacBook_server

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,10 +1,33 @@
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <stdio.h>
 #include "server.hpp"
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <SystemConfiguration/SystemConfiguration.h>
+
+namespace {
+  bool
+  verifyUser(void)
+  {
+    uid_t consoleUID;
+    CFStringRef result = SCDynamicStoreCopyConsoleUser(NULL, &consoleUID, NULL);
+    if (! result) return false;
+    CFRelease(result);
+
+    return (getuid() == consoleUID);
+  }
+}
+
 
 int
 main()
 {
+  if (! verifyUser()) {
+    fprintf(stderr, "Permission denied\n");
+    return 1;
+  }
+
   umask(0077);
 
   KeyRemap4MacBook_server::Server server;
