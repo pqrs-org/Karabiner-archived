1c6850109ccf783c1ab0f6f81b4ea5da6608e703
tekezo <devnull@localhost>
2008-05-05 13:20:36 +0900

    add wait to kext/scripts/load.sh

--- src/core/kext/scripts/load.sh
+++ src/core/kext/scripts/load.sh
@@ -2,3 +2,11 @@
 
 sudo cp -R build/Release/KeyRemap4MacBook.kext /tmp/
 sudo kextload -t /tmp/KeyRemap4MacBook.kext
+
+#--------------------
+while true; do
+    echo "Waiting reconfigure..."
+    [ "x`sysctl -n keyremap4macbook.initialized`" = "x1" ] && exit 0
+    sleep 1
+done
+
