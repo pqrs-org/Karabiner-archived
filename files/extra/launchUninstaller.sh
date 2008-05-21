0c3cee706570c523ff19081d7015cdf37790fa7a
tekezo <devnull@localhost>
2008-05-21 10:17:04 +0900

    update around uninstaller

--- files/extra/launchUninstaller.sh
+++ files/extra/launchUninstaller.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-TMPDIR=`/usr/bin/mktemp -d /tmp/uninstaller.XXXXXX` || exit 1
+TMPDIR=`/usr/bin/mktemp -d /tmp/KeyRemap4MacBook_uninstaller.XXXXXX` || exit 1
 /usr/bin/rsync -a /Library/org.pqrs/KeyRemap4MacBook/app/uninstaller.app "$TMPDIR"
 /usr/bin/open "$TMPDIR/uninstaller.app"
