01eeccfa79cd980182382cc209caeb85b5a5a88d
Takayama Fumihiko <tekezo@pqrs.org>
2008-09-14 22:47:54 +0900

    add ListHookedKeyboard

---
+++ src/core/kext/ListHookedKeyboard.cpp
@@ -0,0 +1,146 @@
+#include "ListHookedKeyboard.hpp"
+#include "Core.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  namespace ListHookedKeyboard {
+    enum {
+      MAXNUM = 16,
+    };
+    Item item[MAXNUM];
+
+    bool
+    append(IOHIKeyboard *kbd)
+    {
+      for (int i = 0; i < MAXNUM; ++i) {
+        if (item[i].get()) continue;
+
+        item[i].initialize(kbd);
+      }
+      return false;
+    }
+
+    Item *
+    get(const IOHIKeyboard *kbd)
+    {
+      if (kbd == NULL) return NULL;
+
+      for (int i = 0; i < MAXNUM; ++i) {
+        if (item[i].get() == kbd) return item + i;
+      }
+      return NULL;
+    }
+
+    Item *
+    get(void)
+    {
+      for (int i = 0; i < MAXNUM; ++i) {
+        if (item[i].get()) return item + i;
+      }
+      return NULL;
+    }
+
+    void
+    refresh(void)
+    {
+      for (int i = 0; i < MAXNUM; ++i) {
+        item[i].refresh();
+      }
+    }
+
+    // ======================================================================
+    void
+    Item::initialize(IOHIKeyboard *_kbd)
+    {
+      kbd = _kbd;
+      refresh();
+    }
+
+    void
+    Item::refresh(void)
+    {
+      if (kbd == NULL) return;
+
+      {
+        KeyboardEventCallback old = getCurrent_keyboardEventAction();
+
+        if (old != hook_KeyboardEventCallback) {
+          IOLog("KeyRemap4MacBook [refresh] KeyboardEventAction\n");
+
+          orig_keyboardEventAction = old;
+          kbd->_keyboardEventAction = reinterpret_cast<KeyboardEventAction>(hook_KeyboardEventCallback);
+
+          orig_keyboardEventTarget = kbd->_keyboardEventTarget;
+        }
+      }
+      {
+        KeyboardSpecialEventCallback old = getCurrent_keyboardSpecialEventAction();
+
+        if (old != hook_KeyboardSpecialEventCallback) {
+          IOLog("KeyRemap4MacBook [refresh] KeyboardSpecialEventAction\n");
+
+          orig_keyboardSpecialEventAction = old;
+          kbd->_keyboardSpecialEventAction = reinterpret_cast<KeyboardSpecialEventAction>(hook_KeyboardSpecialEventCallback);
+
+          orig_keyboardSpecialEventTarget = kbd->_keyboardSpecialEventTarget;
+        }
+      }
+    }
+
+    void
+    Item::terminate(void)
+    {
+      if (! kbd) return;
+
+      if (getCurrent_keyboardEventAction() == hook_KeyboardEventCallback) {
+        kbd->_keyboardEventAction = reinterpret_cast<KeyboardEventAction>(orig_keyboardEventAction);
+      }
+      if (getCurrent_keyboardSpecialEventAction() == hook_KeyboardSpecialEventCallback) {
+        kbd->_keyboardSpecialEventAction = reinterpret_cast<KeyboardSpecialEventAction>(orig_keyboardSpecialEventAction);
+      }
+      kbd = NULL;
+    }
+
+    // ----------------------------------------------------------------------
+    void
+    hook_KeyboardEventCallback(OSObject *target,
+                               unsigned int eventType,
+                               unsigned int flags,
+                               unsigned int key,
+                               unsigned int charCode,
+                               unsigned int charSet,
+                               unsigned int origCharCode,
+                               unsigned int origCharSet,
+                               unsigned int keyboardType,
+                               bool repeat,
+                               AbsoluteTime ts,
+                               OSObject *sender,
+                               void *refcon)
+    {
+      Params_KeyboardEventCallBack params = {
+        target, eventType, flags, key,
+        charCode, charSet, origCharCode, origCharSet,
+        keyboardType, repeat, ts, sender, refcon,
+      };
+      Core::remap_KeyboardEventCallback(&params);
+    }
+
+    void
+    hook_KeyboardSpecialEventCallback(OSObject *target,
+                                      unsigned int eventType,
+                                      unsigned int flags,
+                                      unsigned int key,
+                                      unsigned int flavor,
+                                      UInt64 guid,
+                                      bool repeat,
+                                      AbsoluteTime ts,
+                                      OSObject *sender,
+                                      void *refcon)
+    {
+      Params_KeyboardSpecialEventCallback params = {
+        target, eventType, flags, key, flavor,
+        guid, repeat, ts, sender, refcon,
+      };
+      Core::remap_KeyboardSpecialEventCallback(&params);
+    }
+  }
+}
---
+++ src/core/kext/ListHookedKeyboard.hpp
@@ -0,0 +1,69 @@
+#ifndef LISTHOOKEDKEYBOARD_HPP
+#define LISTHOOKEDKEYBOARD_HPP
+
+#include "base.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  namespace ListHookedKeyboard {
+    class Item;
+    bool append(IOHIKeyboard *kbd);
+    Item *get(const IOHIKeyboard *kbd);
+    Item *get(void);
+    void refresh(void);
+
+    // ----------------------------------------------------------------------
+    class Item {
+    public:
+      Item(void) { kbd = NULL; }
+
+      void initialize(IOHIKeyboard *_kbd);
+      void refresh(void);
+      void terminate(void);
+
+      IOHIKeyboard *get(void) const { return kbd; }
+
+    private:
+      IOHIKeyboard *kbd;
+      KeyboardEventCallback orig_keyboardEventAction;
+      KeyboardSpecialEventCallback orig_keyboardSpecialEventAction;
+      OSObject *orig_keyboardEventTarget;
+      OSObject *orig_keyboardSpecialEventTarget;
+
+      KeyboardEventCallback getCurrent_keyboardEventAction(void) const {
+        if (kbd == NULL) return NULL;
+        return reinterpret_cast<KeyboardEventCallback>(kbd->_keyboardEventAction);
+      }
+      KeyboardSpecialEventCallback getCurrent_keyboardSpecialEventAction(void) const {
+        if (kbd == NULL) return NULL;
+        return reinterpret_cast<KeyboardSpecialEventCallback>(kbd->_keyboardSpecialEventAction);
+      }
+    };
+
+    // ----------------------------------------------------------------------
+    void hook_KeyboardEventCallback(OSObject *target,
+                                    unsigned int eventType,
+                                    unsigned int flags,
+                                    unsigned int key,
+                                    unsigned int charCode,
+                                    unsigned int charSet,
+                                    unsigned int origCharCode,
+                                    unsigned int origCharSet,
+                                    unsigned int keyboardType,
+                                    bool repeat,
+                                    AbsoluteTime ts,
+                                    OSObject *sender,
+                                    void *refcon);
+    void hook_KeyboardSpecialEventCallback(OSObject *target,
+                                           unsigned int eventType,
+                                           unsigned int flags,
+                                           unsigned int key,
+                                           unsigned int flavor,
+                                           UInt64 guid,
+                                           bool repeat,
+                                           AbsoluteTime ts,
+                                           OSObject *sender,
+                                           void *refcon);
+  }
+}
+
+#endif
