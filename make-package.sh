b53fc62d1352081add8661bfb058c57187448f9d
tekezo <devnull@localhost>
2009-09-01 23:01:05 +0900

    add LaunchAgents

--- make-package.sh
+++ make-package.sh
@@ -27,13 +27,13 @@ sudo cp -R files/extra/launchUninstaller.sh "pkgroot/$basedir/extra/"
 
 sudo mkdir -p "pkgroot/Library"
 sudo cp -R files/LaunchDaemons pkgroot/Library
+sudo cp -R files/LaunchAgents pkgroot/Library
 
 sudo mkdir -p "pkgroot/$basedir/server"
 sudo cp src/core/server/build/Release/KeyRemap4MacBook_server "pkgroot/$basedir/server"
 
 sudo mkdir -p "pkgroot/$basedir/app"
 sudo cp -R "src/util/statusbar/build/Release/KeyRemap4MacBook_statusbar.app" "pkgroot/$basedir/app"
-sudo cp -R "src/util/launchd/build/Release/KeyRemap4MacBook_launchd.app" "pkgroot/$basedir/app"
 sudo cp -R "src/util/uninstaller/build/Release/uninstaller.app" "pkgroot/$basedir/app"
 
 sudo mkdir -p "pkgroot/$basedir/bin"
@@ -41,7 +41,6 @@ sudo cp src/bin/sysctl_confd/build/Release/KeyRemap4MacBook_sysctl_confd "pkgroo
 sudo cp src/bin/sysctl_ctl/build/Release/KeyRemap4MacBook_sysctl_ctl "pkgroot/$basedir/bin"
 sudo cp src/bin/sysctl_reset/build/Release/KeyRemap4MacBook_sysctl_reset "pkgroot/$basedir/bin"
 sudo cp src/bin/sysctl_set/build/Release/KeyRemap4MacBook_sysctl_set "pkgroot/$basedir/bin"
-sudo cp src/bin/set_loginwindow/build/Release/set_loginwindow "pkgroot/$basedir/bin"
 sudo cp src/bin/uninstall/build/Release/uninstall "pkgroot/$basedir/bin"
 
 sudo mkdir -p "pkgroot/Library/PreferencePanes"
@@ -56,9 +55,7 @@ sudo chmod 4755 pkgroot/$basedir/bin/uninstall
 sudo chmod 4755 pkgroot/$basedir/server/KeyRemap4MacBook_server
 sudo chmod 755 pkgroot/$basedir/bin/KeyRemap4MacBook_sysctl_confd
 sudo chmod 755 pkgroot/$basedir/bin/KeyRemap4MacBook_sysctl_ctl
-sudo chmod 755 pkgroot/$basedir/bin/set_loginwindow
 sudo chmod 755 pkgroot/$basedir/app/KeyRemap4MacBook_statusbar.app/Contents/MacOS/KeyRemap4MacBook_statusbar
-sudo chmod 755 pkgroot/$basedir/app/KeyRemap4MacBook_launchd.app/Contents/MacOS/KeyRemap4MacBook_launchd
 sudo chmod 755 pkgroot/$basedir/app/uninstaller.app/Contents/MacOS/uninstaller
 sudo chown -R root:wheel pkgroot
 
