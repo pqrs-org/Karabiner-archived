8d0b15ee6a0ca8c8c756ec08e3c6228834090bfd
Takayama Fumihiko <tekezo@pqrs.org>
2009-09-02 19:27:13 +0900

    update src/core/server

--- src/core/server/server.cpp
+++ src/core/server/server.cpp
@@ -62,10 +62,10 @@ KeyRemap4MacBook_server::Server::dispatchOperator(int sock)
   if (read(sock, &operation, sizeof(operation)) < 0) goto error;
 
   switch (operation) {
-    case KeyRemap4MacBook_bridge::REQUEST_ACTIVE_APPLICATION_INFO:
+    case org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::REQUEST_ACTIVE_APPLICATION_INFO:
     {
-      KeyRemap4MacBook_bridge::ActiveApplicationInfo::Reply reply;
-      KeyRemap4MacBook_bridge::Error error = do_ActiveApplicationInfo(&reply);
+      org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::Reply reply;
+      org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::Error error = do_ActiveApplicationInfo(&reply);
       sendReply(sock, &reply, sizeof(reply), error);
       break;
     }
@@ -76,7 +76,7 @@ KeyRemap4MacBook_server::Server::dispatchOperator(int sock)
   return;
 
 error:
-  sendReply(sock, NULL, 0, KeyRemap4MacBook_bridge::ERROR);
+  sendReply(sock, NULL, 0, org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ERROR);
 }
 
 void *
@@ -172,7 +172,7 @@ KeyRemap4MacBook_server::Server::makeSocket(void)
   memset(&listenSocketAddr, 0, sizeof(listenSocketAddr));
   listenSocketAddr.sun_len = sizeof(listenSocketAddr);
   listenSocketAddr.sun_family = AF_UNIX;
-  snprintf(listenSocketAddr.sun_path, sizeof(listenSocketAddr.sun_path), KeyRemap4MacBook_bridge::socketPath);
+  snprintf(listenSocketAddr.sun_path, sizeof(listenSocketAddr.sun_path), org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::socketPath);
 
   if (bind(listenSocket, reinterpret_cast<struct sockaddr *>(&listenSocketAddr), sizeof(listenSocketAddr)) < 0) return false;
 
@@ -182,75 +182,75 @@ KeyRemap4MacBook_server::Server::makeSocket(void)
 void
 KeyRemap4MacBook_server::Server::deleteSocket(void)
 {
-  unlink(KeyRemap4MacBook_bridge::socketPath);
+  unlink(org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::socketPath);
 }
 
 
 // --------------------------------------------------
-KeyRemap4MacBook_bridge::Error
-KeyRemap4MacBook_server::Server::do_ActiveApplicationInfo(KeyRemap4MacBook_bridge::ActiveApplicationInfo::Reply *reply)
+org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::Error
+KeyRemap4MacBook_server::Server::do_ActiveApplicationInfo(org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::Reply *reply)
 {
-  if (! reply) return KeyRemap4MacBook_bridge::ERROR;
+  if (! reply) return org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ERROR;
 
   char applicationName[128];
   getActiveApplicationName(applicationName, sizeof(applicationName));
 
-  reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::UNKNOWN;
+  reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::UNKNOWN;
 
   const char *org_vim = "org.vim.";
   const char *com_adobe = "com.adobe.";
 
   if (strcmp(applicationName, "org.gnu.Emacs") == 0 ||
       strcmp(applicationName, "org.gnu.AquamacsEmacs") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::EMACS;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::EMACS;
 
   } else if (strncmp(applicationName, org_vim, strlen(org_vim)) == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::VI;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::VI;
 
   } else if (strcmp(applicationName, "com.apple.Terminal") == 0 ||
              strcmp(applicationName, "iTerm") == 0 ||
              strcmp(applicationName, "net.sourceforge.iTerm") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::TERMINAL;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::TERMINAL;
 
   } else if (strcmp(applicationName, "com.vmware.fusion") == 0 ||
              strcmp(applicationName, "com.parallels.desktop") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::VIRTUALMACHINE;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::VIRTUALMACHINE;
 
   } else if (strcmp(applicationName, "com.microsoft.rdc") == 0 ||
              strcmp(applicationName, "net.sf.cord") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::REMOTEDESKTOPCONNECTION;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::REMOTEDESKTOPCONNECTION;
 
   } else if (strcmp(applicationName, "org.x.X11") == 0 ||
              strcmp(applicationName, "com.apple.x11") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::X11;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::X11;
 
   } else if (strcmp(applicationName, "com.apple.finder") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::FINDER;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::FINDER;
 
   } else if (strcmp(applicationName, "com.apple.Safari") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::SAFARI;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::SAFARI;
 
   } else if (strcmp(applicationName, "org.mozilla.firefox") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::FIREFOX;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::FIREFOX;
 
   } else if (strcmp(applicationName, "com.apple.iChat") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::ICHAT;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::ICHAT;
 
   } else if (strcmp(applicationName, "com.adiumX.adiumX") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::ADIUMX;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::ADIUMX;
 
   } else if (strcmp(applicationName, "com.skype.skype") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::SKYPE;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::SKYPE;
 
   } else if (strcmp(applicationName, "com.apple.mail") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::MAIL;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::MAIL;
 
   } else if (strcmp(applicationName, "com.apple.TextEdit") == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::EDITOR;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::EDITOR;
 
   } else if (strncmp(applicationName, com_adobe, strlen(com_adobe)) == 0) {
-    reply->type = KeyRemap4MacBook_bridge::ActiveApplicationInfo::ADOBE;
+    reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::ADOBE;
   }
 
-  return KeyRemap4MacBook_bridge::SUCCESS;
+  return org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::SUCCESS;
 }
