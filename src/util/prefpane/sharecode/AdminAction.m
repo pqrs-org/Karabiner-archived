8b5fb6854da6f03079cca86426627433071ecb31
Takayama Fumihiko <tekezo@pqrs.org>
2009-09-07 00:56:07 +0900

    update sharecode

--- src/util/prefpane/sharecode/AdminAction.m
+++ src/util/prefpane/sharecode/AdminAction.m
@@ -4,7 +4,7 @@
 
 static AuthorizationRef authorizationRef = nil;
 
-@implementation AdminAction
+@implementation BUNDLEPREFIX(AdminAction)
 
 + (BOOL) setup
 {
@@ -18,7 +18,7 @@ static AuthorizationRef authorizationRef = nil;
 
 + (BOOL) getPrivilege
 {
-  if (! [AdminAction setup]) return FALSE;
+  if (! [BUNDLEPREFIX(AdminAction) setup]) return FALSE;
 
   AuthorizationItem item;
 
@@ -41,7 +41,7 @@ static AuthorizationRef authorizationRef = nil;
 
 + (BOOL) execCommand:(char *)command
 {
-  if (! [AdminAction getPrivilege]) return FALSE;
+  if (! [BUNDLEPREFIX(AdminAction) getPrivilege]) return FALSE;
 
   char *const args[] = {
     "-c",
