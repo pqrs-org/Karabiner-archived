80016ecdadd32d3b822a25b22d69651b3e7cee44
tekezo <devnull@localhost>
2009-09-07 00:10:09 +0900

    update uninstaller

--- src/util/uninstaller/sharecode/sharecode.h
+++
@@ -1,8 +0,0 @@
-// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
-
-#import "bundleprefix.h"
-
-#import "Common.h"
-#import "OutlineViewUtil.h"
-#import "SysctlWrapper.h"
-#import "XMLTreeWrapper.h"
