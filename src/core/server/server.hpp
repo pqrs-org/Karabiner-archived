29d0e32f095f7b8bdf8c02cd75bc4bee572d97ed
Takayama Fumihiko <tekezo@pqrs.org>
2009-09-08 00:22:57 +0900

    update src/core/server

--- src/core/server/server.hpp
+++ src/core/server/server.hpp
@@ -2,19 +2,12 @@
 #define KEYREMAP4MACBOOK_SERVER_HPP
 
 #include <queue>
-#include "thread.hpp"
 #include "bridge.hpp"
 
 namespace KeyRemap4MacBook_server {
   class Server {
   public:
-    enum {
-      NUM_THREAD = 4,
-    };
-    Server(void) {
-      listenSocket = -1;
-    }
-
+    Server(void) : listenSocket(-1) {}
     void doLoop(void);
 
   private:
@@ -26,17 +19,10 @@ namespace KeyRemap4MacBook_server {
 
     void sendReply(int sock, void *data, size_t size, int error);
     void dispatchOperator(int sock);
-    static void *handleRequestThread(void *arg);
-    bool enqueueRequest(int sock);
 
     org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::Error do_ActiveApplicationInfo(org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::Reply *reply);
 
-    std::queue<int> queueRequestSocket;
-    KeyRemap4MacBook_server::Thread::Mutex requestsMutex;
-    KeyRemap4MacBook_server::Thread::Cond requestCond;
-
     int listenSocket;
-    volatile bool exitLoopFlag;
   };
 }
 
