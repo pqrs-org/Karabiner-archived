9e1a18081f8e19ecfab46135adc0113bb415cb10
tekezo <devnull@localhost>
2009-10-03 00:54:28 +0900

    add remap.commandR2home_vm, remap.optionR2end_vm

--- src/core/server/server.cpp
+++ src/core/server/server.cpp
@@ -161,7 +161,8 @@ KeyRemap4MacBook_server::Server::do_ActiveApplicationInfo(org_pqrs_KeyRemap4MacB
     reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::TERMINAL;
 
   } else if (strcmp(applicationName, "com.vmware.fusion") == 0 ||
-             strcmp(applicationName, "com.parallels.desktop") == 0) {
+             strcmp(applicationName, "com.parallels.desktop") == 0 ||
+             strcmp(applicationName, "org.virtualbox.app.VirtualBox") == 0) {
     reply->type = org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::ActiveApplicationInfo::VIRTUALMACHINE;
 
   } else if (strcmp(applicationName, "com.microsoft.rdc") == 0 ||
