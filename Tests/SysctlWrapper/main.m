7c5ad593bb580db664600e3940f750826c503493
tekezo <devnull@localhost>
2009-10-08 09:25:41 +0900

    fix Tests/SysctlWrapper

--- Tests/SysctlWrapper/main.m
+++ Tests/SysctlWrapper/main.m
@@ -5,7 +5,7 @@
 int
 main()
 {
-  NSString *version = [BUNDLEPREFIX_SysctlWrapper getString:@"keyremap4macbook.version"];
+  NSString *version = [BUNDLEPREFIX(SysctlWrapper) getString:@"keyremap4macbook.version"];
   NSLog(@"version = %s\n", [version UTF8String]);
   return 0;
 }
