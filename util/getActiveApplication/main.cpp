c07bf97121ac4206e9263f8065749ddd3a8a00e8
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-03 18:29:09 +0900

    update util/getActiveApplication

--- util/getActiveApplication/main.cpp
+++ util/getActiveApplication/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <stdlib.h>
-#include "getActiveApplicationName.h"
+#include "server_objc_part.h"
 
 int
 main()
 {
   for (;;) {
     char applicationName[128];
+
+    autoreleasepool_begin();
     getActiveApplicationName(applicationName, sizeof(applicationName));
+    autoreleasepool_end();
+
     std::cout << applicationName << std::endl;
-    sleep(1);
+    //sleep(1);
   }
 }
