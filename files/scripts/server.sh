76ecd75f6e3d775f7a548a9f902c42d0a92587f9
tekezo <devnull@localhost>
2009-11-05 22:45:57 +0900

    update files/scripts/server.sh

--- files/scripts/server.sh
+++ files/scripts/server.sh
@@ -12,6 +12,4 @@ tmpdir=`mktemp -d /tmp/KeyRemap4MacBook_server.$uid.XXXXXXXX`
 /Library/org.pqrs/KeyRemap4MacBook/bin/KeyRemap4MacBook_sysctl_set socket_path "$tmpdir/KeyRemap4MacBook_server"
 
 # kick server
-/Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook_server.app/Contents/MacOS/KeyRemap4MacBook_server "$tmpdir"
-
-exit 0
+exec /Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook_server.app/Contents/MacOS/KeyRemap4MacBook_server "$tmpdir"
