38ac56cc50dc7d97d1da2218680f72b7f3c7be47
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-07 02:11:51 +0900

    add util/DumpInputModeToConsole

---
+++ util/DumpInputModeToConsole/DumpInputModeToConsoleAppDelegate.m
@@ -0,0 +1,34 @@
+//
+//  DumpInputModeToConsoleAppDelegate.m
+//  DumpInputModeToConsole
+//
+//  Created by Takayama Fumihiko on 09/11/07.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "DumpInputModeToConsoleAppDelegate.h"
+#import "server_objc_part.h"
+
+@implementation DumpInputModeToConsoleAppDelegate
+
+@synthesize window;
+
+- (void) threadMain {
+  NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+
+  for (;;) {
+    char inputmodeName[128];
+    getTISPropertyInputModeID(inputmodeName, sizeof(inputmodeName));
+    NSLog(@"%s", inputmodeName);
+    sleep(1);
+  }
+
+  [pool drain];
+  [NSThread exit];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  [NSThread detachNewThreadSelector:@selector(threadMain) toTarget:self withObject:nil];
+}
+
+@end
