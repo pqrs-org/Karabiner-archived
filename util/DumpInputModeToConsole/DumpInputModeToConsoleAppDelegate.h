e788a363921098706e7ce2d5c5530f1f1d0ddf6e
tekezo <devnull@localhost>
2009-11-07 02:11:51 +0900

    add util/DumpInputModeToConsole

---
+++ util/DumpInputModeToConsole/DumpInputModeToConsoleAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DumpInputModeToConsoleAppDelegate.h
+//  DumpInputModeToConsole
+//
+//  Created by Takayama Fumihiko on 09/11/07.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface DumpInputModeToConsoleAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ util/DumpInputModeToConsole/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  DumpInputModeToConsole
+//
+//  Created by Takayama Fumihiko on 09/11/07.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
