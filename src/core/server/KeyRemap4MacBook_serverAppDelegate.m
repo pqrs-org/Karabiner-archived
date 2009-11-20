8de720ed384b35c25bb113aae62bc798da1688b4
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-21 01:15:31 +0900

    update src/core/server to use NSWorkspaceDidActivateApplicationNotification

--- src/core/server/KeyRemap4MacBook_serverAppDelegate.m
+++ src/core/server/KeyRemap4MacBook_serverAppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "KeyRemap4MacBook_serverAppDelegate.h"
 #include "util.h"
+#include "server_objc_part.h"
+#include "server.hpp"
 
 @implementation KeyRemap4MacBook_serverAppDelegate
 
@@ -22,7 +24,28 @@
   [NSThread exit];
 }
 
+- (void) observer_NSWorkspaceDidActivateApplicationNotification:(NSNotification*)notification
+{
+  NSRunningApplication* app = [[notification userInfo] objectForKey:@"NSWorkspaceApplicationKey"];
+  if (! app) return;
+
+  NSString* applicationName = [app bundleIdentifier];
+  if (! applicationName) return;
+
+  setCurrentApplicationType([applicationName UTF8String]);
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                      selector:@selector(observer_NSWorkspaceDidActivateApplicationNotification:)
+                                                      name:NSWorkspaceDidActivateApplicationNotification
+                                                      object:nil];
+
+  char buffer[512];
+  getActiveApplicationName(buffer, sizeof(buffer));
+  setCurrentApplicationType(buffer);
+
+  // ------------------------------------------------------------
   [NSThread detachNewThreadSelector:@selector(threadMain) toTarget:self withObject:nil];
 }
 
