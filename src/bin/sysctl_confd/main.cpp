936cf52bb5c77674c7294505ad6e0c37a20b3dd4
tekezo <devnull@localhost>
2009-11-22 23:57:05 +0900

    add doFirstReset @ src/bin/sysctl_confd

--- src/bin/sysctl_confd/main.cpp
+++ src/bin/sysctl_confd/main.cpp
@@ -3,17 +3,34 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <iostream>
 
 namespace {
   void sigfunc(int param) {
     system("/Library/org.pqrs/KeyRemap4MacBook/bin/KeyRemap4MacBook_sysctl_reset terminate");
     exit(0);
   }
+
+  bool doFirstReset(void) {
+    // We wait till a user session is ready.
+    const int MAX_TRY_COUNT = 5;
+
+    for (int trycount = 0; trycount < MAX_TRY_COUNT; ++trycount) {
+      int exitstatus = system("/Library/org.pqrs/KeyRemap4MacBook/bin/KeyRemap4MacBook_sysctl_reset");
+      if (exitstatus == 0) return true;
+    }
+    return false;
+  }
 }
 
 int
 main()
 {
+  if (! doFirstReset()) {
+    std::cerr << "[ERROR] KeyRemap4MacBook_sysctl_confd: can't reset configuration." << std::endl;
+    exit(1);
+  }
+
   signal(SIGHUP, sigfunc);
   signal(SIGTERM, sigfunc);
 
