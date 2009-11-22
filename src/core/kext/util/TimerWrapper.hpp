adc4ed6cdb6a265faef28753fbd300b543d24519
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-22 12:55:34 +0900

    update coding style @ src/core/kext/util/TimerWrapper

--- src/core/kext/util/TimerWrapper.hpp
+++ src/core/kext/util/TimerWrapper.hpp
@@ -6,16 +6,16 @@
 namespace org_pqrs_KeyRemap4MacBook {
   class TimerWrapper {
   public:
-    TimerWrapper(void) { workLoop = NULL; timer = NULL; }
-    void initialize(IOWorkLoop *_workLoop, OSObject *owner, IOTimerEventSource::Action func);
+    TimerWrapper(void) : workloop_(NULL), timer_(NULL) {}
+    void initialize(IOWorkLoop* wl, OSObject* owner, IOTimerEventSource::Action func);
     void terminate(void);
 
     IOReturn setTimeoutMS(UInt32 ms);
     void cancelTimeout(void);
 
   private:
-    IOWorkLoop *workLoop;
-    IOTimerEventSource *timer;
+    IOWorkLoop* workloop_;
+    IOTimerEventSource* timer_;
   };
 }
 
