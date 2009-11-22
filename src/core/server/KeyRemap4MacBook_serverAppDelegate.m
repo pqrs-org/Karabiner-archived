ed65219d9270f6c3a7bb23013be39552e9d14a90
tekezo <devnull@localhost>
2009-11-22 21:33:23 +0900

    update src/core/server around fast user switching

--- src/core/server/KeyRemap4MacBook_serverAppDelegate.m
+++ src/core/server/KeyRemap4MacBook_serverAppDelegate.m
@@ -6,6 +6,8 @@
 //  Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
+#include <stdlib.h>
+
 #import "KeyRemap4MacBook_serverAppDelegate.h"
 #include "util.h"
 #include "server_objc_part.h"
@@ -24,6 +26,7 @@
   [NSThread exit];
 }
 
+// ------------------------------------------------------------
 - (void) observer_NSWorkspaceDidActivateApplicationNotification:(NSNotification*)notification
 {
   NSRunningApplication* app = [[notification userInfo] objectForKey:@"NSWorkspaceApplicationKey"];
@@ -35,12 +38,39 @@
   setCurrentApplicationType([applicationName UTF8String]);
 }
 
+// ------------------------------------------------------------
+- (void) observer_NSWorkspaceSessionDidBecomeActiveNotification:(NSNotification*)notification
+{
+  NSLog(@"observer_NSWorkspaceSessionDidBecomeActiveNotification");
+  system("/bin/launchctl load /Library/LaunchAgents/org.pqrs.KeyRemap4MacBook.sysctl_confd.plist");
+  // reload myself
+  [NSApp terminate:self];
+}
+
+- (void) observer_NSWorkspaceSessionDidResignActiveNotification:(NSNotification*)notification
+{
+  NSLog(@"observer_NSWorkspaceSessionDidResignActiveNotification");
+  system("/bin/launchctl unload /Library/LaunchAgents/org.pqrs.KeyRemap4MacBook.sysctl_confd.plist");
+}
+
+// ------------------------------------------------------------
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
   [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
                                                       selector:@selector(observer_NSWorkspaceDidActivateApplicationNotification:)
                                                       name:NSWorkspaceDidActivateApplicationNotification
                                                       object:nil];
 
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                      selector:@selector(observer_NSWorkspaceSessionDidBecomeActiveNotification:)
+                                                      name:NSWorkspaceSessionDidBecomeActiveNotification
+                                                      object:nil];
+
+  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
+                                                      selector:@selector(observer_NSWorkspaceSessionDidResignActiveNotification:)
+                                                      name:NSWorkspaceSessionDidResignActiveNotification
+                                                      object:nil];
+
+  // ------------------------------------------------------------
   char buffer[512];
   getActiveApplicationName(buffer, sizeof(buffer));
   setCurrentApplicationType(buffer);
