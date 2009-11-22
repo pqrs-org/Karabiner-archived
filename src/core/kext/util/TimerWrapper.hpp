68bbaf4970d1a7ffc76bfaa5313fe9b574437244
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-22 17:07:06 +0900

    add lock to src/core/kext/util/TimerWrapper

--- src/core/kext/util/TimerWrapper.hpp
+++ src/core/kext/util/TimerWrapper.hpp
@@ -2,20 +2,24 @@
 #define TIMERWRAPPER_HPP
 
 #include "base.hpp"
+#include "IOLockWrapper.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
   class TimerWrapper {
   public:
-    TimerWrapper(void) : workloop_(NULL), timer_(NULL) {}
+    TimerWrapper(void) : workloop_(NULL), timer_(NULL), lock_(NULL) {}
     void initialize(IOWorkLoop* wl, OSObject* owner, IOTimerEventSource::Action func);
     void terminate(void);
 
     IOReturn setTimeoutMS(UInt32 ms);
     void cancelTimeout(void);
 
+    IOLock* getlock(void) { return lock_; }
+
   private:
     IOWorkLoop* workloop_;
     IOTimerEventSource* timer_;
+    IOLock* lock_;
   };
 }
 
