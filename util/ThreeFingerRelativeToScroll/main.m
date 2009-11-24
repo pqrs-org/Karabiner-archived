7d887a82fa01ab1eb3dfb218b8a04c9ddabc6c8e
Takayama Fumihiko <tekezo@pqrs.org>
2009-11-24 23:40:29 +0900

    add util/ThreeFingerRelativeToScroll

---
+++ util/ThreeFingerRelativeToScroll/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ThreeFingerRelativeToScroll
+//
+//  Created by Takayama Fumihiko on 09/11/24.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
