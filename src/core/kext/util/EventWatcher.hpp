d921646fe0db43ed8283f838d9d0617a163638c2
tekezo <devnull@localhost>
2009-12-03 02:16:18 +0900

    update EventWatcher @ kext

--- src/core/kext/util/EventWatcher.hpp
+++ src/core/kext/util/EventWatcher.hpp
@@ -4,17 +4,39 @@
 #include "base.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
-  namespace EventWatcher {
-    void initialize(void);
-    void terminate(void);
+  class EventWatcher {
+    friend class Params_KeyboardEventCallBack;
+    friend class Params_KeyboardSpecialEventCallback;
+    friend class Params_RelativePointerEventCallback;
+    friend class Params_ScrollWheelEventCallback;
 
-    void reset(void);
+  public:
+    static void initialize(void);
+    static void terminate(void);
 
-    void on(void);
+    static void reset(void);
 
-    void set(bool* b);
-    void unset(bool* b);
-  }
+    static void set(bool& b);
+    static void unset(bool& b);
+
+    static void countup(void);
+
+  private:
+    static void on(void);
+
+    enum {
+      MAXNUM = 32,
+    };
+    struct Item {
+      bool* flag;
+      int count;
+    };
+    static Item item_[MAXNUM];
+    static IOLock* lock_;
+
+    // A variable to ignore the event in the sequence that we called EventWatcher::set.
+    static int count_;
+  };
 }
 
 #endif
