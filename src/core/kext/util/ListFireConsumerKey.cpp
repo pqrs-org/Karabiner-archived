4c0481c94e05536f1ad34204d36ddb9653e82e1b
Takayama Fumihiko <tekezo@pqrs.org>
2009-12-02 22:24:45 +0900

    update ListFireConsumerKey @ kext

--- src/core/kext/util/ListFireConsumerKey.cpp
+++ src/core/kext/util/ListFireConsumerKey.cpp
@@ -3,11 +3,8 @@
 
 namespace org_pqrs_KeyRemap4MacBook {
   void
-  ListFireConsumerKey::fire(KeyboardSpecialEventCallback callback,
-                            OSObject *target, AbsoluteTime ts, OSObject *sender, void *refcon)
+  ListFireConsumerKey::fire(OSObject *target, AbsoluteTime ts, OSObject *sender, void *refcon)
   {
-    if (callback == NULL) return;
-
     for (int i = 0; i < size; ++i) {
       Item &item = list[i];
       unsigned int flavor = item.getKey();
@@ -18,7 +15,7 @@ namespace org_pqrs_KeyRemap4MacBook {
         flavor, guid, false, ts, sender, refcon,
       };
 
-      RemapUtil::fireConsumer(callback, params);
+      RemapUtil::fireConsumer(params);
     }
   }
 }
