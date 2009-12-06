69dc5e97061ffaa96e788706fdf453573c11ce9e
Takayama Fumihiko <tekezo@pqrs.org>
2009-12-06 12:52:30 +0900

    update keycode/* @ kext

--- src/core/kext/keycode/make-code.rb
+++ src/core/kext/keycode/make-code.rb
@@ -3,6 +3,7 @@
 Dir.glob("data/*.data") do |filename|
   if /(.+)\.data/ =~ File.basename(filename) then
     classname = $1
+    lastvalue = 0
     open("output/include.#{classname}.hpp", "w") do |of_hpp|
       open("output/include.#{classname}.cpp", "w") do |of_cpp|
         open(filename) do |f|
@@ -13,6 +14,12 @@ Dir.glob("data/*.data") do |filename|
             elsif /^(.+?)\s+(.+)$/ =~ l then
               name = $1
               value = $2
+
+              if value == '--AUTO--' then
+                value = lastvalue.succ
+              end
+              lastvalue = value
+
               of_hpp.print "static const #{classname} #{name};\n"
               of_cpp.print "const #{classname} #{classname}::#{name}(#{value});\n"
             else
