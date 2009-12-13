926f0d6e87b7562ec3e423107c938ce57770a612
tekezo <devnull@localhost>
2009-12-13 12:41:53 +0900

    cleanup NumHeldDownKeys @ kext

--- src/core/kext/util/NumHeldDownKeys.cpp
+++ src/core/kext/util/NumHeldDownKeys.cpp
@@ -1,53 +1,51 @@
 #include "NumHeldDownKeys.hpp"
 #include "Config.hpp"
-#include "RemapUtil.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
-  namespace NumHeldDownKeys {
-    int num = 0;
+  int NumHeldDownKeys::num_ = 0;
 
-    static void
-    log(void)
-    {
-      if (config.debug_devel) {
-        printf("KeyRemap4MacBook NumHeldDownKeys = %d\n", num);
-      }
+  void
+  NumHeldDownKeys::log(void)
+  {
+    if (config.debug_devel) {
+      printf("KeyRemap4MacBook NumHeldDownKeys = %d\n", num_);
     }
+  }
 
-    void
-    set(const RemapParams &remapParams)
-    {
-      if (remapParams.isKeyDownOrModifierDown()) {
-        ++num;
-      } else {
-        --num;
-      }
-      log();
+  void
+  NumHeldDownKeys::set(const RemapParams& remapParams)
+  {
+    if (remapParams.isKeyDownOrModifierDown()) {
+      ++num_;
+    } else {
+      --num_;
     }
+    log();
+  }
 
-    void set(const RemapConsumerParams &remapParams)
-    {
-      if (remapParams.params.eventType == EventType::DOWN) {
-        ++num;
-      } else if (remapParams.params.eventType == EventType::UP) {
-        --num;
-      }
-      log();
+  void
+  NumHeldDownKeys::set(const RemapConsumerParams& remapParams)
+  {
+    if (remapParams.params.eventType == EventType::DOWN) {
+      ++num_;
+    } else if (remapParams.params.eventType == EventType::UP) {
+      --num_;
     }
+    log();
+  }
 
-    void
-    reset(void)
-    {
-      if (config.debug_devel) {
-        printf("KeyRemap4MacBook NumHeldDownKeys::reset\n");
-      }
-      num = 0;
+  void
+  NumHeldDownKeys::reset(void)
+  {
+    if (config.debug_devel) {
+      printf("KeyRemap4MacBook NumHeldDownKeys::reset\n");
     }
+    num_ = 0;
+  }
 
-    bool
-    iszero(void)
-    {
-      return num <= 0;
-    }
+  bool
+  NumHeldDownKeys::iszero(void)
+  {
+    return num_ <= 0;
   }
 }
