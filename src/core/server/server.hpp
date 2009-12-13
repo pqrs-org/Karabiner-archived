c24d7b93b8ea97d19ce467e09b57fce1bb1fe057
tekezo <devnull@localhost>
2009-12-13 22:49:03 +0900

    apply uncrustify

--- src/core/server/server.hpp
+++ src/core/server/server.hpp
@@ -18,7 +18,7 @@ namespace KeyRemap4MacBook_server {
   private:
     bool makeSocket(void);
 
-    void sendReply(int sock, void *data, size_t size, int error);
+    void sendReply(int sock, void* data, size_t size, int error);
     void dispatchOperator(int sock);
 
     org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::Error do_GetWorkspaceData(org_pqrs_KeyRemap4MacBook::KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& reply);
