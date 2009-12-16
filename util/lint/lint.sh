bafef0b45f552ab1e2da9cd579e26f1592c9bc02
Takayama Fumihiko <tekezo@pqrs.org>
2009-12-16 18:03:25 +0900

    update util/link to support ThreeFingerRelativeToScroll

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -4,5 +4,11 @@ basedir=`dirname $0`
 
 for f in `find $basedir/../../* -name 'project.pbxproj'`; do
     echo "Check $f"
-    "$basedir/xcodeproj.rb" < $f
+
+    if [ "$(basename $(dirname $f))" = "ThreeFingerRelativeToScroll.xcodeproj" ]; then
+        # ThreeFingerRelativeToScroll doesn't use GC to prevent malloc error in private API.
+        "$basedir/xcodeproj.rb" GCC_ENABLE_OBJC_GC < $f || exit 1
+    else
+        "$basedir/xcodeproj.rb" < $f || exit 1
+    fi
 done
