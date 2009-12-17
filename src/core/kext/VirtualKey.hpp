6a56aefc3e267d47057a698c7eae86cdcde442a7
tekezo <devnull@localhost>
2009-12-17 23:43:42 +0900

    update VirtualKey @ kext  - add Handle_VK_JIS_TOGGLE_EISUU_KANA  - add handle_VK_JIS_EISUU_x2  - add handle_VK_JIS_KANA_x2  - add handle_VK_JIS_BACKSLASH

--- src/core/kext/VirtualKey.hpp
+++ src/core/kext/VirtualKey.hpp
@@ -32,7 +32,20 @@ namespace org_pqrs_KeyRemap4MacBook {
   private:
     static Handle_VK_LOCK_common h_;
   };
+
   // ----------------------------------------------------------------------
+  class Handle_VK_JIS_TOGGLE_EISUU_KANA {
+  public:
+    static bool handle(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata);
+
+  private:
+    // It is necessary to save toKeyCode for KeyUp.
+    static KeyCode newkeycode_;
+  };
+
+  bool handle_VK_JIS_EISUU_x2(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata);
+  bool handle_VK_JIS_KANA_x2(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata);
+  bool handle_VK_JIS_BACKSLASH(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata);
 }
 
 #endif
