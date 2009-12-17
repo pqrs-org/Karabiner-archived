4410e58076edae27c637474dcfb1cd5ccd3acbdf
tekezo <devnull@localhost>
2009-12-17 23:12:54 +0900

    add VirtualKey.[ch]pp @ kext

---
+++ src/core/kext/VirtualKey.cpp
@@ -0,0 +1,25 @@
+#include "FlagStatus.hpp"
+#include "VirtualKey.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  // ----------------------------------------------------------------------
+  bool
+  Handle_VK_LOCK_common::handle(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata)
+  {
+    if (params.key != key_) return false;
+
+    if (params.eventType == EventType::DOWN) {
+      active_ = ! active_;
+      if (active_) {
+        FlagStatus::lock_increase(flag_);
+      } else {
+        FlagStatus::lock_decrease(flag_);
+      }
+    }
+
+    return true;
+  }
+
+  Handle_VK_LOCK_common Handle_VK_LOCK_FN::h_(KeyCode::VK_LOCK_FN, ModifierFlag::FN);
+  // ----------------------------------------------------------------------
+}
---
+++ src/core/kext/VirtualKey.hpp
@@ -0,0 +1,30 @@
+#ifndef VIRTUALKEY_HPP
+#define VIRTUALKEY_HPP
+
+#include "bridge.hpp"
+#include "CallbackWrapper.hpp"
+#include "KeyCode.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  class Handle_VK_LOCK_common {
+  public:
+    Handle_VK_LOCK_common(const KeyCode& k, const ModifierFlag& f) :
+      active_(false), key_(k), flag_(f) {}
+    bool handle(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata);
+
+  private:
+    bool active_;
+    const KeyCode& key_;
+    const ModifierFlag& flag_;
+  };
+
+  class Handle_VK_LOCK_FN {
+  public:
+    static bool handle(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata) { return h_.handle(params, workspacedata); }
+
+  private:
+    static Handle_VK_LOCK_common h_;
+  };
+}
+
+#endif
