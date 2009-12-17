b5d16f9342dd76e19b0084c21e1706a91c2b68df
Takayama Fumihiko <tekezo@pqrs.org>
2009-12-17 23:43:42 +0900

    update VirtualKey @ kext  - add Handle_VK_JIS_TOGGLE_EISUU_KANA  - add handle_VK_JIS_EISUU_x2  - add handle_VK_JIS_KANA_x2  - add handle_VK_JIS_BACKSLASH

--- src/core/kext/VirtualKey.cpp
+++ src/core/kext/VirtualKey.cpp
@@ -1,5 +1,6 @@
 #include "FlagStatus.hpp"
 #include "VirtualKey.hpp"
+#include "RemapUtil.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
   // ----------------------------------------------------------------------
@@ -22,5 +23,63 @@ namespace org_pqrs_KeyRemap4MacBook {
 
   Handle_VK_LOCK_common Handle_VK_LOCK_FN::h_(KeyCode::VK_LOCK_FN, ModifierFlag::FN);
   Handle_VK_LOCK_common Handle_VK_LOCK_COMMAND_R::h_(KeyCode::VK_LOCK_COMMAND_R, ModifierFlag::COMMAND_R);
+
   // ----------------------------------------------------------------------
+  bool
+  Handle_VK_JIS_TOGGLE_EISUU_KANA::handle(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata)
+  {
+    if (params.key != KeyCode::VK_JIS_TOGGLE_EISUU_KANA) return false;
+
+    if (params.eventType == EventType::DOWN) {
+      if (workspacedata.inputmode == KeyRemap4MacBook_bridge::GetWorkspaceData::INPUTMODE_JAPANESE) {
+        newkeycode_ = KeyCode::JIS_EISUU;
+      } else {
+        newkeycode_ = KeyCode::JIS_KANA;
+      }
+    }
+
+    params.key = newkeycode_;
+    RemapUtil::fireKey(params, workspacedata);
+    return true;
+  }
+
+  KeyCode Handle_VK_JIS_TOGGLE_EISUU_KANA::newkeycode_;
+
+  // ----------------------------------------------------------------------
+  bool
+  handle_VK_JIS_EISUU_x2(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata)
+  {
+    if (params.key != KeyCode::VK_JIS_EISUU_x2) return false;
+
+    if (params.eventType == EventType::DOWN) {
+      for (int i = 0; i < 2; ++i) {
+        RemapUtil::fireKey_downup(params.flags, KeyCode::JIS_EISUU, params.keyboardType, workspacedata);
+      }
+    }
+    return true;
+  }
+
+  bool
+  handle_VK_JIS_KANA_x2(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata)
+  {
+    if (params.key != KeyCode::VK_JIS_KANA_x2) return false;
+
+    if (params.eventType == EventType::DOWN) {
+      for (int i = 0; i < 2; ++i) {
+        RemapUtil::fireKey_downup(params.flags, KeyCode::JIS_KANA, params.keyboardType, workspacedata);
+      }
+    }
+    return true;
+  }
+
+  bool
+  handle_VK_JIS_BACKSLASH(Params_KeyboardEventCallBack& params, const KeyRemap4MacBook_bridge::GetWorkspaceData::Reply& workspacedata)
+  {
+    if (params.key != KeyCode::VK_JIS_BACKSLASH) return false;
+
+    params.key = KeyCode::BACKSLASH;
+    params.keyboardType = KeyboardType::MACBOOK;
+    RemapUtil::fireKey(params, workspacedata);
+    return true;
+  }
 }
