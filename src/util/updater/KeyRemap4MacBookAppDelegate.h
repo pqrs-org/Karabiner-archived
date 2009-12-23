6db3a76d8852714e4b6c2f8fe00018c8f3f16374
Takayama Fumihiko <tekezo@pqrs.org>
2009-12-24 01:09:38 +0900

    add src/util/updater

---
+++ src/util/updater/KeyRemap4MacBookAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  KeyRemap4MacBookAppDelegate.h
+//  KeyRemap4MacBook
+//
+//  Created by Takayama Fumihiko on 09/12/24.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface KeyRemap4MacBookAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ src/util/updater/KeyRemap4MacBookAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  KeyRemap4MacBookAppDelegate.m
+//  KeyRemap4MacBook
+//
+//  Created by Takayama Fumihiko on 09/12/24.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "KeyRemap4MacBookAppDelegate.h"
+
+@implementation KeyRemap4MacBookAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
