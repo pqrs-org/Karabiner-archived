6def527b90e71da2c912ca7d9ac48e37e0ac72a7
Takayama Fumihiko <tekezo@pqrs.org>
2009-12-27 23:05:19 +0900

    add files/prefpane/scripts/conv.rb

---
+++ files/prefpane/scripts/conv.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/ruby
+
+xmlfile = ARGV[0]
+if xmlfile.nil? || ! FileTest.exist?(xmlfile) then
+  $stderr.print "Usage: conv.rb checkbox.xml\n"
+  exit 1
+end
+
+open(xmlfile) do |f|
+  lines = f.readlines
+
+  while true
+    isreplaced = false
+    result = []
+
+    lines.each do |l|
+      if /<!--\s*#include "(.+)"\s*-->/ =~ l then
+        includefile = $1
+        unless FileTest.exist?(includefile) then
+          $stderr.print "No such file: #{includefile}\n"
+          exit 1
+        end
+
+        open(includefile) do |infile|
+          result += infile.readlines
+        end
+        isreplaced = true
+      else
+        result << l
+      end
+    end
+
+    lines = result
+
+    break unless isreplaced
+  end
+
+  outputdir = File.dirname($0) + '/../output'
+  if ! FileTest.exist?(outputdir) then
+    Dir.mkdir(outputdir)
+  end
+
+  open("#{outputdir}/checkbox.xml", "w") do |outfile|
+    outfile.print lines.join('')
+  end
+end
