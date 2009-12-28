88540bf5844176aa99a6e53de4918c833e1da278
tekezo <devnull@localhost>
2009-12-28 09:18:25 +0900

    update updater

--- src/util/updater/KeyRemap4MacBookAppDelegate.m
+++ src/util/updater/KeyRemap4MacBookAppDelegate.m
@@ -19,7 +19,7 @@
   return [BUNDLEPREFIX(Common) getExecResult:path args:args];
 }
 
-- (int) isCheckUpdate
+- (int) getCheckUpdate
 {
   NSString* result = [self execSysctl:[NSArray arrayWithObjects:@"checkupdate", nil]];
   return [result intValue];
@@ -27,10 +27,16 @@
 
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification
 {
-  if ([self isCheckUpdate]) {
-    NSLog(@"checkForUpdatesInBackground");
-    [_suupdater checkForUpdatesInBackground];
+  int checkupdate = [self getCheckUpdate];
+  // check nothing.
+  if (checkupdate == 0) return;
+
+  // check beta & stable releases.
+  if (checkupdate == 2) {
+    [_suupdater setFeedURL:[[[NSURL alloc] initWithString:@"http://pqrs.org/macosx/keyremap4macbook/files/appcast-devel.xml"] autorelease]];
   }
+  NSLog(@"checkForUpdatesInBackground %@", [[_suupdater feedURL] absoluteString]);
+  [_suupdater checkForUpdatesInBackground];
 }
 
 @end
