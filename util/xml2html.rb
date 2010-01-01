796236739b1ef692c9e9ca96b0dfea089abace8a
Takayama Fumihiko <tekezo@pqrs.org>
2010-01-01 17:13:37 +0900

    update util/xml2html.rb

--- util/xml2html.rb
+++ util/xml2html.rb
@@ -2,34 +2,40 @@
 
 require 'cgi'
 
-system('make -C ../files/prefpane')
-
-file = "%s/../files/prefpane/output/checkbox.xml" % File.dirname($0)
-open(file) do |f|
-  total = 0
-  while l = f.gets
-    if /<list>/ =~ l then
-      print "<ul>"
-    end
-    if /<\/list>/ =~ l then
-      print "</ul>"
-    end
-    if /<item>/ =~ l then
-      print "<li>"
-    end
-    if /<\/item>/ =~ l then
-      print "</li>"
-    end
-    if /<name>(.+)<\/name>/ =~ l then
-      print $1
-    end
-    if /<appendix>(.+)<\/appendix>/ =~ l then
-      print "<br />", $1
-    end
-    if /<sysctl>/ =~ l then
-      total += 1
-    end
+lines = []
+if ARGV[0] == '-' then
+  lines = $stdin.readlines
+else
+  system('make -C ../files/prefpane > /dev/null')
+  file = "%s/../files/prefpane/output/checkbox.xml" % File.dirname($0)
+  open(file) do |f|
+    lines = f.readlines
   end
-  print "\n\n"
-  print "total: #{total}\n"
 end
+
+total = 0
+lines.each do |l|
+  if /<list>/ =~ l then
+    print "<ul>"
+  end
+  if /<\/list>/ =~ l then
+    print "</ul>"
+  end
+  if /<item>/ =~ l then
+    print "<li>"
+  end
+  if /<\/item>/ =~ l then
+    print "</li>"
+  end
+  if /<name>(.+)<\/name>/ =~ l then
+    print $1
+  end
+  if /<appendix>(.+)<\/appendix>/ =~ l then
+    print "<br />", $1
+  end
+  if /<sysctl>/ =~ l then
+    total += 1
+  end
+end
+print "\n\n"
+print "total: #{total}\n"
