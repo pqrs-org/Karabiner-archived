1051c7c3d52b4c13ab5bcc7b39a7654e524bbb68
tekezo <devnull@localhost>
2010-01-11 20:41:12 +0900

    add auto_ptr.hpp @ kext

---
+++ src/core/kext/util/auto_ptr.hpp
@@ -0,0 +1,17 @@
+#ifndef AUTO_PTR_HPP
+#define AUTO_PTR_HPP
+
+#define DECLARE_AUTO_PTR(CLASS)                         \
+  class auto_ptr {                                      \
+  public:                                               \
+    auto_ptr(CLASS * ptr) { ptr_ = ptr; }               \
+    ~auto_ptr(void) { if (ptr_) delete ptr_; }          \
+    bool operator!(void) const { return ptr_ != NULL; } \
+    CLASS & operator*(void) const { return *ptr_; }     \
+                                                        \
+  private:                                              \
+    auto_ptr(auto_ptr & ptr);                           \
+    CLASS* ptr_;                                        \
+  };
+
+#endif
