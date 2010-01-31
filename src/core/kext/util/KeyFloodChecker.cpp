7768d3eb840758de01993558241afda78143c17e
Takayama Fumihiko <tekezo@pqrs.org>
2010-01-31 21:12:27 +0900

    add option.jis_drop_frequently_eisuukana

---
+++ src/core/kext/util/KeyFloodChecker.cpp
@@ -0,0 +1,34 @@
+#include "KeyFloodChecker.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  void
+  KeyFloodChecker::add(void)
+  {
+    for (int i = 0; i < MAXNUM; ++i) {
+      if (! item[i].active) {
+        item[i].active = true;
+        item[i].ic.begin();
+        return;
+      }
+    }
+  }
+
+  bool
+  KeyFloodChecker::check(uint32_t millisec, int allowdnum) {
+    int count = 0;
+
+    for (int i = 0; i < MAXNUM; ++i) {
+      if (! item[i].active) continue;
+
+      if (item[i].ic.getmillisec() < millisec) {
+        ++count;
+      } else {
+        // drop
+        item[i].active = false;
+      }
+    }
+
+    printf("KeyFloodChecker: count=%d\n", count);
+    return count <= allowdnum;
+  }
+}
