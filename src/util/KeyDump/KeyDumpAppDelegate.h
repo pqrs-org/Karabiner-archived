41245e90537d74259333a20f795f94dae5c518ee
tekezo <devnull@localhost>
2010-02-15 13:52:36 +0900

    update KeyDump for server_objc_part updates

--- src/util/KeyDump/KeyDumpAppDelegate.h
+++ src/util/KeyDump/KeyDumpAppDelegate.h
@@ -9,13 +9,17 @@
 #import <Cocoa/Cocoa.h>
 #import "EventQueue.h"
 #import "OtherInformationStore.h"
+#import "server_objc_part.h"
 
 @interface KeyDumpAppDelegate : NSObject <NSApplicationDelegate> {
   NSWindow* window;
   IBOutlet id keyResponder_;
   IBOutlet OtherInformationStore* otherinformationstore_;
+  IBOutlet ServerObjcPart* serverobjcpart_;
 }
 
 @property (assign) IBOutlet NSWindow* window;
+@property (assign) ServerObjcPart* serverobjcpart_;
+@property (assign) OtherInformationStore* otherinformationstore_;
 
 @end
