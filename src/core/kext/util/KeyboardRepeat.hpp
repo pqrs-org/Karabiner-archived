8b40f09e9d48f553f73c6ac458335532a503371b
tekezo <devnull@localhost>
2010-02-19 23:53:56 +0900

    add Consumer support at KeyboardRepeat @ kext

--- src/core/kext/util/KeyboardRepeat.hpp
+++ src/core/kext/util/KeyboardRepeat.hpp
@@ -32,23 +32,43 @@ namespace org_pqrs_KeyRemap4MacBook {
       set(p.eventType, p.flags, p.key, p.keyboardType, wait);
     }
 
+    // for consumer
+    static void set(EventType eventType,
+                    Flags flags,
+                    ConsumerKeyCode key);
+    static void set(const Params_KeyboardSpecialEventCallback& p) {
+      set(p.eventType, p.flags, p.key);
+    }
+
     // --------------------------------------------------
     // primitive operations.
     static void primitive_add(EventType eventType,
                               Flags flags,
                               KeyCode key,
                               KeyboardType keyboardType);
+
+    // We don't need to provide primitive_add for consumer.
+
     static void primitive_start(int wait = config.get_repeat_initial_wait());
 
   private:
     enum {
       MAXNUM = 16,
+
+      CONSUMER_INITIAL_WAIT = 500,
+      CONSUMER_WAIT = 100,
     };
     struct Item {
-      Item(void) : params(NULL), active(false) {}
+      enum Type {
+        TYPE_KEYBOARD,
+        TYPE_CONSUMER,
+      };
+      Item(void) : params(NULL), params_consumer(NULL), active(false), type(TYPE_KEYBOARD) {}
 
       Params_KeyboardEventCallBack* params;
+      Params_KeyboardSpecialEventCallback* params_consumer;
       bool active;
+      Type type;
     };
 
     // ------------------------------------------------------------
@@ -60,6 +80,9 @@ namespace org_pqrs_KeyRemap4MacBook {
                                      Flags flags,
                                      KeyCode key,
                                      KeyboardType keyboardType);
+    static void primitive_add_nolock(EventType eventType,
+                                     Flags flags,
+                                     ConsumerKeyCode key);
     static void primitive_start_nolock(int wait);
 
     static int getActiveItemNum(void);
