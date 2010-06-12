d1998e9cdb4f35a3d67bd460bd77612698436133
tekezo <devnull@localhost>
2010-06-12 13:14:41 +0900

    add ButtonStatus @ kext

---
+++ Tests/kext/ButtonStatus/Config.hpp
@@ -0,0 +1,1 @@
+../keycode/Config.hpp
---
+++ src/core/kext/util/ButtonStatus.cpp
@@ -0,0 +1,54 @@
+#include "ButtonStatus.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  ButtonStatus::Item ButtonStatus::item_[ButtonStatus::MAXNUM];
+
+  // ----------------------------------------------------------------------
+  bool
+  ButtonStatus::initialize(void)
+  {
+    for (int i = 0; i < MAXNUM; ++i) {
+      item_[i].initialize(1 << i);
+    }
+    return true;
+  }
+
+  void
+  ButtonStatus::set(Buttons current, Buttons previous)
+  {
+    for (int i = 0; i < MAXNUM; ++i) {
+      item_[i].set(current, previous);
+    }
+  }
+
+  void
+  ButtonStatus::reset(void)
+  {
+    for (int i = 0; i < MAXNUM; ++i) {
+      item_[i].reset();
+    }
+  }
+
+  Buttons
+  ButtonStatus::makeButtons(void)
+  {
+    Buttons flags;
+    for (int i = 0; i < MAXNUM; ++i) {
+      flags.add(item_[i].makeButtons());
+    }
+    return flags;
+  }
+
+  // ------------------------------------------------------------
+#define FOREACH_TO_FLAGS(METHOD) {          \
+    for (int i = 0; i < MAXNUM; ++i) {      \
+      if (buttons.isOn(item_[i].button_)) { \
+        item_[i].METHOD();                  \
+      }                                     \
+    }                                       \
+}
+
+  void ButtonStatus::increase(Buttons buttons) { FOREACH_TO_FLAGS(increase); }
+  void ButtonStatus::decrease(Buttons buttons) { FOREACH_TO_FLAGS(decrease); }
+#undef FOREACH_TO_FLAGS
+}
