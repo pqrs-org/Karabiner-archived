d728e140a01485810c085045d2c0b43e8e6868d1
tekezo <devnull@localhost>
2010-06-21 22:21:15 +0900

    add src/core/kext/config/inc.preprocess.rb

--- src/core/kext/config/make-code.rb
+++ src/core/kext/config/make-code.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 
+require 'inc.preprocess.rb'
 require 'inc.filter.rb'
 
 $outfile = {
@@ -27,85 +28,6 @@ $func = {
 
   :simultaneouskeypresses => [],
 }
-
-# ======================================================================
-def getextrakey(key)
-  case key
-  when 'HOME'
-    'CURSOR_LEFT'
-  when 'END'
-    'CURSOR_RIGHT'
-  when 'PAGEUP'
-    'CURSOR_UP'
-  when 'PAGEDOWN'
-    'CURSOR_DOWN'
-  when 'FORWARD_DELETE'
-    'DELETE'
-  else
-    nil
-  end
-end
-
-def preprocess(listAutogen)
-  modify = false
-
-  list = []
-  listAutogen.each do |autogen|
-    if /VK_(COMMAND|CONTROL|SHIFT|OPTION)/ =~ autogen then
-      key = $1
-      list << autogen.gsub(/VK_#{key}/, "ModifierFlag::#{key}_L")
-      list << autogen.gsub(/VK_#{key}/, "ModifierFlag::#{key}_R")
-      modify = true
-    elsif /VK_MOD_CCOS_L/ =~ autogen then
-      list << autogen.gsub(/VK_MOD_CCOS_L/, "ModifierFlag::COMMAND_L | ModifierFlag::CONTROL_L | ModifierFlag::OPTION_L | ModifierFlag::SHIFT_L")
-      modify = true
-    elsif /VK_MOD_CCS_L/ =~ autogen then
-      list << autogen.gsub(/VK_MOD_CCS_L/, "ModifierFlag::COMMAND_L | ModifierFlag::CONTROL_L | ModifierFlag::SHIFT_L")
-      modify = true
-    elsif /VK_MOD_CCO_L/ =~ autogen then
-      list << autogen.gsub(/VK_MOD_CCO_L/, "ModifierFlag::COMMAND_L | ModifierFlag::CONTROL_L | ModifierFlag::OPTION_L")
-      modify = true
-    elsif /VK_MOD_ANY/ =~ autogen then
-      # to reduce combination, we ignore same modifier combination such as (COMMAND_L | COMMAND_R).
-      modifiers = [
-                   'VK_COMMAND',
-                   'VK_CONTROL',
-                   'ModifierFlag::FN',
-                   'VK_OPTION',
-                   'VK_SHIFT',
-                  ]
-      (0 .. modifiers.length).to_a.reverse.each do |i|
-        modifiers.combination(i).to_a.each do |pattern|
-          pat = pattern.dup
-          pat << "ModifierFlag::NONE"
-          list << autogen.gsub(/VK_MOD_ANY/, pat.join(' | '))
-        end
-      end
-      modify = true
-
-    elsif /FROMKEYCODE_(HOME|END|PAGEUP|PAGEDOWN|FORWARD_DELETE)\s*,\s*ModifierFlag::/ =~ autogen then
-      key = $1
-      extrakey = getextrakey(key)
-      list << autogen.gsub(/FROMKEYCODE_#{key}\s*,/, "KeyCode::#{key},")
-      list << autogen.gsub(/FROMKEYCODE_#{key}\s*,/, "KeyCode::#{extrakey}, ModifierFlag::FN |")
-      modify = true
-    elsif /FROMKEYCODE_(HOME|END|PAGEUP|PAGEDOWN|FORWARD_DELETE)/ =~ autogen then
-      key = $1
-      extrakey = getextrakey(key)
-      list << autogen.gsub(/FROMKEYCODE_#{key}\s*,/, "KeyCode::#{key},")
-      list << autogen.gsub(/FROMKEYCODE_#{key}\s*,/, "KeyCode::#{extrakey}, ModifierFlag::FN,")
-      modify = true
-    else
-      list << autogen
-    end
-  end
-
-  if modify then
-    list = preprocess(list)
-  end
-
-  list
-end
 
 # ======================================================================
 def parseautogen(name, lines, autogen_index)
@@ -354,7 +276,7 @@ $stdin.read.scan(/<item>.+?<\/item>/m).each do |item|
 
   # ======================================================================
   lines = item.split(/\n/)
-  lines = preprocess(lines)
+  lines = Preprocesser.new().preprocess(lines)
 
   code_key, code_consumer, code_pointing, code_variable, autogen_index, simultaneouskeypresses_code, simultaneouskeypresses_variable = parseautogen(name, lines, 0)
 
