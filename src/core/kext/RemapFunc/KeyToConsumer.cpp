e04d5c817f313beeeb90a8460024ccff99a93678
Takayama Fumihiko <tekezo@pqrs.org>
2010-06-28 10:26:23 +0900

    add RemapFunc/KeyToConsumer

---
+++ src/core/kext/RemapFunc/KeyToConsumer.cpp
@@ -0,0 +1,90 @@
+#include "KeyToConsumer.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  namespace RemapFunc {
+    void
+    KeyToConsumer::initialize(void)
+    {
+      keytokey_.initialize();
+      consumertoconsumer_.initialize();
+    }
+
+    void
+    KeyToConsumer::terminate(void)
+    {
+      keytokey_.terminate();
+      consumertoconsumer_.terminate();
+    }
+
+    void
+    KeyToConsumer::add(KeyCode newval)
+    {
+      switch (index_) {
+        case 0:
+          fromKey_.key = newval;
+          keytokey_.add(newval);
+          keytokey_.add(KeyCode::VK_NONE);
+          break;
+        default:
+          IOLOG_ERROR("Invalid KeyToConsumer::add\n");
+          break;
+      }
+      ++index_;
+    }
+
+    void
+    KeyToConsumer::add(ConsumerKeyCode newval)
+    {
+      switch (index_) {
+        case 0:
+          IOLOG_ERROR("Invalid KeyToConsumer::add\n");
+          break;
+        case 1:
+          consumertoconsumer_.add(ConsumerKeyCode::VK_PSEUDO_KEY);
+          consumertoconsumer_.add(fromKey_.flags);
+          // pass-through
+        default:
+          consumertoconsumer_.add(newval);
+          break;
+      }
+      ++index_;
+    }
+
+    void
+    KeyToConsumer::add(Flags newval)
+    {
+      switch (index_) {
+        case 0:
+          IOLOG_ERROR("Invalid KeyToConsumer::add\n");
+          break;
+        case 1:
+          fromKey_.flags = newval;
+          break;
+        default:
+          consumertoconsumer_.add(newval);
+          break;
+      }
+    }
+
+    bool
+    KeyToConsumer::remap(RemapParams& remapParams)
+    {
+      if (! fromkeychecker_.isFromKey(remapParams, fromKey_.key, fromKey_.flags)) return false;
+
+      bool isKeyDown = remapParams.isKeyDownOrModifierDown();
+      bool result = keytokey_.remap(remapParams);
+      if (! result) return false;
+
+      // ----------------------------------------
+      Params_KeyboardSpecialEventCallback::auto_ptr ptr(Params_KeyboardSpecialEventCallback::alloc(isKeyDown ? EventType::DOWN : EventType::UP,
+                                                                                                   FlagStatus::makeFlags(),
+                                                                                                   ConsumerKeyCode::VK_PSEUDO_KEY,
+                                                                                                   false));
+      if (! ptr) return false;
+      Params_KeyboardSpecialEventCallback& params = *ptr;
+
+      RemapConsumerParams rp(params);
+      return consumertoconsumer_.remap(rp);
+    }
+  }
+}
---
+++ src/core/kext/RemapFunc/KeyToConsumer.hpp
@@ -0,0 +1,37 @@
+#ifndef KEYTOCONSUMER_HPP
+#define KEYTOCONSUMER_HPP
+
+#include "RemapFuncBase.hpp"
+#include "KeyToKey.hpp"
+#include "ConsumerToConsumer.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  namespace RemapFunc {
+    class KeyToConsumer {
+    public:
+      KeyToConsumer(void) : index_(0) {}
+      bool remap(RemapParams& remapParams);
+      void initialize(void);
+      void terminate(void);
+
+      // ----------------------------------------
+      // [0] => fromKey
+      // [1] => toKeys[0]
+      // [2] => toKeys[1]
+      // [3] => ...
+      void add(KeyCode newval);
+      void add(ConsumerKeyCode newval);
+      void add(Flags newval);
+
+    private:
+      size_t index_;
+      FromKeyChecker fromkeychecker_;
+      PairKeyFlags fromKey_;
+
+      KeyToKey keytokey_;
+      ConsumerToConsumer consumertoconsumer_;
+    };
+  }
+}
+
+#endif
