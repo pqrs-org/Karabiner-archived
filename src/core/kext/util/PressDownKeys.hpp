bb34fa83c4696e2c854800b1f98affec64e37a51
Takayama Fumihiko <tekezo@pqrs.org>
2010-07-10 13:09:51 +0900

    update PressDownKeys

--- src/core/kext/util/PressDownKeys.hpp
+++ src/core/kext/util/PressDownKeys.hpp
@@ -3,26 +3,29 @@
 
 #include "base.hpp"
 #include "KeyCode.hpp"
+#include "List.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
   // store EventOutput::FireKey results
   class PressDownKeys {
   public:
     static void initialize(void);
+    static void terminate(void);
     static void add(KeyCode key, KeyboardType keyboardType);
     static void remove(KeyCode key, KeyboardType keyboardType);
     static void clear(void);
 
   private:
-    enum {
-      MAXNUM = 16,
-    };
-    struct Item {
-      bool enable;
+    class Item : public List::Item {
+    public:
+      Item(KeyCode k, KeyboardType kt) : key(k), keyboardType(kt) {}
+      virtual ~Item(void) {}
+
       KeyCode key;
       KeyboardType keyboardType;
     };
-    static Item item_[MAXNUM];
+    static List* list_;
+    static IOLock* lock_;
   };
 }
 
