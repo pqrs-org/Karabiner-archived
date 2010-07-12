b52453f4a12e6cc3889a29032c38272dd86ad564
Takayama Fumihiko <tekezo@pqrs.org>
2010-07-12 22:16:55 +0900

    add IOLOG_DEVEL

--- src/core/kext/util/EventInputQueue.cpp
+++ src/core/kext/util/EventInputQueue.cpp
@@ -437,6 +437,8 @@ namespace org_pqrs_KeyRemap4MacBook {
 
     if (! queue_) return;
 
+    IOLOG_DEVEL("EventInputQueue::fire queue_->size = %d\n", static_cast<int>(queue_->size()));
+
     Item* p = static_cast<Item*>(queue_->front());
     if (! p) return;
 
