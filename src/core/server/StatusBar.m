447ff778c588460a3288549df5e067584aa7f6f2
tekezo <devnull@localhost>
2010-07-22 23:31:37 +0900

    add StatusBar @ server

---
+++ src/core/server/StatusBar.m
@@ -0,0 +1,80 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+#import "StatusBar.h"
+#import "ConfigControl.h"
+
+@implementation StatusBar
+
+- (void) refresh
+{
+  if (! [ConfigControl isStatusbarEnable]) {
+    if (statusItem_) {
+      [[NSStatusBar systemStatusBar] removeStatusItem:statusItem_];
+      [statusItem_ release];
+    }
+
+  } else {
+    statusItem_ = [[NSStatusBar systemStatusBar] statusItemWithLength:24];
+    [statusItem_ retain];
+
+    [statusItem_ setTitle:@""];
+    [statusItem_ setToolTip:@"KeyRemap4MacBook"];
+    [statusItem_ setImage:[NSImage imageNamed:@"icon.statusbar.0"]];
+    [statusItem_ setAlternateImage:[NSImage imageNamed:@"icon.statusbar.1"]];
+    [statusItem_ setHighlightMode:YES];
+
+    [statusItem_ setMenu:menu_];
+  }
+}
+
+- (void) statusBarItemSelected:(id)sender
+{
+  NSString* idx = [sender representedObject];
+  [ConfigControl select:idx];
+}
+
+- (void) menuNeedsUpdate:(NSMenu*)menu
+{
+  // --------------------
+  // clear
+  for (;;) {
+    NSMenuItem* item = [menu itemAtIndex:0];
+    if (item == nil || [[item title] isEqualToString:@"endoflist"]) break;
+
+    [menu removeItemAtIndex:0];
+  }
+
+  // --------------------
+  // append
+  NSArray* list = [ConfigControl getConfigList];
+  int i = 0;
+  for (id name in list) {
+    if ([name length] == 0) continue;
+
+    NSString* selected = [name substringToIndex:1];
+    NSString* title = [name substringFromIndex:1];
+
+    NSMenuItem* newItem = [[[NSMenuItem alloc] initWithTitle:title action:@selector(statusBarItemSelected:) keyEquivalent:@""] autorelease];
+    NSString* idx = [[[NSString alloc] initWithFormat:@"%d", i] autorelease];
+    [newItem setRepresentedObject:idx];
+    if ([selected isEqualToString:@"+"]) {
+      [newItem setState:NSOnState];
+    } else {
+      [newItem setState:NSOffState];
+    }
+    [menu insertItem:newItem atIndex:i];
+    ++i;
+  }
+}
+
+- (IBAction) openPreferencePane:(id)sender
+{
+  [[NSWorkspace sharedWorkspace] openFile:@"/Library/PreferencePanes/KeyRemap4MacBook.prefPane"];
+}
+
+- (IBAction) launchEventViewer:(id)sender
+{
+  [[NSWorkspace sharedWorkspace] launchApplication:@"/Library/org.pqrs/KeyRemap4MacBook/app/KeyDump.app"];
+}
+
+@end
