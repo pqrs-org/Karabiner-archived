cfde8abc008b55bfbbc7fcd31afcd2924c0a2708
tekezo <devnull@localhost>
2010-09-30 01:23:30 +0900

    update keycode/make-code.rb to output include.KeyCode.raw

--- src/core/kext/keycode/make-code.rb
+++ src/core/kext/keycode/make-code.rb
@@ -6,33 +6,41 @@ Dir.glob("*.data") do |filename|
   if /(.+)\.data/ =~ File.basename(filename) then
     classname = $1
     lastvalue = 0
-    open("../output/include.#{classname}.hpp", "w") do |of_hpp|
-      open("../output/include.#{classname}.cpp", "w") do |of_cpp|
-        open("| cpp -P #{filename}") do |f|
-          while l = f.gets
-            if /^\/\// =~ l then
-              of_hpp.print l
-              of_cpp.print l
-            elsif /^(.+?)\s+(.+)$/ =~ l then
-              name = $1
-              value = $2
 
-              if value == '--AUTO--' then
-                value = lastvalue.succ
-              end
-              lastvalue = value
+    outfile = {
+      :hpp => open("../output/include.#{classname}.hpp", "w"),
+      :cpp => open("../output/include.#{classname}.cpp", "w"),
+      :raw => open("../output/include.#{classname}.raw", "w"),
+    }
 
-              of_hpp.print "static const #{classname} #{name};\n"
-              of_cpp.print "const #{classname} #{classname}::#{name}(#{value});\n"
-            else
-              unless l.strip.empty? then
-                of_hpp.print "ERROR #{l}\n"
-                of_cpp.print "ERROR #{l}\n"
-              end
-            end
+    open("| cpp -P #{filename}") do |f|
+      while l = f.gets
+        if /^\/\// =~ l then
+          outfile[:hpp] << l
+          outfile[:cpp] << l
+        elsif /^(.+?)\s+(.+)$/ =~ l then
+          name = $1
+          value = $2
+
+          if value == '--AUTO--' then
+            value = lastvalue.succ
+          end
+          lastvalue = value
+
+          outfile[:hpp] << "static const #{classname} #{name};\n"
+          outfile[:cpp] << "const #{classname} #{classname}::#{name}(#{value});\n"
+          outfile[:raw] << "#{classname}::#{name} #{value}\n"
+        else
+          unless l.strip.empty? then
+            outfile[:hpp] << "ERROR #{l}\n"
+            outfile[:cpp] << "ERROR #{l}\n"
           end
         end
       end
+    end
+
+    outfile.each do |name,file|
+      file.close
     end
   end
 end
