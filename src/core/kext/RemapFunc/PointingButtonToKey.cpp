a708dd1c4b72150e4d7a7af644532271de00305b
Takayama Fumihiko <tekezo@pqrs.org>
2010-10-08 00:14:22 +0900

    update RemapFunc/PointingButtonToKey to use BRIDGE_DATATYPE

--- src/core/kext/RemapFunc/PointingButtonToKey.cpp
+++ src/core/kext/RemapFunc/PointingButtonToKey.cpp
@@ -15,50 +15,62 @@ namespace org_pqrs_KeyRemap4MacBook {
     }
 
     void
-    PointingButtonToKey::add(PointingButton newval)
+    PointingButtonToKey::add(unsigned int datatype, unsigned int newval)
     {
-      switch (index_) {
-        case 0:
-          fromButton_.button = newval;
-          break;
-        default:
-          IOLOG_ERROR("Invalid PointingButtonToKey::add\n");
-          break;
-      }
-      ++index_;
-    }
+      switch (datatype) {
+        case BRIDGE_DATATYPE_POINTINGBUTTON:
+        {
+          switch (index_) {
+            case 0:
+              fromButton_.button = newval;
+              break;
+            default:
+              IOLOG_ERROR("Invalid PointingButtonToKey::add\n");
+              break;
+          }
+          ++index_;
 
-    void
-    PointingButtonToKey::add(KeyCode newval)
-    {
-      switch (index_) {
-        case 0:
-          IOLOG_ERROR("Invalid PointingButtonToKey::add\n");
           break;
+        }
 
-        case 1:
-          // pass-through (== no break)
-          keytokey_.add(KeyCode::VK_PSEUDO_KEY);
-          keytokey_.add(fromButton_.flags);
-        default:
-          keytokey_.add(newval);
-          break;
-      }
-      ++index_;
-    }
+        case BRIDGE_DATATYPE_KEYCODE:
+        {
+          switch (index_) {
+            case 0:
+              IOLOG_ERROR("Invalid PointingButtonToKey::add\n");
+              break;
 
-    void
-    PointingButtonToKey::add(Flags newval)
-    {
-      switch (index_) {
-        case 0:
-          IOLOG_ERROR("Invalid PointingButtonToKey::add\n");
+            case 1:
+              // pass-through (== no break)
+              keytokey_.add(KeyCode::VK_PSEUDO_KEY);
+              keytokey_.add(fromButton_.flags);
+            default:
+              keytokey_.add(KeyCode(newval));
+              break;
+          }
+          ++index_;
+
           break;
-        case 1:
-          fromButton_.flags = newval;
+        }
+
+        case BRIDGE_DATATYPE_FLAGS:
+        {
+          switch (index_) {
+            case 0:
+              IOLOG_ERROR("Invalid PointingButtonToKey::add\n");
+              break;
+            case 1:
+              fromButton_.flags = newval;
+              break;
+            default:
+              keytokey_.add(Flags(newval));
+              break;
+          }
           break;
+        }
+
         default:
-          keytokey_.add(newval);
+          IOLOG_ERROR("PointingButtonToKey::add invalid datatype:%d\n", datatype);
           break;
       }
     }
