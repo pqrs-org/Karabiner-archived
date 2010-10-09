c66f97a172366d3771a325ed70273381cef571f8
Takayama Fumihiko <tekezo@pqrs.org>
2010-10-10 07:20:02 +0900

    Updated kext/RemapFilter/ConfigFilter

--- src/core/kext/RemapFilter/ConfigFilter.cpp
+++ src/core/kext/RemapFilter/ConfigFilter.cpp
@@ -1,11 +1,62 @@
+#include "bridge.h"
 #include "ConfigFilter.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFilter {
+    ConfigFilter::ConfigFilter(unsigned int t) : type_(t)
+    {
+      targets_ = new Vector_ConfigPointer();
+    }
+
+    ConfigFilter::~ConfigFilter(void)
+    {
+      if (targets_) {
+        delete targets_;
+      }
+    }
+
     bool
     ConfigFilter::isblocked(void)
     {
-      return true;
+      if (! targets_) return false;
+
+      switch (type_) {
+        case BRIDGE_FILTERTYPE_CONFIG_NOT:
+        {
+          for (size_t i = 0; i < targets_->size(); ++i) {
+            unsigned int* p = (*targets_)[i].pointer;
+            if (p && *p) {
+              return true;
+            }
+          }
+          return false;
+        }
+
+        case BRIDGE_FILTERTYPE_CONFIG_ONLY:
+        {
+          for (size_t i = 0; i < targets_->size(); ++i) {
+            unsigned int* p = (*targets_)[i].pointer;
+            if (p && *p) {
+              return false;
+            }
+          }
+          return true;
+        }
+
+        default:
+          IOLOG_ERROR("ConfigFilter::isblocked unknown type_(%d)\n", type_);
+          break;
+      }
+
+      return false;
+    }
+
+    void
+    ConfigFilter::add(unsigned int* newval)
+    {
+      if (! targets_) return;
+
+      targets_->push_back(newval);
     }
   }
 }
