330b85297e9843f5e639bc9f0c41ce8b97c2be1f
tekezo <devnull@localhost>
2010-10-09 13:08:38 +0900

    Updated RemapClass::Item

--- src/core/kext/RemapClass.cpp
+++ src/core/kext/RemapClass.cpp
@@ -30,6 +30,7 @@ namespace org_pqrs_KeyRemap4MacBook {
       IOLOG_ERROR("RemapClass::Item::initialize failed to allocate.\n"); \
       goto error;                                                        \
     } else {                                                             \
+      (POINTER)->initialize();                                           \
       for (size_t i = 1;; i += 2) {                                      \
         size_t datatype_index = i;                                       \
         size_t value_index    = i + 1;                                   \
