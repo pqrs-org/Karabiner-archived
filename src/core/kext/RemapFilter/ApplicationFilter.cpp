3fab53a0322b86010be42805e2c7fdd3b3abb8e3
tekezo <devnull@localhost>
2010-10-10 07:57:36 +0900

    Added kext/RemapFilter/ApplicationFilter

---
+++ src/core/kext/RemapFilter/ApplicationFilter.cpp
@@ -0,0 +1,65 @@
+#include "base.hpp"
+#include "bridge.h"
+#include "bridge.hpp"
+#include "CommonData.hpp"
+#include "ApplicationFilter.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  namespace RemapFilter {
+    ApplicationFilter::ApplicationFilter(unsigned int t) : type_(t)
+    {
+      targets_ = new Vector_FilterValue();
+    }
+
+    ApplicationFilter::~ApplicationFilter(void)
+    {
+      if (targets_) {
+        delete targets_;
+      }
+    }
+
+    void
+    ApplicationFilter::add(unsigned int newval)
+    {
+      if (! targets_) return;
+
+      targets_->push_back(newval);
+    }
+
+    bool
+    ApplicationFilter::isblocked(void)
+    {
+      if (! targets_) return false;
+
+      unsigned int current = CommonData::getcurrent_workspacedata().type;
+
+      switch (type_) {
+        case BRIDGE_FILTERTYPE_APPLICATION_NOT:
+        {
+          for (size_t i = 0; i < targets_->size(); ++i) {
+            if ((*targets_)[i].value == current) {
+              return true;
+            }
+          }
+          return false;
+        }
+
+        case BRIDGE_FILTERTYPE_APPLICATION_ONLY:
+        {
+          for (size_t i = 0; i < targets_->size(); ++i) {
+            if ((*targets_)[i].value == current) {
+              return false;
+            }
+          }
+          return true;
+        }
+
+        default:
+          IOLOG_ERROR("ApplicationFilter::isblocked unknown type_(%d)\n", type_);
+          break;
+      }
+
+      return false;
+    }
+  }
+}
