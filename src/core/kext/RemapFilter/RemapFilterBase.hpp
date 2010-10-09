a7f3307eb2e26a462b3c70c5828f13be9d2fdbe8
Takayama Fumihiko <tekezo@pqrs.org>
2010-10-10 07:34:41 +0900

    Updated kext/RemapFilter/ConfigFilter

--- src/core/kext/RemapFilter/RemapFilterBase.hpp
+++ src/core/kext/RemapFilter/RemapFilterBase.hpp
@@ -7,9 +7,9 @@ namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFilter {
     struct ConfigPointer {
       ConfigPointer(void) : pointer(NULL) {}
-      ConfigPointer(unsigned int* p) : pointer(p) {}
+      ConfigPointer(int* p) : pointer(p) {}
 
-      unsigned int* pointer;
+      int* pointer;
     };
     // XXX: For ConfigFilter. Remove me.
     DECLARE_VECTOR(ConfigPointer);
