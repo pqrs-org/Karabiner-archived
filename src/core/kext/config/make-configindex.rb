b3422b67b912cbeda752f56cb4737f7db30bd815
tekezo <devnull@localhost>
2010-10-13 22:55:18 +0900

    Added kext/keycode/data/ConfigIndex.data

---
+++ src/core/kext/config/make-configindex.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+
+$outfile = {
+  :configindex => open('../keycode/data/include/ConfigIndex.GENERATED.data', 'w'),
+}
+
+# ======================================================================
+require 'rubygems'
+require 'xml/libxml'
+require 'inc.preprocess.rb'
+
+index = 0
+parser = XML::Parser.file('../../../../files/prefpane/output/checkbox.xml')
+libxmldoc = parser.parse
+
+# ----------------------------------------
+libxmldoc.root.find('//sysctl').each do |node|
+  name = node.children.map{|n| n.to_s}.join('')
+  name.gsub!(/\./, '_')
+
+  $outfile[:configindex] << "#{name} #{index}\n"
+  index += 1
+end
+
+# ======================================================================
+$outfile.each do |name,file|
+  file.close
+end
