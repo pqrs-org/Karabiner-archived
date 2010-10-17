2270472631f59ae05465368213fe50bbed9671fa
Takayama Fumihiko <tekezo@pqrs.org>
2010-10-17 16:25:41 +0900

    update kext/config

--- src/core/kext/config/inc.remapclass.rb
+++ src/core/kext/config/inc.remapclass.rb
@@ -5,13 +5,8 @@ require 'inc.filter.rb'
 require 'inc.keycode.rb'
 
 class RemapClass
-  @@entries = []
   @@simultaneous_keycode_index = 0
   @@variable_index = 0
-
-  def RemapClass.get_entries
-    return @@entries
-  end
 
   def RemapClass.reset_variable_index
     @@variable_index = 0
@@ -22,34 +17,16 @@ class RemapClass
 
     @code = {
       # functions
-      :initialize                   => '',
-      :remap_setkeyboardtype        => '',
-      :get_statusmessage            => '',
-    }
-
-    @@entries << {
-      :name => @name,
-      :initialize => [],
-      :terminate => [],
-      :remap_setkeyboardtype        => [],
-      :remap_key                    => [],
-      :remap_consumer               => [],
-      :remap_pointing               => [],
-      :remap_simultaneouskeypresses => [],
-      :remap_dropkeyafterremap      => [],
-      :get_statusmessage            => [],
-      :enabled                      => [],
+      :initialize_vector => [],
+      :setkeyboardtype   => nil,
+      :statusmessage     => 'NULL',
     }
   end
   attr_accessor :name, :filter, :code
 
-  def append_to_code_initialize(params, operation, filter)
-    # We split the initialize function per value.
-    # If a large number of values exist, the kernel stack is wasted when the monolithic initialize function is called.
-    # So, we split it.
-
+  def append_to_code_initialize_vector(params, operation, filtervec)
     args = []
-    args << "static_cast<unsigned int>(BRIDGE_REMAPTYPE_#{operation.upcase})"
+    args << "BRIDGE_REMAPTYPE_#{operation.upcase}"
 
     params.split(/,/).each do |p|
       datatype = nil
@@ -59,15 +36,15 @@ class RemapClass
         next if value.empty?
         newdatatype = nil
         if /^KeyCode::(.+)$/ =~ value then
-          newdatatype = 'static_cast<unsigned int>(BRIDGE_DATATYPE_KEYCODE)'
+          newdatatype = 'BRIDGE_DATATYPE_KEYCODE'
         elsif /^ModifierFlag::(.+)$/ =~ value then
-          newdatatype = 'static_cast<unsigned int>(BRIDGE_DATATYPE_FLAGS)'
+          newdatatype = 'BRIDGE_DATATYPE_FLAGS'
         elsif /^ConsumerKeyCode::(.+)$/ =~ value then
-          newdatatype = 'static_cast<unsigned int>(BRIDGE_DATATYPE_CONSUMERKEYCODE)'
+          newdatatype = 'BRIDGE_DATATYPE_CONSUMERKEYCODE'
         elsif /^PointingButton::(.+)$/ =~ value then
-          newdatatype = 'static_cast<unsigned int>(BRIDGE_DATATYPE_POINTINGBUTTON)'
+          newdatatype = 'BRIDGE_DATATYPE_POINTINGBUTTON'
         elsif /^Option::(.+)$/ =~ value then
-          newdatatype = 'static_cast<unsigned int>(BRIDGE_DATATYPE_OPTION)'
+          newdatatype = 'BRIDGE_DATATYPE_OPTION'
         else
           print "[ERROR] unknown datatype #{value}\n"
           throw :exit
@@ -88,43 +65,36 @@ class RemapClass
       args << newval.join('|')
     end
 
-    @code[:initialize] += "static const unsigned int #{@name}_vec#{@@variable_index}[] = { #{args.join(',')} };\n"
-    @code[:initialize] += "static const unsigned int #{@name}_filter#{@@variable_index}[] = { #{filter} };\n"
-  end
-  protected :append_to_code_initialize
+    @code[:initialize_vector] << args.count
+    @code[:initialize_vector] += args
 
-  def handle_autogen(autogen_node, filter)
+    unless filtervec.empty? then
+      @code[:initialize_vector] += filtervec
+    end
+  end
+  protected :append_to_code_initialize_vector
+
+  def handle_autogen(autogen_node, filtervec)
     if /^--(.+?)-- (.+)/ =~ autogen_node.inner_xml.strip then
       operation = $1
       params = $2
 
       case operation
-      when 'SetKeyboardType'
-        @code[:remap_setkeyboardtype] += "keyboardType = #{params}.get(); return;\n"
-
-      when 'DropKeyAfterRemap'
-        append_to_code_initialize(params, operation, filter)
-        @@variable_index += 1
-
       when 'ShowStatusMessage'
-        @code[:get_statusmessage] += "return #{params};\n"
+        @code[:statusmessage] = params
+
+      when 'SetKeyboardType'
+        @code[:setkeyboardtype] = KeyCode.v('KeyboardType', params)
 
       when 'SimultaneousKeyPresses'
         params = "KeyCode::VK_SIMULTANEOUSKEYPRESSES_#{@@simultaneous_keycode_index}, " + params
         @@simultaneous_keycode_index += 1
-        append_to_code_initialize(params, operation, filter)
+        append_to_code_initialize_vector(params, operation, filtervec)
         @@variable_index += 1
 
-      when 'KeyToKey', 'KeyToConsumer', 'KeyToPointingButton', 'DoublePressModifier', 'HoldingKeyToKey', 'IgnoreMultipleSameKeyPress', 'KeyOverlaidModifier'
-        append_to_code_initialize(params, operation, filter)
-        @@variable_index += 1
-
-      when 'ConsumerToConsumer', 'ConsumerToKey'
-        append_to_code_initialize(params, operation, filter)
-        @@variable_index += 1
-
-      when 'PointingButtonToPointingButton', 'PointingButtonToKey', 'PointingRelativeToScroll'
-        append_to_code_initialize(params, operation, filter)
+      when 'KeyToKey', 'KeyToConsumer', 'KeyToPointingButton', 'DoublePressModifier', 'HoldingKeyToKey', 'IgnoreMultipleSameKeyPress', 'KeyOverlaidModifier',
+        'ConsumerToConsumer', 'ConsumerToKey', 'PointingButtonToPointingButton', 'PointingButtonToKey', 'PointingRelativeToScroll', 'DropKeyAfterRemap'
+        append_to_code_initialize_vector(params, operation, filtervec)
         @@variable_index += 1
 
       else
@@ -134,178 +104,30 @@ class RemapClass
     end
   end
 
-  def empty?
-    @code.each do |k, v|
-      return false unless v.empty?
-    end
-    return true
-  end
-
-  def to_code(item_node)
+  def to_code(item_node, outfile)
     item_node.find('.//autogen').each do |autogen_node|
       filter = Filter.new
-      filter = filter.to_code(item_node, autogen_node)
-      handle_autogen(autogen_node, filter)
+      filtervec = filter.to_vector(item_node, autogen_node)
+      handle_autogen(autogen_node, filtervec)
     end
 
     # ----------------------------------------
-    return '' if empty?
+    outfile << "static const unsigned int* remapclass_#{@name}_initialize_vector[] = {\n"
+    outfile << "  BRIDGE_INITIALIZE_VECTOR_FORMAT_VERSION,\n"
+    outfile << "  #{@code[:initialize_vector].count},\n"
+    outfile << "  #{@code[:initialize_vector].join(',')}\n"
+    outfile << "};\n"
 
-    code = ''
+    outfile << "static const char* remapclass_#{@name}_statusmessage = #{@code[:statusmessage]};\n"
 
-    code += @code[:initialize]
-    if @@variable_index > 0 then
-      code += "static const unsigned int* #{@name}_initialize_vector[] = {\n"
-      @@variable_index.times do |i|
-        code += "  #{@name}_filter#{i}, #{@name}_vec#{i},\n"
-      end
-      code += "};\n"
-      code += "static const unsigned int #{@name}_initialize_size[] = {\n"
-      @@variable_index.times do |i|
-        code += "  sizeof(#{@name}_filter#{i}) / sizeof(#{@name}_filter#{i}[0]),\n"
-        code += "  sizeof(#{@name}_vec#{i}) / sizeof(#{@name}_vec#{i}[0]),\n"
-      end
-      code += "};\n"
-    end
-
-    classname = "RemapClass_#{@name}"
-
-    code += "class #{classname} {\n"
-    code += "public:\n"
-
-    # ----------------------------------------------------------------------
-    code += "static void initialize(void) {\n"
-    if @@variable_index > 0 then
-      code += "  for (size_t i = 0; i < sizeof(value_) / sizeof(value_[0]); ++i) {\n"
-      code += "    value_[i].initialize_filter(#{@name}_initialize_vector[i * 2], #{@name}_initialize_size[i * 2]);\n"
-      code += "    value_[i].initialize_remap(#{@name}_initialize_vector[i * 2 + 1], #{@name}_initialize_size[i * 2 + 1]);\n"
-      code += "  }\n"
-    end
-    code += "}\n"
-    @@entries[-1][:initialize] << "RemapClass_#{@name}::initialize"
-
-    code += "static void terminate(void) {\n"
-    if @@variable_index > 0 then
-      code += "  for (size_t i = 0; i < sizeof(value_) / sizeof(value_[0]); ++i) {\n"
-      code += "    value_[i].terminate();\n"
-      code += "  }\n"
-    end
-    code += "}\n"
-    @@entries[-1][:terminate] << "RemapClass_#{@name}::terminate"
-
-    # ----------------------------------------------------------------------
-    unless @code[:remap_setkeyboardtype].empty? then
-      code += "static void remap_setkeyboardtype(KeyboardType& keyboardType) {\n"
-      code += @code[:remap_setkeyboardtype]
-      code += "}\n"
-
-      @@entries[-1][:remap_setkeyboardtype] << "#{classname}::remap_setkeyboardtype"
+    if @code[:setkeyboardtype].nil? then
+      outfile << "static const bool remapclass_#{@name}_is_setkeyboardtype = false;\n"
+      outfile << "static const unsigned int remapclass_#{@name}_setkeyboardtype = 0;\n"
     else
-      @@entries[-1][:remap_setkeyboardtype] << "NULL"
+      outfile << "static const bool remapclass_#{@name}_is_setkeyboardtype = true;\n"
+      outfile << "static const unsigned int remapclass_#{@name}_setkeyboardtype = #{@code[:setkeyboardtype]};\n"
     end
 
-    # ----------------------------------------------------------------------
-    if @@variable_index > 0 then
-      code += "static void remap_key(RemapParams& remapParams) {\n"
-      code += "  for (int i = 0; i < #{@@variable_index}; ++i) {\n"
-      code += "    if (value_[i].remap(remapParams)) return;\n"
-      code += "  }\n"
-      code += "}\n"
-
-      @@entries[-1][:remap_key] << "#{classname}::remap_key"
-    else
-      @@entries[-1][:remap_key] << "NULL"
-    end
-
-    # ----------------------------------------------------------------------
-    if @@variable_index > 0 then
-      code += "static void remap_consumer(RemapConsumerParams& remapParams) {\n"
-      code += "  for (int i = 0; i < #{@@variable_index}; ++i) {\n"
-      code += "    if (value_[i].remap(remapParams)) return;\n"
-      code += "  }\n"
-      code += "}\n"
-
-      @@entries[-1][:remap_consumer] << "#{classname}::remap_consumer"
-    else
-      @@entries[-1][:remap_consumer] << "NULL"
-    end
-
-    # ----------------------------------------------------------------------
-    if @@variable_index > 0 then
-      code += "static void remap_pointing(RemapPointingParams_relative& remapParams) {\n"
-      code += "  for (int i = 0; i < #{@@variable_index}; ++i) {\n"
-      code += "    if (value_[i].remap(remapParams)) return;\n"
-      code += "  }\n"
-      code += "}\n"
-
-      @@entries[-1][:remap_pointing] << "#{classname}::remap_pointing"
-    else
-      @@entries[-1][:remap_pointing] << "NULL"
-    end
-
-    # ----------------------------------------------------------------------
-    if @@variable_index > 0 then
-      code += "static void remap_simultaneouskeypresses(void) {\n"
-      code += "  for (int i = 0; i < #{@@variable_index}; ++i) {\n"
-      code += "    value_[i].remap_SimultaneousKeyPresses();\n"
-      code += "  }\n"
-      code += "}\n"
-
-      @@entries[-1][:remap_simultaneouskeypresses] << "#{classname}::remap_simultaneouskeypresses"
-    else
-      @@entries[-1][:remap_simultaneouskeypresses] << "NULL"
-    end
-
-    # ----------------------------------------------------------------------
-    unless @code[:get_statusmessage].empty? then
-      code += "static const char* get_statusmessage(void) {\n"
-      code += @code[:get_statusmessage]
-      code += "}\n"
-
-      @@entries[-1][:get_statusmessage] << "#{classname}::get_statusmessage"
-    else
-      @@entries[-1][:get_statusmessage] << "NULL"
-    end
-
-    # ----------------------------------------------------------------------
-    if @@variable_index > 0 then
-      code += "static bool remap_dropkeyafterremap(const Params_KeyboardEventCallBack& params) {\n"
-      code += "  if (! enabled()) return false;\n"
-      code += "  for (int i = 0; i < #{@@variable_index}; ++i) {\n"
-      code += "    if (value_[i].drop(params)) return true;\n"
-      code += "  }\n"
-      code += "  return false;\n"
-      code += "}\n"
-
-      @@entries[-1][:remap_dropkeyafterremap] << "#{classname}::remap_dropkeyafterremap"
-    end
-
-    # ----------------------------------------------------------------------
-    code   += "static bool enabled(void) {\n"
-    if @name == 'notsave_passthrough' then
-      code += "return config.notsave_passthrough;\n"
-    elsif /^passthrough_/ =~ @name then
-      code += "return config.enabled_flags[#{KeyCode.ConfigIndex(@name)}];\n"
-    else
-      code += "return config.enabled_flags[#{KeyCode.ConfigIndex(@name)}] && ! config.notsave_passthrough;\n"
-    end
-    code   += "}\n"
-
-    @@entries[-1][:enabled] << "#{classname}::enabled"
-
-    # ----------------------------------------
-    code += "\n"
-    code += "private:\n"
-    if @@variable_index > 0 then
-      code += "static RemapClass::Item value_[#{@@variable_index}];\n"
-    end
-    code += "};\n"
-
-    if @@variable_index > 0 then
-      code += "RemapClass::Item #{classname}::value_[#{@@variable_index}];\n"
-    end
-    code += "\n\n"
-
-    code
+    outfile << "\n"
   end
 end
