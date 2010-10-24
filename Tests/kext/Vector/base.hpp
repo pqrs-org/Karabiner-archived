6983c20cb9ae1794767f9dcefb498b9484097d3f
Takayama Fumihiko <tekezo@pqrs.org>
2010-10-24 11:18:45 +0900

    add kext/util/Vector::reserve

---
+++ Tests/kext/Vector/base.hpp
@@ -0,0 +1,6 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+#include <stdio.h>
+
+#endif
--- src/core/kext/util/Vector.hpp
+++ src/core/kext/util/Vector.hpp
@@ -3,47 +3,59 @@
 
 #include "base.hpp"
 
-#define DECLARE_VECTOR(TYPENAME)                                      \
-  class Vector_ ## TYPENAME {                                         \
-  public:                                                             \
-    Vector_ ## TYPENAME(void) : vector_(NULL), size_(0) {}            \
-    ~Vector_ ## TYPENAME(void) {                                      \
-      if (vector_) {                                                  \
-        delete[] vector_;                                             \
-        vector_ = NULL;                                               \
-      }                                                               \
-    }                                                                 \
-                                                                      \
-    Vector_ ## TYPENAME & push_back(const TYPENAME &newval) {         \
-      TYPENAME* p = new TYPENAME[size_ + 1];                          \
-      if (p) {                                                        \
-        for (size_t i = 0; i < size_; ++i) {                          \
-          p[i] = vector_[i];                                          \
-        }                                                             \
-        p[size_] = newval;                                            \
-      }                                                               \
-      ++size_;                                                        \
-                                                                      \
-      if (vector_) {                                                  \
-        delete[] vector_;                                             \
-      }                                                               \
-                                                                      \
-      vector_ = p;                                                    \
-                                                                      \
-      return *this;                                                   \
-    }                                                                 \
-                                                                      \
-    size_t size(void) const { return size_; }                         \
-    bool empty(void) const { return size_ == 0; }                     \
-                                                                      \
-    TYPENAME & operator[](size_t n) { return vector_[n]; }            \
-    const TYPENAME& operator[](size_t n) const { return vector_[n]; } \
-    TYPENAME& front(void) { return vector_[0]; }                      \
-    TYPENAME& back(void) { return vector_[size_ - 1]; }               \
-                                                                      \
-  private:                                                            \
-    TYPENAME* vector_;                                                \
-    size_t size_;                                                     \
+#define DECLARE_VECTOR(TYPENAME)                                         \
+  class Vector_ ## TYPENAME {                                            \
+  public:                                                                \
+    Vector_ ## TYPENAME(void) : vector_(NULL), size_(0), capacity_(0) {} \
+    ~Vector_ ## TYPENAME(void) {                                         \
+      if (vector_) {                                                     \
+        delete[] vector_;                                                \
+      }                                                                  \
+    }                                                                    \
+                                                                         \
+    void reserve(size_t n) {                                             \
+      if (n <= capacity_) return;                                        \
+                                                                         \
+      TYPENAME* p = new TYPENAME[n];                                     \
+      if (p) {                                                           \
+        capacity_ = n;                                                   \
+        for (size_t i = 0; i < size_; ++i) {                             \
+          p[i] = vector_[i];                                             \
+        }                                                                \
+                                                                         \
+        if (vector_) {                                                   \
+          delete[] vector_;                                              \
+        }                                                                \
+        vector_ = p;                                                     \
+      }                                                                  \
+    }                                                                    \
+                                                                         \
+    Vector_ ## TYPENAME & push_back(const TYPENAME &newval) {            \
+      if (size_ >= capacity_) {                                          \
+        reserve(capacity_ + 1);                                          \
+      }                                                                  \
+                                                                         \
+      if (size_ < capacity_) {                                           \
+        vector_[size_] = newval;                                         \
+        ++size_;                                                         \
+      }                                                                  \
+                                                                         \
+      return *this;                                                      \
+    }                                                                    \
+                                                                         \
+    size_t size(void) const { return size_; }                            \
+    size_t capacity(void) const { return capacity_; }                    \
+    bool empty(void) const { return size_ == 0; }                        \
+                                                                         \
+    TYPENAME & operator[](size_t n) { return vector_[n]; }               \
+    const TYPENAME& operator[](size_t n) const { return vector_[n]; }    \
+    TYPENAME& front(void) { return vector_[0]; }                         \
+    TYPENAME& back(void) { return vector_[size_ - 1]; }                  \
+                                                                         \
+  private:                                                               \
+    TYPENAME* vector_;                                                   \
+    size_t size_;                                                        \
+    size_t capacity_;                                                    \
   };
 
 #endif
