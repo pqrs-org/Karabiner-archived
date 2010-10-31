b6c107f54c4eb859bd2695269e85079176a8fe53
tekezo <devnull@localhost>
2010-10-31 17:46:59 +0900

    update .hgignore

---
+++ src/core/bridge/keycode/output/include.EventType.hpp
@@ -0,0 +1,4 @@
+static const EventType DOWN;
+static const EventType UP;
+static const EventType MODIFY;
+static const EventType VK__AUTOINDEX__BEGIN__;
---
+++ src/core/bridge/keycode/output/include.Option.hpp
@@ -0,0 +1,3 @@
+static const Option KEYOVERLAIDMODIFIER_REPEAT;
+static const Option SIMULTANEOUSKEYPRESSES_RAW;
+static const Option VK__AUTOINDEX__BEGIN__;
