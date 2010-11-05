bf31177c6ac05e64fb99fa5d15466018d7fb7e73
tekezo <devnull@localhost>
2010-11-05 22:58:36 +0900

    Added server/WorkSpaceData

---
+++ src/core/server/WorkSpaceData.h
@@ -0,0 +1,8 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+#import <Cocoa/Cocoa.h>
+
+@interface WorkSpaceData
+
++ (unsigned int) getApplicationType:(NSDictionary*)dict name:(NSString*)name;
+
+@end
