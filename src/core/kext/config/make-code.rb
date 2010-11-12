b239e80ebc003338240235c9ef4be0a2eb75d596
tekezo <devnull@localhost>
2010-11-13 02:30:22 +0900

    update around vk_config

--- src/core/kext/config/make-code.rb
+++ src/core/kext/config/make-code.rb
@@ -28,10 +28,9 @@ ARGV.each do |xmlpath|
   # setup KeyCode
   KeyCode.setup_configindex(libxmldoc.root)
 
-  libxmldoc.root.find('//vk_config').each do |node|
-    list = node.parent.find('./sysctl')
-    throw :exit if list.length != 1
-    name = list[0].inner_xml.gsub(/\./, '_')
+  libxmldoc.root.find('//sysctl').each do |node|
+    next if node['vk_config'] != 'true'
+    name = node.inner_xml.gsub(/\./, '_')
 
     KeyCode.append_autoindexed('KeyCode', "VK_CONFIG_TOGGLE_#{name}")
     KeyCode.append_autoindexed('KeyCode', "VK_CONFIG_FORCE_ON_#{name}")
