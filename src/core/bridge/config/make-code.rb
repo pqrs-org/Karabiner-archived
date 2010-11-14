01401982741aa745a05ee3a87ea8e985b6a6ebba
tekezo <devnull@localhost>
2010-11-14 16:46:12 +0900

    update bridge/config

--- src/core/bridge/config/make-code.rb
+++ src/core/bridge/config/make-code.rb
@@ -6,11 +6,17 @@ require 'xml/libxml'
 GC.disable
 
 $outfile = {
-  :hpp => open('output/include.bridge_essential_config_index.hpp', 'w'),
-  :cpp => open('output/include.bridge_essential_config_index.cpp', 'w'),
+  :hpp   => open('output/include.bridge_essential_config_index.hpp', 'w'),
+  :cpp   => open('output/include.bridge_essential_config_index.cpp', 'w'),
+  :plist => open('output/include.bridge_essential_config_index.plist', 'w'),
 }
 
 configindex = 0
+
+$outfile[:plist].print '<?xml version="1.0" encoding="UTF-8"?>' + "\n"
+$outfile[:plist].print '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">' + "\n"
+$outfile[:plist].print '<plist version="1.0">' + "\n"
+$outfile[:plist].print '  <array>' + "\n"
 
 ARGV.each do |xmlpath|
   parser = XML::Parser.file(xmlpath)
@@ -31,12 +37,15 @@ ARGV.each do |xmlpath|
       end
 
       $outfile[:hpp].print "BRIDGE_ESSENTIAL_CONFIG_INDEX_#{name} = #{configindex},\n"
+      $outfile[:plist].print "    <string>#{name}</string>\n"
       configindex += 1
     end
   end
 end
 
 $outfile[:hpp].print "BRIDGE_ESSENTIAL_CONFIG_INDEX__END__ = #{configindex}\n"
+$outfile[:plist].print "  </array>\n"
+$outfile[:plist].print "</plist>\n"
 
 $outfile.each do |key,file|
   file.close
