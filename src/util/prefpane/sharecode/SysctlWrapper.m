c1ae8951a2eaa43142f359a553ad6d15d06e276b
tekezo <devnull@localhost>
2010-11-16 04:37:30 +0900

    update server

--- src/util/prefpane/sharecode/SysctlWrapper.m
+++ src/util/prefpane/sharecode/SysctlWrapper.m
@@ -37,7 +37,7 @@
   return [[[NSNumber alloc] initWithInt:value] autorelease];
 }
 
-+ (void) setSysctlInt:(NSString*)base name:(NSString*)name value:(NSNumber*)value sysctl_set:(NSString*)sysctl_set sysctl_ctl:(NSString*)sysctl_ctl
++ (void) setSysctlInt:(NSString*)base name:(NSString*)name value:(NSNumber*)value sysctl_set:(NSString*)sysctl_set
 {
   NSString* entry = [NSString stringWithFormat:@"%@.%@", base, name];
 
@@ -48,10 +48,6 @@
   NSArray* args_set = [NSArray arrayWithObjects:name, [value stringValue], nil];
   NSTask* task_set = [NSTask launchedTaskWithLaunchPath:sysctl_set arguments:args_set];
   [task_set waitUntilExit];
-
-  NSArray* args_save = [NSArray arrayWithObjects:@"save", nil];
-  NSTask* task_save = [NSTask launchedTaskWithLaunchPath:sysctl_ctl arguments:args_save];
-  [task_save waitUntilExit];
 }
 
 @end
