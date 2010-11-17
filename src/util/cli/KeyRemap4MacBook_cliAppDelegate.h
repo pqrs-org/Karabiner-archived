51a2492fefc1c1879b62f06b243c7c270c8d782f
tekezo <devnull@localhost>
2010-11-17 22:28:50 +0900

    update util/cli

--- src/util/cli/KeyRemap4MacBook_cliAppDelegate.h
+++ src/util/cli/KeyRemap4MacBook_cliAppDelegate.h
@@ -1,8 +1,10 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 #import <Cocoa/Cocoa.h>
+#import "PreferencesClient.h"
 
 @interface KeyRemap4MacBook_cliAppDelegate : NSObject <NSApplicationDelegate> {
   NSWindow* window;
+  IBOutlet org_pqrs_KeyRemap4MacBook_PreferencesClient* preferencesclient_;
 }
 
 @property (assign) IBOutlet NSWindow* window;
