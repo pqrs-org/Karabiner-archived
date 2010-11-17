180451655e629e8c0f2bf8429b1b20eb4b9fd67a
Takayama Fumihiko <tekezo@pqrs.org>
2010-11-17 21:44:12 +0900

    Added cli

---
+++ src/util/cli/KeyRemap4MacBook_cliAppDelegate.h
@@ -0,0 +1,10 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+#import <Cocoa/Cocoa.h>
+
+@interface KeyRemap4MacBook_cliAppDelegate : NSObject <NSApplicationDelegate> {
+  NSWindow* window;
+}
+
+@property (assign) IBOutlet NSWindow* window;
+
+@end
---
+++ src/util/cli/KeyRemap4MacBook_cliAppDelegate.m
@@ -0,0 +1,10 @@
+#import "KeyRemap4MacBook_cliAppDelegate.h"
+
+@implementation KeyRemap4MacBook_cliAppDelegate
+
+@synthesize window;
+
+- (void) applicationDidFinishLaunching:(NSNotification*)aNotification {
+}
+
+@end
