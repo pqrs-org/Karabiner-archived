0064d785680ea2997b9f4e058e60d4d221fce2bd
Takayama Fumihiko <tekezo@pqrs.org>
2010-11-19 22:15:58 +0900

    update server

--- src/core/server/Classes/ConfigXMLParser.m
+++ src/core/server/Classes/ConfigXMLParser.m
@@ -118,26 +118,4 @@
   return name;
 }
 
-// ======================================================================
-- (NSString*) get_private_xml_path
-{
-  NSFileManager* filemanager = [NSFileManager defaultManager];
-  NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
-  NSString* path = [paths objectAtIndex:0];
-  path = [path stringByAppendingPathComponent:@"KeyRemap4MacBook"];
-  if (! [filemanager fileExistsAtPath:path]) {
-    [filemanager createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:NULL];
-  }
-  if ([filemanager fileExistsAtPath:path]) {
-    path = [path stringByAppendingPathComponent:@"private.xml"];
-  }
-
-  BOOL isDirectory;
-  if ([filemanager fileExistsAtPath:path isDirectory:&isDirectory] && ! isDirectory) {
-    return path;
-  }
-
-  return @"";
-}
-
 @end
