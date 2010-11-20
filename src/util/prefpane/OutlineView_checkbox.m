99c510e42e1b7b0bd5698c9a964e89df3842d5e4
Takayama Fumihiko <tekezo@pqrs.org>
2010-11-20 22:56:34 +0900

    update prefpane

--- src/util/prefpane/OutlineView_checkbox.m
+++ src/util/prefpane/OutlineView_checkbox.m
@@ -4,30 +4,13 @@
 
 @implementation org_pqrs_KeyRemap4MacBook_OutlineView_checkbox
 
-- (void) dealloc
+- (id) init
 {
-  if (datasource_) {
-    [datasource_ release];
-  }
+  [super init];
 
-  [super dealloc];
-}
+  ischeckbox_ = YES;
 
-- (void) load:(BOOL)force
-{
-  if (force) {
-    if (datasource_) {
-      [datasource_ release];
-      datasource_ = nil;
-    }
-  }
-
-  if (datasource_) return;
-
-  datasource_ = [[preferencesclient_ proxy] preferencepane_checkbox];
-  if (datasource_) {
-    [datasource_ retain];
-  }
+  return self;
 }
 
 - (IBAction) reloadXML:(id)sender
@@ -47,7 +30,7 @@
   [self load:YES];
 
   // ----------------------------------------
-  [outlineview_checkbox_ reloadData];
+  [outlineview_ reloadData];
 }
 
 /* ------------------------------------------------------------ */
@@ -157,104 +140,6 @@
     [_outlineView_checkbox expandItem:nil expandChildren:YES];
   }
 #endif
-}
-
-/* ---------------------------------------------------------------------- */
-- (NSUInteger) outlineView:(NSOutlineView*)outlineView numberOfChildrenOfItem:(id)item
-{
-  NSArray* a = nil;
-
-  // root object
-  if (! item) {
-    [self load:NO];
-    a = datasource_;
-
-  } else {
-    a = [item objectForKey:@"children"];
-  }
-
-  if (! a) return 0;
-  return [a count];
-}
-
-- (id) outlineView:(NSOutlineView*)outlineView child:(NSUInteger)idx ofItem:(id)item
-{
-  NSArray* a = nil;
-
-  // root object
-  if (! item) {
-    [self load:NO];
-    a = datasource_;
-
-  } else {
-    a = [item objectForKey:@"children"];
-  }
-
-  if (! a) return nil;
-  if (idx >= [a count]) return nil;
-  return [a objectAtIndex:idx];
-}
-
-- (BOOL) outlineView:(NSOutlineView*)outlineView isItemExpandable:(id)item
-{
-  NSArray* a = [item objectForKey:@"children"];
-  return a ? YES : NO;
-}
-
-- (id) outlineView:(NSOutlineView*)outlineView objectValueForTableColumn:(NSTableColumn*)tableColumn byItem:(id)item
-{
-  NSButtonCell* cell = [tableColumn dataCell];
-  if (! cell) return nil;
-
-  [cell setTitle:[item objectForKey:@"name"]];
-  NSString* identifier = [item objectForKey:@"identifier"];
-
-  if (! identifier || [identifier hasPrefix:@"notsave."]) {
-    [cell setImagePosition:NSNoImage];
-    return nil;
-
-  } else {
-    [cell setImagePosition:NSImageLeft];
-    return [NSNumber numberWithInt:[[preferencesclient_ proxy] value:identifier]];
-  }
-
-  return nil;
-}
-
-- (CGFloat) outlineView:(NSOutlineView*)outlineView heightOfRowByItem:(id)item
-{
-  NSNumber* number = [item objectForKey:@"cached_height"];
-  if (number) return [number floatValue];
-
-  number = [item objectForKey:@"height"];
-  if (! number) {
-    number = [NSNumber numberWithFloat:0];
-  } else {
-    number = [NSNumber numberWithDouble:([number intValue] * (CGFloat)([outlineView rowHeight]))];
-  }
-  return [number floatValue];
-}
-
-- (void) outlineView:(NSOutlineView*)outlineView setObjectValue:(id)object forTableColumn:(NSTableColumn*)tableColumn byItem:(id)item
-{
-  NSString* identifier = [item objectForKey:@"identifier"];
-
-  if (identifier) {
-    if (! [identifier hasPrefix:@"notsave."]) {
-      int value = [[preferencesclient_ proxy] value:identifier];
-      value = ! value;
-      [[preferencesclient_ proxy] setValueForName:value forName:identifier];
-    }
-  } else {
-    // expand/collapse tree
-    if ([outlineView isExpandable:item]) {
-      if ([outlineView isItemExpanded:item]) {
-        [outlineView collapseItem:item];
-      } else {
-        [outlineView expandItem:item];
-      }
-    }
-  }
 }
 
 @end
