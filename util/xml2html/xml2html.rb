e3173c4c9e8ff8fdf3bc58cdf4b01b0f5446fc57
tekezo <devnull@localhost>
2010-11-20 13:16:35 +0900

    update util/xml2html/xml2html.rb

--- util/xml2html/xml2html.rb
+++ util/xml2html/xml2html.rb
@@ -1,41 +1,30 @@
 #!/usr/bin/ruby
 
-require 'cgi'
+require 'rubygems'
+require 'xml/libxml'
 
-lines = []
-if ARGV[0] == '-' then
-  lines = $stdin.readlines
-else
-  system('make -C ../../files/prefpane > /dev/null')
-  file = "%s/../../files/prefpane/output/checkbox.xml" % File.dirname($0)
-  open(file) do |f|
-    lines = f.readlines
-  end
-end
+system('make -C ../../files/prefpane > /dev/null')
+file = "%s/../../files/prefpane/output/checkbox.xml" % File.dirname($0)
+parser = XML::Parser.file(file)
+libxmldoc = parser.parse
 
-total = 0
-lines.each do |l|
-  if /<list>/ =~ l then
-    print "<ul>"
-  end
-  if /<\/list>/ =~ l then
-    print "</ul>"
-  end
-  if /<item>/ =~ l then
+libxmldoc.find('//list').each do |node_list|
+  print "<ul>"
+  node_list.find('./item').each do |node_item|
     print "<li>"
-  end
-  if /<\/item>/ =~ l then
+    node_item.find('./name').each do |node_name|
+      print node_name.inner_xml
+      print "<br/>"
+    end
+    node_item.find('./appendix').each do |node_appendix|
+      print "  #{node_appendix.inner_xml}<br/>"
+    end
     print "</li>"
   end
-  if /<name>(.+)<\/name>/ =~ l then
-    print $1
-  end
-  if /<appendix>(.+)<\/appendix>/ =~ l then
-    print "<br />", $1
-  end
-  if /<identifier.*?>/ =~ l then
-    total += 1
-  end
+  print "</ul>\n"
 end
+
 print "\n\n"
+
+total = libxmldoc.find('//identifier').count
 print "total: #{total}\n"
