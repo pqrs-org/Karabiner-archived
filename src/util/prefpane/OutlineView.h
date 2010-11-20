b56cb17d220958937ab03e1d2ca0f44fb5939406
Takayama Fumihiko <tekezo@pqrs.org>
2010-11-20 21:40:49 +0900

    update prefpane

--- src/util/prefpane/OutlineView.h
+++ src/util/prefpane/OutlineView.h
@@ -2,13 +2,11 @@
 #import <Cocoa/Cocoa.h>
 #import "PreferencesClient.h"
 
-@interface org_pqrs_KeyRemap4MacBook_OutlineViewDataSource : NSObject {
-  SEL selector_;
+@interface org_pqrs_KeyRemap4MacBook_OutlineView : NSObject {
   NSArray* datasource_;
+  BOOL ischeckbox_;
 
   IBOutlet org_pqrs_KeyRemap4MacBook_PreferencesClient* preferencesclient_;
 }
-
-- (void) load:(BOOL)force;
 
 @end
--- src/util/prefpane/OutlineView_number.m
+++ src/util/prefpane/OutlineView_number.m
@@ -4,99 +4,13 @@
 
 @implementation org_pqrs_KeyRemap4MacBook_OutlineView_number
 
-- (void) dealloc
+- (id) init
 {
-  if (datasource_) {
-    [datasource_ release];
-  }
+  [super init];
 
-  [super dealloc];
-}
+  ischeckbox_ = NO;
 
-- (void) load:(BOOL)force
-{
-  if (force) {
-    if (datasource_) {
-      [datasource_ release];
-      datasource_ = nil;
-    }
-  }
-
-  if (datasource_) return;
-
-  datasource_ = [[preferencesclient_ proxy] preferencepane_number];
-  if (datasource_) {
-    [datasource_ retain];
-  }
-}
-
-/* ------------------------------------------------------------ */
-- (NSUInteger) outlineView:(NSOutlineView*)outlineView numberOfChildrenOfItem:(id)item
-{
-  NSArray* a = nil;
-
-  // root object
-  if (! item) {
-    [self load:NO];
-    a = datasource_;
-
-  } else {
-    a = [item objectForKey:@"children"];
-  }
-
-  if (! a) return 0;
-  return [a count];
-}
-
-- (id) outlineView:(NSOutlineView*)outlineView child:(NSUInteger)idx ofItem:(id)item
-{
-  NSArray* a = nil;
-
-  // root object
-  if (! item) {
-    [self load:NO];
-    a = datasource_;
-
-  } else {
-    a = [item objectForKey:@"children"];
-  }
-
-  if (! a) return nil;
-  if (idx >= [a count]) return nil;
-  return [a objectAtIndex:idx];
-}
-
-- (BOOL) outlineView:(NSOutlineView*)outlineView isItemExpandable:(id)item
-{
-  NSArray* a = [item objectForKey:@"children"];
-  return a ? YES : NO;
-}
-
-- (id) outlineView:(NSOutlineView*)outlineView objectValueForTableColumn:(NSTableColumn*)tableColumn byItem:(id)item
-{
-  id columnIdentifier = [tableColumn identifier];
-
-  if ([columnIdentifier isEqualToString:@"name"] ||
-      [columnIdentifier isEqualToString:@"baseunit"] ||
-      [columnIdentifier isEqualToString:@"default"]) {
-    return [item objectForKey:columnIdentifier];
-
-  } else if ([columnIdentifier isEqualToString:@"value"]) {
-    NSString* identifier = [item objectForKey:@"identifier"];
-    if (! identifier) return nil;
-
-    return [NSNumber numberWithInt:[[preferencesclient_ proxy] value:identifier]];
-  }
-
-  return nil;
-}
-
-- (void) outlineView:(NSOutlineView*)outlineView setObjectValue:(id)object forTableColumn:(NSTableColumn*)tableColumn byItem:(id)item
-{
-  NSString* identifier = [item objectForKey:@"identifier"];
-  if (! identifier) return;
-
-  [[preferencesclient_ proxy] setValueForName:[object intValue] forName:identifier];
+  return self;
 }
 
 @end
