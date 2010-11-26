a17da471ddfec2011ab6813e1292dbb213c13456
tekezo <devnull@localhost>
2010-11-27 07:07:49 +0900

    fix make-source.sh

--- make-source.sh
+++ make-source.sh
@@ -13,7 +13,7 @@ rm -rf $workdir/.hg*
 find $workdir -name '.DS_Store' -print0 | xargs -0 rm -f
 
 # archive
-(cd $tmpdir; zip -r -9 $name.zip $name)
+(cd $tmpdir; zip -r -9 --symlinks $name.zip $name)
 mv $tmpdir/$name.zip .
 chmod 644 $name.zip
 
