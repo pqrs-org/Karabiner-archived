bde573e62ae012e95957d8a318a49bddc0bd2043
tekezo <devnull@localhost>
2010-12-24 12:11:16 +0900

    Added server/Classes/StatusWindow

---
+++ src/core/server/Classes/StatusWindow.h
@@ -0,0 +1,17 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+#import <Cocoa/Cocoa.h>
+#import "PreferencesManager.h"
+
+@interface StatusWindow : NSObject {
+  IBOutlet NSWindow* statuswindow_;
+  IBOutlet NSTextField* label_;
+  IBOutlet NSImageView* backgroud_;
+  IBOutlet PreferencesManager* preferencesmanager_;
+
+  NSMutableArray* lines_;
+}
+
+- (void) resetStatusMessage;
+- (void) setStatusMessage:(NSUInteger)lineIndex message:(NSString*)message;
+
+@end
