21dd162c6b721d21125030d4f547552aba6c2fe6
tekezo <devnull@localhost>
2010-12-25 01:04:20 +0900

    cleanup KeyDump

--- src/util/KeyDump/KeyDumpAppDelegate.m
+++ src/util/KeyDump/KeyDumpAppDelegate.m
@@ -7,44 +7,34 @@
 
 #import "KeyDumpAppDelegate.h"
 #import <Carbon/Carbon.h>
-#import "server_objc_part.h"
 
 @implementation KeyDumpAppDelegate
 
 @synthesize window;
 
 // ------------------------------------------------------------
-- (void) observer_NSWorkspaceDidActivateApplicationNotification:(NSNotification*)notification
+- (void) observer_applicationChanged:(NSNotification*)notification
 {
-  NSString* name = [serverobjcpart_ getActiveApplicationName];
-  [otherinformationstore_ setApplicationName:name];
+  [otherinformationstore_ setApplicationName:[[notification userInfo] objectForKey:@"name"]];
 }
 
 // ------------------------------------------------------------
-- (void) observer_kTISNotifySelectedKeyboardInputSourceChanged:(NSNotification*)notification
+- (void) observer_inputSourceChanged:(NSNotification*)notification
 {
-  NSString* name = [serverobjcpart_ getTISPropertyInputModeID];
-  [otherinformationstore_ setInputSourceName:name];
+  [otherinformationstore_ setInputSourceName:[[notification userInfo] objectForKey:@"name"]];
 }
 
 // ------------------------------------------------------------
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification {
   [window makeFirstResponder:keyResponder_];
 
-  [[[NSWorkspace sharedWorkspace] notificationCenter] addObserver:self
-                                                         selector:@selector(observer_NSWorkspaceDidActivateApplicationNotification:)
-                                                             name:NSWorkspaceDidActivateApplicationNotification
-                                                           object:nil];
-
-  [[NSDistributedNotificationCenter defaultCenter] addObserver:self
-                                                      selector:@selector(observer_kTISNotifySelectedKeyboardInputSourceChanged:)
-                                                          name:(NSString*)(kTISNotifySelectedKeyboardInputSourceChanged)
-                                                        object:nil];
-
   [otherinformationstore_ setVersion];
   [otherinformationstore_ setApplicationName:nil];
+  [otherinformationstore_ setInputSourceName:nil];
 
-  [self observer_kTISNotifySelectedKeyboardInputSourceChanged:nil];
+  NSString* observedObject = @"org.pqrs.KeyRemap4MacBook.KeyDump";
+  [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(observer_applicationChanged:) name:@"applicationChanged" object:observedObject];
+  [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(observer_inputSourceChanged:) name:@"inputSourceChanged" object:observedObject];
 }
 
 - (BOOL) applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)theApplication {
