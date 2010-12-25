9b9c30649ee49912ae09e46cf5cc18483d642ab0
Takayama Fumihiko <tekezo@pqrs.org>
2010-12-26 00:35:30 +0900

    cleanup cli

--- src/util/cli/KeyRemap4MacBook_cliAppDelegate.h
+++ src/util/cli/KeyRemap4MacBook_cliAppDelegate.h
@@ -1,10 +1,10 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 #import <Cocoa/Cocoa.h>
-#import "PreferencesClient.h"
+#import "KeyRemap4MacBookClient.h"
 
 @interface KeyRemap4MacBook_cliAppDelegate : NSObject <NSApplicationDelegate> {
   NSWindow* window;
-  IBOutlet org_pqrs_KeyRemap4MacBook_PreferencesClient* preferencesclient_;
+  IBOutlet org_pqrs_KeyRemap4MacBook_Client* client_;
 }
 
 @property (assign) IBOutlet NSWindow* window;
