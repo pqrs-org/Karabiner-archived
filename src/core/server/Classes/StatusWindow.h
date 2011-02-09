485761428fe48d7ca524c5d4c15ccc80eb456064
Takayama Fumihiko <tekezo@pqrs.org>
2011-02-10 00:31:37 +0900

    Cleanup server around StatusWindow

--- src/core/server/Classes/StatusWindow.h
+++ src/core/server/Classes/StatusWindow.h
@@ -4,27 +4,13 @@
 #import "PreferencesManager.h"
 
 @interface StatusWindow : NSObject <GrowlApplicationBridgeDelegate> {
-  IBOutlet NSWindow* statuswindow_;
-  IBOutlet NSTextField* label_;
-  IBOutlet NSImageView* backgroud_;
   IBOutlet PreferencesManager* preferencesmanager_;
 
   NSMutableArray* lines_;
-  NSViewAnimation* animation_;
   NSString* messageForGrowl_;
-
-  int last_parameter_statuswindow_alpha_font_;
-  int last_parameter_statuswindow_alpha_background_;
-  int last_parameter_statuswindow_posx_adjustment_;
-  int last_parameter_statuswindow_posy_adjustment_;
-  int last_parameter_statuswindow_fadeout_duration_;
 }
-
-- (void) setupStatusWindow;
 
 - (void) resetStatusMessage;
 - (void) setStatusMessage:(NSUInteger)lineIndex message:(NSString*)message;
-
-- (void) refreshWindowPosition:(BOOL)force;
 
 @end
