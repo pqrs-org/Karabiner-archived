4ca82d58047fc5491d1c9e9070de41269274ab2b
Takayama Fumihiko <tekezo@pqrs.org>
2011-02-10 01:52:45 +0900

    change Growl.framework to Growl-WithInstaller.framework

--- src/core/server/Classes/StatusWindow.h
+++ src/core/server/Classes/StatusWindow.h
@@ -1,6 +1,6 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 #import <Cocoa/Cocoa.h>
-#import <Growl/Growl.h>
+#import <Growl-WithInstaller/Growl.h>
 #import "PreferencesManager.h"
 
 @interface StatusWindow : NSObject <GrowlApplicationBridgeDelegate> {
