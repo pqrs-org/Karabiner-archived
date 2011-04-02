b5ea8462d0b18f6db3b93d4505cc2ae1e95ae948
Takayama Fumihiko <tekezo@pqrs.org>
2011-04-02 17:01:24 +0900

    Update kext/Client

--- src/core/kext/Client.cpp
+++ src/core/kext/Client.cpp
@@ -121,16 +121,6 @@ namespace org_pqrs_KeyRemap4MacBook {
 
     IOLockWrapper::ScopedLock lk(lock_);
 
-    if (type == KeyRemap4MacBook_bridge::REQUEST_STATUS_MESSAGE) {
-      if (Config::get_essential_config(BRIDGE_ESSENTIAL_CONFIG_INDEX_general_hide_statusmessage)) {
-        if (! request) return 0;
-        char* p = reinterpret_cast<KeyRemap4MacBook_bridge::StatusMessage::Request*>(request)->message;
-        if (p[0] != '\0') {
-          return 0;
-        }
-      }
-    }
-
     // ------------------------------------------------------------
     int result = 0;
     int error = 0;
