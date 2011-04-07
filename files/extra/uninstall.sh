de72a71e1ef3e68d618e76900465bd9887ab21d4
Takayama Fumihiko <tekezo@pqrs.org>
2011-04-08 00:19:27 +0900

    udpate uninstall.sh

--- files/extra/uninstall.sh
+++ files/extra/uninstall.sh
@@ -2,6 +2,10 @@
 
 /Library/org.pqrs/KeyRemap4MacBook/extra/uninstall_core.sh
 
+# remove receipts
 rm -f /var/db/receipts/org.pqrs.driver.KeyRemap4MacBook.*
+
+# kill processes
+/usr/bin/killall KeyRemap4MacBook_server 2>/dev/null
 
 exit 0
