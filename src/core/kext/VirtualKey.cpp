93b78542fbe13b7bf2bc931c117c1c0ba576051b
Takayama Fumihiko <tekezo@pqrs.org>
2011-06-13 23:59:40 +0900

    Added kext/VirtualKey/VK_TEMPORARY (split from VirtualKey.cpp)

--- src/core/kext/VirtualKey.cpp
+++ src/core/kext/VirtualKey.cpp
@@ -10,7 +10,7 @@ namespace org_pqrs_KeyRemap4MacBook {
   VirtualKey::initialize(IOWorkLoop& workloop)
   {
     VirtualKey::VK_MOUSEKEY::initialize(workloop);
-    Handle_VK_JIS_TEMPORARY::initialize(workloop);
+    VirtualKey::VK_JIS_TEMPORARY::initialize(workloop);
 
     VirtualKey::VK_CONFIG::initialize();
   }
@@ -19,7 +19,7 @@ namespace org_pqrs_KeyRemap4MacBook {
   VirtualKey::terminate(void)
   {
     VirtualKey::VK_MOUSEKEY::terminate();
-    Handle_VK_JIS_TEMPORARY::terminate();
+    VirtualKey::VK_JIS_TEMPORARY::terminate();
 
     VirtualKey::VK_CONFIG::terminate();
   }
@@ -38,155 +38,4 @@ namespace org_pqrs_KeyRemap4MacBook {
     if (VirtualKey::VK_MOUSEKEY::is_VK_MOUSEKEY(keycode)) return true;
     return false;
   }
-
-  // ----------------------------------------------------------------------
-  void
-  Handle_VK_JIS_TEMPORARY::initialize(IOWorkLoop& workloop)
-  {
-    fire_timer_.initialize(&workloop, NULL, Handle_VK_JIS_TEMPORARY::fire_timer_callback);
-    fireKeyInfo_.active = false;
-  }
-
-  void
-  Handle_VK_JIS_TEMPORARY::terminate(void)
-  {
-    fire_timer_.terminate();
-  }
-
-  bool
-  Handle_VK_JIS_TEMPORARY::handle_core(const Params_KeyboardEventCallBack& params,
-                                       KeyCode key,
-                                       InputModeDetail inputmodedetail)
-  {
-    if (params.key != key) return false;
-
-    if (params.ex_iskeydown) {
-      if (savedinputmodedetail_ == InputModeDetail::NONE) {
-        savedinputmodedetail_ = CommonData::getcurrent_workspacedata().inputmodedetail;
-        currentinputmodedetail_ = CommonData::getcurrent_workspacedata().inputmodedetail;
-      }
-      firekeytoinputdetail(params, inputmodedetail);
-    }
-
-    return true;
-  }
-
-  bool
-  Handle_VK_JIS_TEMPORARY::handle_RESTORE(const Params_KeyboardEventCallBack& params)
-  {
-    if (params.key != KeyCode::VK_JIS_TEMPORARY_RESTORE) return false;
-
-    if (params.ex_iskeydown) {
-      if (savedinputmodedetail_ != InputModeDetail::NONE) {
-        firekeytoinputdetail(params, savedinputmodedetail_);
-        savedinputmodedetail_ = InputModeDetail::NONE;
-        currentinputmodedetail_ = InputModeDetail::NONE;
-      }
-    }
-
-    return true;
-  }
-
-  bool
-  Handle_VK_JIS_TEMPORARY::handle(const Params_KeyboardEventCallBack& params)
-  {
-    // ------------------------------------------------------------
-    if (handle_core(params,
-                    KeyCode::VK_JIS_TEMPORARY_ROMAN,
-                    InputModeDetail::ROMAN)) return true;
-
-    if (handle_core(params,
-                    KeyCode::VK_JIS_TEMPORARY_HIRAGANA,
-                    InputModeDetail::JAPANESE_HIRAGANA)) return true;
-
-    if (handle_core(params,
-                    KeyCode::VK_JIS_TEMPORARY_KATAKANA,
-                    InputModeDetail::JAPANESE_KATAKANA)) return true;
-
-    // OK, Ainu is not Japanese.
-    // But the input source of Ainu is Kotoeri, we need to handle it here.
-    if (handle_core(params,
-                    KeyCode::VK_JIS_TEMPORARY_AINU,
-                    InputModeDetail::AINU)) return true;
-
-    // ------------------------------------------------------------
-    if (handle_RESTORE(params)) return true;
-
-    // ------------------------------------------------------------
-    // flash keyevent
-    if (fireKeyInfo_.active) {
-      fire_timer_.cancelTimeout();
-      fire();
-    }
-
-    return false;
-  }
-
-  void
-  Handle_VK_JIS_TEMPORARY::firekeytoinputdetail(const Params_KeyboardEventCallBack& params,
-                                                InputModeDetail inputmodedetail)
-  {
-    inputmodedetail = normalize(inputmodedetail);
-    currentinputmodedetail_ = normalize(currentinputmodedetail_);
-
-    // ------------------------------------------------------------
-    if (inputmodedetail == currentinputmodedetail_) return;
-    if (inputmodedetail == InputModeDetail::NONE) return;
-
-    currentinputmodedetail_ = inputmodedetail;
-
-    // ------------------------------------------------------------
-    if (inputmodedetail == InputModeDetail::ROMAN) {
-      fireKeyInfo_.flags = Flags(0);
-      fireKeyInfo_.key = KeyCode::JIS_EISUU;
-
-    } else if (inputmodedetail == InputModeDetail::JAPANESE_HIRAGANA) {
-      fireKeyInfo_.flags = Flags(0);
-      fireKeyInfo_.key = KeyCode::JIS_KANA;
-
-    } else if (inputmodedetail == InputModeDetail::JAPANESE_KATAKANA) {
-      fireKeyInfo_.flags = ModifierFlag::SHIFT_L;
-      fireKeyInfo_.key = KeyCode::JIS_KANA;
-
-    } else if (inputmodedetail == InputModeDetail::AINU) {
-      fireKeyInfo_.flags = ModifierFlag::OPTION_L;
-      fireKeyInfo_.key = KeyCode::JIS_KANA;
-
-    } else {
-      return;
-    }
-
-    fireKeyInfo_.keyboardType = params.keyboardType;
-    fireKeyInfo_.active = true;
-
-    fire_timer_.setTimeoutMS(KEYEVENT_DELAY_MS);
-  }
-
-  InputModeDetail
-  Handle_VK_JIS_TEMPORARY::normalize(InputModeDetail imd)
-  {
-    if (imd == InputModeDetail::JAPANESE) {
-      return InputModeDetail::JAPANESE_HIRAGANA;
-    }
-    return imd;
-  }
-
-  void
-  Handle_VK_JIS_TEMPORARY::fire_timer_callback(OSObject* notuse_owner, IOTimerEventSource* notuse_sender)
-  {
-    fire();
-  }
-
-  void
-  Handle_VK_JIS_TEMPORARY::fire(void)
-  {
-    if (! fireKeyInfo_.active) return;
-    fireKeyInfo_.active = false;
-    EventOutputQueue::FireKey::fire_downup(fireKeyInfo_.flags, fireKeyInfo_.key, fireKeyInfo_.keyboardType);
-  }
-
-  InputModeDetail Handle_VK_JIS_TEMPORARY::savedinputmodedetail_(0);
-  InputModeDetail Handle_VK_JIS_TEMPORARY::currentinputmodedetail_(0);
-  Handle_VK_JIS_TEMPORARY::FireKeyInfo Handle_VK_JIS_TEMPORARY::fireKeyInfo_;
-  TimerWrapper Handle_VK_JIS_TEMPORARY::fire_timer_;
 }
--- src/core/kext/util/EventOutputQueue.cpp
+++ src/core/kext/util/EventOutputQueue.cpp
@@ -237,7 +237,7 @@ namespace org_pqrs_KeyRemap4MacBook {
     if (VirtualKey::VK_JIS_TOGGLE_EISUU_KANA::handle(params)) return;
     if (VirtualKey::VK_JIS_BACKSLASH::handle(params)) return;
     if (VirtualKey::VK_JIS_YEN::handle(params)) return;
-    if (Handle_VK_JIS_TEMPORARY::handle(params)) return;
+    if (VirtualKey::VK_JIS_TEMPORARY::handle(params)) return;
     if (params.key == KeyCode::VK_MODIFIER_EXTRA1 ||
         params.key == KeyCode::VK_MODIFIER_EXTRA2 ||
         params.key == KeyCode::VK_MODIFIER_EXTRA3 ||
