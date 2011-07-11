8d40e7da575de8c6557340c941dccc8513fcc1ba
Takayama Fumihiko <tekezo@pqrs.org>
2011-07-12 00:26:22 +0900

    Added Classes/RemapClassesInitializeVector @ server

---
+++ src/core/server/Classes/RemapClassesInitializeVector.m
@@ -0,0 +1,95 @@
+#import "RemapClassesInitializeVector.h"
+#include "bridge.h"
+
+@implementation RemapClassesInitializeVector
+
+enum {
+  MEMORY_BLOCK_SIZE = 1024 * 1024, // 1MB
+  INDEX_OF_FORMAT_VERSION = 0,
+  INDEX_OF_COUNT = 1,
+};
+
+- (id) init
+{
+  self = [super init];
+
+  if (self) {
+    capacity_ = MEMORY_BLOCK_SIZE;
+    data_ = (uint32_t*)(malloc(capacity_));
+    size_ = 0;
+    freezed_ = NO;
+
+    if (capacity_ <= INDEX_OF_COUNT) {
+      @throw [NSException
+            exceptionWithName: @"RemapClassesInitializeVector init error"
+            reason: @"capacity_ is too small"
+            userInfo: nil];
+    }
+    data_[INDEX_OF_FORMAT_VERSION] = BRIDGE_REMAPCLASS_INITIALIZE_VECTOR_FORMAT_VERSION;
+    data_[INDEX_OF_COUNT] = 0;
+  }
+
+  return self;
+}
+
+- (void) dealloc
+{
+  if (data_) {
+    free(data_);
+    data_ = NULL;
+  }
+
+  [super dealloc];
+}
+
+- (void) addVector:(NSArray*)vector
+{
+  // ----------------------------------------
+  // realloc if needed
+  NSUInteger newcount = [vector count] + 1;
+
+  while (size_ + newcount > capacity_) {
+    data_ = (uint32_t*)(realloc(data_, capacity_ + MEMORY_BLOCK_SIZE));
+    if (! data_) return;
+    capacity_ += MEMORY_BLOCK_SIZE;
+  }
+
+  // ----------------------------------------
+  // increase count
+  ++(data_[INDEX_OF_COUNT]);
+
+  // ----------------------------------------
+  // append to data_
+  uint32_t* p = data_ + size_;
+
+  *p++ = (uint32_t)([vector count]);
+  for (NSNumber* number in vector) {
+    *p++ = [number unsignedIntValue];
+  }
+
+  size_ += newcount;
+}
+
+- (void) setFreezed
+{
+  freezed_ = YES;
+}
+
+- (BOOL) freezed
+{
+  return freezed_;
+}
+
+- (uint32_t*) rawValue
+{
+  if (! freezed_) return NULL;
+  return data_;
+}
+
+- (size_t) size
+{
+  if (! freezed_) return 0;
+  return size_;
+}
+
+@end
