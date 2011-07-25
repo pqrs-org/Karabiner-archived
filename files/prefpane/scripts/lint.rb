ddecde218485f9bb1e0491eaba6d0c1ef3d9336a
Takayama Fumihiko <tekezo@pqrs.org>
2011-07-25 22:55:03 +0900

    update files/prefpane/scripts/lint.rb (remove libxml for Lion)

--- files/prefpane/scripts/lint.rb
+++ files/prefpane/scripts/lint.rb
@@ -1,19 +1,14 @@
 #!/usr/bin/ruby
 # -*- coding: utf-8 -*-
 
-require 'rubygems'
-require 'xml/libxml'
-
 file = "%s/../output/checkbox.xml" % File.dirname($0)
-parser = XML::Parser.file(file)
-libxmldoc = parser.parse
-
-libxmldoc.root.find('//identifier').each do |node|
-  identifier = node.inner_xml
+xml = File.read(file)
+xml.scan(/<identifier(\s.*?)?>(.+?)<\/identifier>/m).each do |matches|
+  identifier = matches[1].strip
   if /^private./ =~ identifier or /^notsave.private/ =~ identifier then
     print "\n"
     print "[ERROR] private.* identifier is forbidden in checkbox.xml.\n"
-    p node
+    p identifier
     print "\n"
     exit 1
   end
