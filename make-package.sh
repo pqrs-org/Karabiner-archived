c4d58823ec0622cd612894790d4df9588d903b43
Takayama Fumihiko <tekezo@pqrs.org>
2011-08-09 22:47:03 +0900

    update make-package.sh (remove sudo, remove --no-recommend from packagemaker arguments)

--- make-package.sh
+++ make-package.sh
@@ -12,73 +12,72 @@ make clean build || exit $?
 # http://developer.apple.com/documentation/Darwin/Conceptual/KEXTConcept/KEXTConceptPackaging/packaging_kext.html
 echo "Copy Files"
 
-sudo rm -rf pkgroot
-sudo mkdir -p pkgroot
+rm -rf pkgroot
+mkdir -p pkgroot
 
 basedir="/Library/org.pqrs/KeyRemap4MacBook"
-sudo mkdir -p "pkgroot/$basedir"
-sudo cp -R src/core/kext/build/Release/KeyRemap4MacBook.kext "pkgroot/$basedir"
-sudo cp -R files/scripts "pkgroot/$basedir"
+mkdir -p "pkgroot/$basedir"
+cp -R src/core/kext/build/Release/KeyRemap4MacBook.kext "pkgroot/$basedir"
+cp -R files/scripts "pkgroot/$basedir"
 
-sudo mkdir -p "pkgroot/$basedir/prefpane"
-sudo cp files/prefpane/number.xml files/prefpane/output/checkbox.xml "pkgroot/$basedir/prefpane"
+mkdir -p "pkgroot/$basedir/prefpane"
+cp files/prefpane/number.xml files/prefpane/output/checkbox.xml "pkgroot/$basedir/prefpane"
 
-sudo mkdir -p "pkgroot/$basedir/extra"
-sudo cp -R pkginfo/Resources/preflight "pkgroot/$basedir/extra/uninstall_core.sh"
-sudo cp -R files/extra/launchUninstaller.sh "pkgroot/$basedir/extra/"
-sudo cp -R files/extra/uninstall.sh "pkgroot/$basedir/extra/"
+mkdir -p "pkgroot/$basedir/extra"
+cp -R pkginfo/Resources/preflight "pkgroot/$basedir/extra/uninstall_core.sh"
+cp -R files/extra/launchUninstaller.sh "pkgroot/$basedir/extra/"
+cp -R files/extra/uninstall.sh "pkgroot/$basedir/extra/"
 
-sudo mkdir -p "pkgroot/Library"
-sudo cp -R files/LaunchDaemons pkgroot/Library
-sudo cp -R files/LaunchAgents pkgroot/Library
+mkdir -p "pkgroot/Library"
+cp -R files/LaunchDaemons pkgroot/Library
+cp -R files/LaunchAgents pkgroot/Library
 
-sudo mkdir -p "pkgroot/$basedir/app"
-sudo cp -R "src/core/server/build/Release/KeyRemap4MacBook.app" "pkgroot/$basedir/app"
-sudo cp -R "src/util/KeyDump/build/Release/KeyDump.app" "pkgroot/$basedir/app"
-sudo cp -R "src/util/multitouchextension/build/Release/KeyRemap4MacBook_multitouchextension.app" "pkgroot/$basedir/app"
-sudo cp -R "src/util/cli/build/Release/KeyRemap4MacBook_cli.app" "pkgroot/$basedir/app"
-sudo cp -R "src/util/uninstaller/build/Release/uninstaller.app" "pkgroot/$basedir/app"
+mkdir -p "pkgroot/$basedir/app"
+cp -R "src/core/server/build/Release/KeyRemap4MacBook.app" "pkgroot/$basedir/app"
+cp -R "src/util/KeyDump/build/Release/KeyDump.app" "pkgroot/$basedir/app"
+cp -R "src/util/multitouchextension/build/Release/KeyRemap4MacBook_multitouchextension.app" "pkgroot/$basedir/app"
+cp -R "src/util/cli/build/Release/KeyRemap4MacBook_cli.app" "pkgroot/$basedir/app"
+cp -R "src/util/uninstaller/build/Release/uninstaller.app" "pkgroot/$basedir/app"
 
-sudo mkdir -p "pkgroot/Library/PreferencePanes"
-sudo cp -R "src/util/prefpane/build/Release/KeyRemap4MacBook.prefPane" "pkgroot/Library/PreferencePanes"
+mkdir -p "pkgroot/Library/PreferencePanes"
+cp -R "src/util/prefpane/build/Release/KeyRemap4MacBook.prefPane" "pkgroot/Library/PreferencePanes"
 
-sudo find pkgroot -type d -print0 | xargs -0 sudo chmod 755
-sudo find pkgroot -type f -print0 | xargs -0 sudo chmod 644
-sudo find pkgroot -type l -print0 | xargs -0 sudo chmod -h 755
-sudo find pkgroot -name '*.sh' -print0 | xargs -0 sudo chmod 755
-for file in `sudo find pkgroot -type f`; do
+find pkgroot -type d -print0 | xargs -0 chmod 755
+find pkgroot -type f -print0 | xargs -0 chmod 644
+find pkgroot -name '*.sh' -print0 | xargs -0 chmod 755
+for file in `find pkgroot -type f`; do
     if ./pkginfo/is-mach-o.sh "$file"; then
-        sudo chmod 755 "$file"
+        chmod 755 "$file"
     fi
 done
-sudo chown -R root:wheel pkgroot
-
-sudo chmod 1775 pkgroot/Library
-sudo chown root:admin pkgroot/Library
 
 # --------------------------------------------------
 echo "Exec PackageMaker"
 
-sudo rm -rf $pkgName
-sudo $packagemaker \
+rm -rf $pkgName
+
+# Note: Don't add --no-recommend option.
+# It breaks /Library permission.
+# (It overwrites /Library permission with pkgroot/Library permission.)
+# - Mac OS X 10.5: /Library is 1775
+# - Mac OS X 10.6: /Library is 0755
+$packagemaker \
     --root pkgroot \
     --info pkginfo/Info.plist \
     --resources pkginfo/Resources \
     --title "KeyRemap4MacBook $version" \
-    --no-recommend \
     --no-relocate \
     --out $pkgName
 
 # --------------------------------------------------
 echo "Make Archive"
 
-sudo chown -R root:wheel $pkgName
-sudo zip -r $archiveName $pkgName
-sudo rm -rf $pkgName
-sudo chmod 644 $archiveName
+zip -X -r $archiveName $pkgName
+rm -rf $pkgName
+chmod 644 $archiveName
 unzip $archiveName
 
 # --------------------------------------------------
 echo "Cleanup"
-sudo rm -rf pkgroot
+rm -rf pkgroot
 make -C src clean
