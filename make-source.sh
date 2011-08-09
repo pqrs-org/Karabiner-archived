46cd6e4ca74c6d789a59b443da0be6e867035430
Takayama Fumihiko <tekezo@pqrs.org>
2011-08-09 22:37:03 +0900

    update make-source.sh (add -X to zip arguments)

--- make-source.sh
+++ make-source.sh
@@ -12,7 +12,7 @@ rsync -a . --exclude '.git/' --exclude '.gitignore' $workdir
 find $workdir -name '.DS_Store' -print0 | xargs -0 rm -f
 
 # archive
-(cd $tmpdir; zip -r -9 --symlinks $name.zip $name)
+(cd $tmpdir; zip -X -r -9 --symlinks $name.zip $name)
 mv $tmpdir/$name.zip .
 chmod 644 $name.zip
 
