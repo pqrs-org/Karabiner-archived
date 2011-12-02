18ab1b5b2ad041ea73b356247bcb009cc4a9dc1e
Takayama Fumihiko <tekezo@pqrs.org>
2011-12-02 19:44:03 +0900

    Added Tests/server

---
+++ Tests/server/KeyRemap4MacBookTest/KeyRemap4MacBookTest.h
@@ -0,0 +1,13 @@
+//
+//  KeyRemap4MacBookTest.h
+//  KeyRemap4MacBookTest
+//
+//  Created by Takayama Fumihiko on 11/12/02.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KeyRemap4MacBookTest : SenTestCase
+
+@end
