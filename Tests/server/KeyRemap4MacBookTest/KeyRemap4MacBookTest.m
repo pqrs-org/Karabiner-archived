f136d03a61210458547fa9b68cad69705189c657
Takayama Fumihiko <tekezo@pqrs.org>
2011-12-02 19:44:03 +0900

    Added Tests/server

---
+++ Tests/server/KeyRemap4MacBookTest/KeyRemap4MacBookTest.m
@@ -0,0 +1,32 @@
+//
+//  KeyRemap4MacBookTest.m
+//  KeyRemap4MacBookTest
+//
+//  Created by Takayama Fumihiko on 11/12/02.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "KeyRemap4MacBookTest.h"
+
+@implementation KeyRemap4MacBookTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in KeyRemap4MacBookTest");
+}
+
+@end
