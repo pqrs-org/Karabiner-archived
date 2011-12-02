c2e5cf1bea386bee79de100c28e29a68aa5711a0
Takayama Fumihiko <tekezo@pqrs.org>
2011-12-02 21:15:46 +0900

    improve NSString+HashBraces

--- src/core/server/Classes/NSString+HashBraces.m
+++ src/core/server/Classes/NSString+HashBraces.m
@@ -1,20 +1,30 @@
-#import "NSString+StringByReplacingOccurrencesOfDictionary.h"
+#import "NSString+HashBraces.h"
 
 @implementation NSString (HashBraces)
 
-- (NSString*) stringByReplacingHashBracesOccurrencesOfDictionary:(NSDictionary*)replacementDictionary options:(NSStringCompareOptions)options
+- (NSString*) stringByReplacingHashBracesOccurrencesOfDictionary:(NSDictionary*)replacementDictionary
 {
+  // withDictionary is {"#{XXX}" => "111", "#{YYY}" => "222"}.
+  // Then replace "#{XXX}" to "111", "#{YYY}" to "222".
+
+  // A simple way:
+  //   Calling stringByReplacingOccurrencesOfString to each item in replacementDictionary.
+  //
+  // Above method is too slow if target string (self) is too big and replacementDictionary has many items.
+  // (Because full scan of target string happens many times (replacementDictionary keys size).)
+  //
+  // So, we scan "#{" from target string and replacing them.
+  // This way needs full scan of target string only once.
+  // It reduces processing time dramatically if target string is too big.
+
   NSString* string = [NSString stringWithString:self];
 
   NSRange searchRange = NSMakeRange(0, [string length]);
 
-  // withDictionary is {"#{XXX}" => "111", "#{YYY}" => "222"}.
-  // Then replace "#{XXX}" to "111", "#{YYY}" to "222".
-
   for (;;) {
     // ------------------------------------------------------------
     // Searching "#{"
-    NSRange replacementBegin = [string rangeOfString:@"#{" options:options range:searchRange];
+    NSRange replacementBegin = [string rangeOfString:@"#{" options:NSLiteralSearch range:searchRange];
     if (replacementBegin.location == NSNotFound) break;
 
     // Setting length to 0 here becuase we adjust it after replacing.
@@ -25,14 +35,22 @@
     // Replacing "#{...}"
     NSRange range = NSMakeRange(replacementBegin.location,
                                 [string length] - replacementBegin.location);
-    for (NSString* target in replacementDictionary) {
-      NSRange replacementRange = [string rangeOfString:target
-                                               options:(options | NSAnchoredSearch)
+    for (NSString* replacementTarget in replacementDictionary) {
+      NSRange replacementRange = [string rangeOfString:replacementTarget
+                                               options:(NSLiteralSearch | NSAnchoredSearch)
                                                  range:range];
       if (replacementRange.location != NSNotFound) {
+        NSString* replacementString = [replacementDictionary objectForKey:replacementTarget];
+
         string = [string stringByReplacingCharactersInRange:replacementRange
-                                                 withString:[replacementDictionary objectForKey:target]];
-        searchRange.location = replacementBegin.location;
+                                                 withString:replacementString];
+
+        // Setting next searchRange.location to replacementBegin.location for when replacementString is empty.
+        // But, we need to avoid infinity replacing loop.
+        // If replacementString is begun with "#{", we skip it from next searchRange.
+        if (! [replacementString hasPrefix:@"#{"]) {
+          searchRange.location = replacementBegin.location;
+        }
         break;
       }
     }
