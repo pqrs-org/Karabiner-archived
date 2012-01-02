222c1e48e22daeb988f290eee2037b6b7ea3649e
Takayama Fumihiko <tekezo@pqrs.org>
2012-01-02 23:16:58 +0900

    fixed ctime of private.xml

--- src/core/server/Classes/ConfigXMLParser.m
+++ src/core/server/Classes/ConfigXMLParser.m
@@ -176,10 +176,14 @@ static ConfigXMLParser* global_instance = nil;
   if (! [filemanager fileExistsAtPath:path]) {
     [filemanager copyItemAtPath:[[NSBundle mainBundle] pathForResource:@"private" ofType:@"xml"] toPath:path error:NULL];
 
-    // copyItemAtPath does not change mtime of file. (mtime of destination file == mtime of source file.)
+    // copyItemAtPath does not change ctime and mtime of file.
+    // (For example, mtime of destination file == mtime of source file.)
     // Therefore, we need to set mtime to current time.
-    [filemanager setAttributes:[NSDictionary dictionaryWithObject:[NSDate date] forKey:NSFileModificationDate]
-                  ofItemAtPath:path error:NULL];
+    NSDictionary* attr = [NSDictionary dictionaryWithObjectsAndKeys:
+                          [NSDate date], NSFileCreationDate,
+                          [NSDate date], NSFileModificationDate,
+                          nil];
+    [filemanager setAttributes:attr ofItemAtPath:path error:NULL];
   }
 
   BOOL isDirectory;
