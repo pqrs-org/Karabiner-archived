4e71f015c3c7745e101258ad089a6abd02635ed9
Takayama Fumihiko <tekezo@pqrs.org>
2012-02-09 19:12:03 +0900

    replace HEAD: to HEAD @ make-source.sh

--- make-source.sh
+++ make-source.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 name="$(basename $(pwd))-$(cat version)"
-git archive --format=zip --prefix="${name}/" HEAD: > $name.zip
+git archive --format=zip --prefix="${name}/" HEAD > $name.zip
 chmod 644 $name.zip
