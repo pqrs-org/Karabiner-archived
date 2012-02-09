c41b1363e91a78cff0207b0821f866529e6dda3c
Takayama Fumihiko <tekezo@pqrs.org>
2012-02-09 19:00:56 +0900

    update make-source.sh to use git

--- make-source.sh
+++ make-source.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
 name="$(basename $(pwd))-$(cat version)"
-tmpdir=$(mktemp -d "/tmp/$name.XXXXXX")
-workdir="$tmpdir/$name"
-
-# copy
-rsync -a . --exclude '.git/' --exclude '.gitignore' $workdir
-
-# cleanup
-(cd $workdir; make clean)
-find $workdir -name '.DS_Store' -print0 | xargs -0 rm -f
-
-# archive
-(cd $tmpdir; zip -X -r -9 --symlinks $name.zip $name)
-mv $tmpdir/$name.zip .
+git archive --format=zip --prefix="${name}/" HEAD: > $name.zip
 chmod 644 $name.zip
-
-# purge tmpdir
-rm -rf $tmpdir
