e7c9580463196c1c78a7afe9e8cfbfc72ed7e2f7
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-05 14:59:31 +0900

    Added util/dump_xml_compiler_result/dump_xml_compiler_result

---
+++ util/dump_xml_compiler_result/dump_xml_compiler_result/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  dump_xml_compiler_result
+//
+//  Created by Takayama Fumihiko on 12/04/05.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char* argv[])
+{
+  return NSApplicationMain(argc, (const char**)argv);
+}
