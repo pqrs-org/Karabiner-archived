7ef8942820478a76d0cbe2092e340c8837b9b7e4
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-05 14:59:31 +0900

    Added util/dump_xml_compiler_result/dump_xml_compiler_result

---
+++ util/dump_xml_compiler_result/dump_xml_compiler_result/AppDelegate.h
@@ -0,0 +1,11 @@
+// -*- Mode: objc -*-
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
+  NSWindow* window;
+}
+
+@property (assign) IBOutlet NSWindow* window;
+
+@end
---
+++ util/dump_xml_compiler_result/dump_xml_compiler_result/AppDelegate.m
@@ -0,0 +1,36 @@
+#import "AppDelegate.h"
+#include "pqrs/xml_compiler_bindings_clang.h"
+
+@implementation AppDelegate
+
+@synthesize window;
+
+- (void) applicationDidFinishLaunching:(NSNotification*)aNotification
+{
+  NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
+  NSString* path = [paths objectAtIndex:0];
+  path = [path stringByAppendingPathComponent:@"KeyRemap4MacBook"];
+
+  struct pqrs_xml_compiler* p = NULL;
+  int error = pqrs_xml_compiler_initialize(&p,
+                                           "/Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook.app/Contents/Resources",
+                                           [path UTF8String]);
+  if (error) {
+    NSLog(@"Error %d", error);
+    exit(1);
+  }
+  pqrs_xml_compiler_reload(p);
+
+  size_t size = pqrs_xml_compiler_get_remapclasses_initialize_vector_size(p);
+  const uint32_t* v = pqrs_xml_compiler_get_remapclasses_initialize_vector_data(p);
+  for (size_t i = 0; i < size; ++i) {
+    printf("%d\n", v[i]);
+  }
+  fflush(stdout);
+
+  pqrs_xml_compiler_terminate(&p);
+
+  [[NSApplication sharedApplication] terminate:nil];
+}
+
+@end
