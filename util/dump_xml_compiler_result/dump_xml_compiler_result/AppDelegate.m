72f80e04de8f16dccc8f995d14edbcea33406cd1
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-06 18:46:54 +0900

    Added dump_tree @ dump_xml_compiler_result

--- util/dump_xml_compiler_result/dump_xml_compiler_result/AppDelegate.m
+++ util/dump_xml_compiler_result/dump_xml_compiler_result/AppDelegate.m
@@ -5,30 +5,71 @@
 
 @synthesize window;
 
+static int total_identifier_count_ = 0;
+
+- (NSString*) escapeHTML:(NSString*)string
+{
+  string = [string stringByReplacingOccurrencesOfString:@"&"  withString:@"&amp;"];
+  string = [string stringByReplacingOccurrencesOfString:@"<"  withString:@"&lt;"];
+  string = [string stringByReplacingOccurrencesOfString:@">"  withString:@"&gt;"];
+  return string;
+}
+
+- (void) dump_tree:(const pqrs_xml_compiler_preferences_checkbox_node_tree*)node_tree
+{
+  if (node_tree) {
+    size_t size = pqrs_xml_compiler_get_preferences_checkbox_node_tree_children_count(node_tree);
+    if (size > 0) {
+      printf("<ul>\n");
+      for (size_t i = 0; i < size; ++i) {
+        const pqrs_xml_compiler_preferences_checkbox_node_tree* child =
+          pqrs_xml_compiler_get_preferences_checkbox_node_tree_child(node_tree, i);
+        NSString* name = [NSString stringWithUTF8String:pqrs_xml_compiler_get_preferences_checkbox_node_tree_name(child)];
+        printf("<li>%s</li>\n", [[self escapeHTML:name] UTF8String]);
+
+        [self dump_tree:child];
+
+        if (pqrs_xml_compiler_get_preferences_checkbox_node_tree_identifier(child)) {
+          printf("<identifier>%s</identifier>\n", pqrs_xml_compiler_get_preferences_checkbox_node_tree_identifier(child));
+          ++total_identifier_count_;
+        }
+      }
+      printf("</ul>\n");
+    }
+  }
+}
+
 - (void) applicationDidFinishLaunching:(NSNotification*)aNotification
 {
-  NSArray* paths = NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES);
-  NSString* path = [paths objectAtIndex:0];
-  path = [path stringByAppendingPathComponent:@"KeyRemap4MacBook"];
-
-  pqrs_xml_compiler* p = NULL;
-  int error = pqrs_xml_compiler_initialize(&p,
+  pqrs_xml_compiler* pqrs_xml_compiler = NULL;
+  int error = pqrs_xml_compiler_initialize(&pqrs_xml_compiler,
                                            "/Library/org.pqrs/KeyRemap4MacBook/app/KeyRemap4MacBook.app/Contents/Resources",
-                                           [path UTF8String]);
+                                           "/noexistspath");
   if (error) {
     NSLog(@"Error %d", error);
     exit(1);
   }
-  pqrs_xml_compiler_reload(p);
+  pqrs_xml_compiler_reload(pqrs_xml_compiler);
 
-  size_t size = pqrs_xml_compiler_get_remapclasses_initialize_vector_size(p);
-  const uint32_t* v = pqrs_xml_compiler_get_remapclasses_initialize_vector_data(p);
-  for (size_t i = 0; i < size; ++i) {
-    printf("%d\n", v[i]);
+  NSArray* arguments = [[NSProcessInfo processInfo] arguments];
+  if ([arguments count] == 1 ||
+      [[arguments objectAtIndex:1] isEqualToString:@"dump_data"]) {
+    size_t size = pqrs_xml_compiler_get_remapclasses_initialize_vector_size(pqrs_xml_compiler);
+    const uint32_t* v = pqrs_xml_compiler_get_remapclasses_initialize_vector_data(pqrs_xml_compiler);
+    for (size_t i = 0; i < size; ++i) {
+      printf("%d\n", v[i]);
+    }
+
+  } else if ([[arguments objectAtIndex:1] isEqualToString:@"dump_tree"]) {
+    const pqrs_xml_compiler_preferences_checkbox_node_tree* node_tree =
+      pqrs_xml_compiler_get_preferences_checkbox_node_tree_root(pqrs_xml_compiler);
+    [self dump_tree:node_tree];
+
+    printf("Total items: %d\n", total_identifier_count_);
   }
-  fflush(stdout);
 
-  pqrs_xml_compiler_terminate(&p);
+  fflush(stdout);
+  pqrs_xml_compiler_terminate(&pqrs_xml_compiler);
 
   [[NSApplication sharedApplication] terminate:nil];
 }
