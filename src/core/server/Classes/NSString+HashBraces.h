92588d000d5aade308249ca5caf57b37a5fa4159
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-08 17:40:55 +0900

    remove ConfigXMLParser from server

--- src/core/server/Classes/NSString+HashBraces.h
+++
@@ -1,8 +0,0 @@
-// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
-#import <Cocoa/Cocoa.h>
-
-@interface NSString (HashBraces)
-
-- (NSString*) stringByReplacingHashBracesOccurrencesOfDictionary:(NSDictionary*)replacementDictionary;
-
-@end
