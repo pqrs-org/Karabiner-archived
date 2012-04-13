adb08da2ce2858c60880ec5733ef7c8dfbd75967
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-14 01:02:15 +0900

    Added pqrs/xml_compiler/detail/symbol_map.hpp

--- src/lib/xml_compiler/include/pqrs/xml_compiler.hpp
+++ src/lib/xml_compiler/include/pqrs/xml_compiler.hpp
@@ -109,26 +109,7 @@ namespace pqrs {
     };
     typedef std::tr1::shared_ptr<xml_file_path> xml_file_path_ptr;
 
-    // ============================================================
-    class symbol_map {
-    public:
-      symbol_map(void);
-      void clear(void);
-      void dump(void) const; // For debug
-
-      uint32_t get(const std::string& name) const;
-      uint32_t get(const std::string& type, const std::string& name) const;
-
-      boost::optional<uint32_t> get_optional(const std::string& name) const;
-      boost::optional<uint32_t> get_optional(const std::string& type, const std::string& name) const;
-
-      // Call add("KeyCode", "RETURN", 36) to register "KeyCode::RETURN = 36".
-      void add(const std::string& type, const std::string& name, uint32_t value);
-      void add(const std::string& type, const std::string& name);
-
-    private:
-      std::tr1::unordered_map<std::string, uint32_t> symbol_map_;
-    };
+#include "pqrs/xml_compiler/detail/symbol_map.hpp"
 
     // ============================================================
     class appdef {
@@ -277,10 +258,6 @@ namespace pqrs {
     void reload_replacementdef_(pqrs::string::replacement& replacement) const;
     void traverse_replacementdef_(const boost::property_tree::ptree& pt,
                                   pqrs::string::replacement& replacement) const;
-
-    void reload_symbol_map_(symbol_map& symbol_map) const;
-    void traverse_symbol_map_(const boost::property_tree::ptree& pt,
-                              symbol_map& symbol_map) const;
 
     void reload_appdef_(symbol_map& symbol_map,
                         std::vector<std::tr1::shared_ptr<appdef> >& app) const;
