31ba70ca41018fcd9d4ca25d46be43155cdd7dff
Takayama Fumihiko <tekezo@pqrs.org>
2012-04-30 20:37:39 +0900

    improved around extracted_ptree (share stack)

--- src/lib/xml_compiler/src/xml_compiler+extracted_ptree.cpp
+++ src/lib/xml_compiler/src/xml_compiler+extracted_ptree.cpp
@@ -4,9 +4,9 @@ namespace pqrs {
   void
   xml_compiler::extracted_ptree::extracted_ptree_iterator::increment(void)
   {
-    if (stack_.empty()) return;
+    if (ended_()) return;
 
-    auto& top = stack_.top();
+    auto& top = extracted_ptree_.stack_.top();
     if (top.it == top.end) return;
 
     ++(top.it);
@@ -17,41 +17,26 @@ namespace pqrs {
   const xml_compiler::extracted_ptree::node
   xml_compiler::extracted_ptree::extracted_ptree_iterator::dereference(void) const
   {
-    if (stack_.empty()) {
-      throw xml_compiler_logic_error("stack_.empty() in extracted_ptree_iterator.");
+    if (ended_()) {
+      throw xml_compiler_logic_error("extracted_ptree_iterator::dereference is called with invalid iterator.");
     }
 
-    auto& top = stack_.top();
+    auto& top = extracted_ptree_.stack_.top();
     if (top.it == top.end) {
-      throw xml_compiler_logic_error("it == end in extracted_ptree_iterator.");
+      throw xml_compiler_logic_error("it == end in extracted_ptree_iterator::dereference.");
     }
 
     return node(*(top.it), extracted_ptree_, top.parent_replacement);
   }
 
-  bool
-  xml_compiler::extracted_ptree::extracted_ptree_iterator::equal(const extracted_ptree_iterator const& other) const
-  {
-    if (stack_.empty() && other.stack_.empty()) {
-      return true;
-    }
-    if (stack_.empty() || other.stack_.empty()) {
-      return false;
-    }
-
-    auto& top1 = stack_.top();
-    auto& top2 = other.stack_.top();
-    return top1.it == top2.it;
-  }
-
   void
   xml_compiler::extracted_ptree::extracted_ptree_iterator::extract_include_(void)
   {
-    if (stack_.empty()) return;
+    if (ended_()) return;
 
     auto& xml_compiler = extracted_ptree_.xml_compiler_;
 
-    auto& top = stack_.top();
+    auto& top = extracted_ptree_.stack_.top();
     auto& it = *(top.it);
 
     if (it.first != "include") return;
@@ -107,7 +92,7 @@ namespace pqrs {
           auto root_node = pt_ptr->begin();
           auto& root_children = root_node->second;
           if (! root_children.empty()) {
-            stack_.push(stack_data(pt_ptr, replacement_ptr, root_children));
+            extracted_ptree_.stack_.push(stack_data(pt_ptr, replacement_ptr, root_children));
             extracted_ptree_.included_files_.push_back(xml_file_path);
             extract_include_();
           }
@@ -122,9 +107,9 @@ namespace pqrs {
   xml_compiler::extracted_ptree::extracted_ptree_iterator::collapse_(void)
   {
     for (;;) {
-      if (stack_.empty()) break;
+      if (ended_()) break;
 
-      auto& top = stack_.top();
+      auto& top = extracted_ptree_.stack_.top();
       if (top.it != top.end) {
         break;
       }
@@ -135,7 +120,7 @@ namespace pqrs {
         }
         extracted_ptree_.included_files_.pop_back();
       }
-      stack_.pop();
+      extracted_ptree_.stack_.pop();
     }
   }
 }
