c24ef4402289917d76fddb20fd7f05520e96d436
Takayama Fumihiko <tekezo@pqrs.org>
2012-05-04 12:44:20 +0900

    move code to header

--- src/lib/xml_compiler/include/pqrs/string.hpp
+++ src/lib/xml_compiler/include/pqrs/string.hpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/optional.hpp>
 #include <tr1/cstdint>
 #include <tr1/unordered_map>
@@ -23,9 +24,39 @@ namespace pqrs {
                                                                    replacement replacement);
 
     // octal,decimal,hex is supported.
-    static boost::optional<uint32_t> to_uint32_t(const char* string);
-    static boost::optional<uint32_t> to_uint32_t(const std::string& string);
-    static boost::optional<uint32_t> to_uint32_t(const boost::optional<std::string>& string);
+    static boost::optional<uint32_t> to_uint32_t(const char* string) {
+      if (! string) return boost::none;
+      if (! *string) return boost::none;
+
+      try {
+        if (string[0] == '0') {
+          // hex
+          if (string[1] == 'x' || string[1] == 'X') {
+            uint32_t v = boost::lexical_cast<hex_to<uint32_t> >(string);
+            return v;
+          }
+
+          // oct
+          uint32_t v = boost::lexical_cast<oct_to<uint32_t> >(string);
+          return v;
+
+        } else {
+          // decimal
+          return boost::lexical_cast<uint32_t>(string);
+        }
+      } catch (boost::bad_lexical_cast&) {
+        return boost::none;
+      }
+    }
+
+    static boost::optional<uint32_t> to_uint32_t(const std::string& string) {
+      return to_uint32_t(string.c_str());
+    }
+
+    static boost::optional<uint32_t> to_uint32_t(const boost::optional<std::string>& string) {
+      if (! string) return boost::none;
+      return to_uint32_t(*string);
+    }
 
     // tokenizer
     class tokenizer {
@@ -78,6 +109,36 @@ namespace pqrs {
     }
 
   private:
+    template<typename T>
+    class hex_to
+    {
+    public:
+      operator T(void) const { return value; }
+      friend std::istream& operator>>(std::istream& in, hex_to& out)
+      {
+        in >> std::hex >> out.value;
+        return in;
+      }
+
+    private:
+      T value;
+    };
+
+    template<typename T>
+    class oct_to
+    {
+    public:
+      operator T(void) const { return value; }
+      friend std::istream& operator>>(std::istream& in, oct_to& out)
+      {
+        in >> std::oct >> out.value;
+        return in;
+      }
+
+    private:
+      T value;
+    };
+
     static int string_by_replacing_double_curly_braces_(std::string& out,
                                                         std::istream& istream,
                                                         replacement replacement);
--- src/lib/xml_compiler/src/string.cpp
+++ src/lib/xml_compiler/src/string.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <boost/lexical_cast.hpp>
 #include "pqrs/string.hpp"
 #include "pqrs/vector.hpp"
 
@@ -149,78 +148,5 @@ namespace pqrs {
 
     // ----------------------------------------
     return string_by_replacing_double_curly_braces_(out, istream, replacement);
-  }
-
-// ============================================================
-// for boost::lexical_cast
-  template<typename T>
-  class hex_to
-  {
-  public:
-    operator T(void) const { return value; }
-    friend std::istream& operator>>(std::istream& in, hex_to& out)
-    {
-      in >> std::hex >> out.value;
-      return in;
-    }
-
-  private:
-    T value;
-  };
-
-// for boost::lexical_cast
-  template<typename T>
-  class oct_to
-  {
-  public:
-    operator T(void) const { return value; }
-    friend std::istream& operator>>(std::istream& in, oct_to& out)
-    {
-      in >> std::oct >> out.value;
-      return in;
-    }
-
-  private:
-    T value;
-  };
-
-  boost::optional<uint32_t>
-  string::to_uint32_t(const char* string)
-  {
-    if (! string) return boost::none;
-    if (! *string) return boost::none;
-
-    try {
-      if (string[0] == '0') {
-        // hex
-        if (string[1] == 'x' || string[1] == 'X') {
-          uint32_t v = boost::lexical_cast<hex_to<uint32_t> >(string);
-          return v;
-        }
-
-        // oct
-        uint32_t v = boost::lexical_cast<oct_to<uint32_t> >(string);
-        return v;
-
-      } else {
-        // decimal
-        return boost::lexical_cast<uint32_t>(string);
-      }
-    } catch (boost::bad_lexical_cast&) {
-      return boost::none;
-    }
-  }
-
-  boost::optional<uint32_t>
-  string::to_uint32_t(const std::string& string)
-  {
-    return to_uint32_t(string.c_str());
-  }
-
-  boost::optional<uint32_t>
-  string::to_uint32_t(const boost::optional<std::string>& string)
-  {
-    if (! string) return boost::none;
-    return to_uint32_t(*string);
   }
 }
