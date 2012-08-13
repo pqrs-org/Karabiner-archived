36fc68590ca962ea5f22964342cf63de55f585d6
Takayama Fumihiko <tekezo@pqrs.org>
2012-08-14 01:37:13 +0900

    update launchUninstaller.sh to call setpermissions.sh

--- files/extra/launchUninstaller.sh
+++ files/extra/launchUninstaller.sh
@@ -2,4 +2,5 @@
 
 TMPDIR=`/usr/bin/mktemp -d /tmp/KeyRemap4MacBook_uninstaller.XXXXXX` || exit 1
 /usr/bin/rsync -a /Library/org.pqrs/KeyRemap4MacBook/app/uninstaller.app "$TMPDIR"
+/Library/org.pqrs/KeyRemap4MacBook/extra/setpermissions.sh "$TMPDIR/uninstaller.app"
 /usr/bin/open "$TMPDIR/uninstaller.app"
