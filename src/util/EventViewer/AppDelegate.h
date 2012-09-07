7d18d82d7f6c04752e713e28c9dee065bf41986b
Takayama Fumihiko <tekezo@pqrs.org>
2012-09-08 00:14:15 +0900

    cleanup EventViewer

--- src/util/EventViewer/AppDelegate.h
+++ src/util/EventViewer/AppDelegate.h
@@ -1,16 +1,13 @@
 // -*- Mode: objc -*-
 //
-//  KeyDumpAppDelegate.h
-//  KeyDump
-//
-//  Created by Takayama Fumihiko on 09/10/25.
+//  AppDelegate.h
 //
 
 #import <Cocoa/Cocoa.h>
 #import "EventQueue.h"
 #import "OtherInformationStore.h"
 
-@interface KeyDumpAppDelegate : NSObject <NSApplicationDelegate> {
+@interface AppDelegate : NSObject <NSApplicationDelegate> {
   NSWindow* window;
   IBOutlet id keyResponder_;
   IBOutlet OtherInformationStore* otherinformationstore_;
