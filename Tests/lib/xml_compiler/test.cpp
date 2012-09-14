3a6ac87dec4f5049f51ee8c7852b44904229adb1
Takayama Fumihiko <tekezo@pqrs.org>
2012-09-15 01:43:53 +0900

    Added detail to languagedef

--- Tests/lib/xml_compiler/test.cpp
+++ Tests/lib/xml_compiler/test.cpp
@@ -573,6 +573,43 @@ TEST(pqrs_xml_compiler, reload_invalid_xml)
               std::string(xml_compiler.get_error_information().get_message()));
     EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
   }
+
+  {
+    pqrs::xml_compiler xml_compiler("data/system_xml", "data/invalid_xml/languagedef_no_name");
+    xml_compiler.reload();
+    EXPECT_EQ("No <name> within <languagedef>.", std::string(xml_compiler.get_error_information().get_message()));
+    EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
+  }
+  {
+    pqrs::xml_compiler xml_compiler("data/system_xml", "data/invalid_xml/languagedef_empty_name");
+    xml_compiler.reload();
+    EXPECT_EQ("Empty <name> within <languagedef>.",
+              std::string(xml_compiler.get_error_information().get_message()));
+    EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
+  }
+  {
+    pqrs::xml_compiler xml_compiler("data/system_xml", "data/invalid_xml/languagedef_no_value");
+    xml_compiler.reload();
+    EXPECT_EQ("No value definition within <languagedef>.",
+              std::string(xml_compiler.get_error_information().get_message()));
+    EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
+  }
+  {
+    pqrs::xml_compiler xml_compiler("data/system_xml", "data/invalid_xml/languagedef_empty_value");
+    xml_compiler.reload();
+    EXPECT_EQ("Empty value definition within <languagedef>.",
+              std::string(xml_compiler.get_error_information().get_message()));
+    EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
+  }
+  {
+    pqrs::xml_compiler xml_compiler("data/system_xml", "data/invalid_xml/languagedef_multiple_values");
+    xml_compiler.reload();
+    EXPECT_EQ("<languagedef> must not have multiple values:\n"
+              "\n"
+              "<inputsourceid_equal>com.apple.keylayout.Dvorak</inputsourceid_equal>",
+              std::string(xml_compiler.get_error_information().get_message()));
+    EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
+  }
 }
 
 TEST(pqrs_xml_compiler_symbol_map, add)
