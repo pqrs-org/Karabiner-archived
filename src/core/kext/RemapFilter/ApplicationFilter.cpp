60bcae2dd1a78bac70a239502a2fe8525173ae6f
Takayama Fumihiko <tekezo@pqrs.org>
2012-09-30 21:13:45 +0900

    Change Vector_FilterValue* to Vector_FilterValue

--- src/core/kext/RemapFilter/ApplicationFilter.cpp
+++ src/core/kext/RemapFilter/ApplicationFilter.cpp
@@ -6,30 +6,20 @@
 namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFilter {
     ApplicationFilter::ApplicationFilter(unsigned int t) : type_(t)
-    {
-      targets_ = new Vector_FilterValue();
-    }
+    {}
 
     ApplicationFilter::~ApplicationFilter(void)
-    {
-      if (targets_) {
-        delete targets_;
-      }
-    }
+    {}
 
     void
     ApplicationFilter::add(unsigned int newval)
     {
-      if (! targets_) return;
-
-      targets_->push_back(newval);
+      targets_.push_back(newval);
     }
 
     bool
     ApplicationFilter::isblocked(void)
     {
-      if (! targets_) return false;
-
       unsigned int current = CommonData::getcurrent_workspacedata().applicationtype;
 
       switch (type_) {
@@ -38,8 +28,8 @@ namespace org_pqrs_KeyRemap4MacBook {
         {
           bool isnot = (type_ == BRIDGE_FILTERTYPE_APPLICATION_NOT);
 
-          for (size_t i = 0; i < targets_->size(); ++i) {
-            if ((*targets_)[i] == current) {
+          for (size_t i = 0; i < targets_.size(); ++i) {
+            if (targets_[i] == current) {
               return isnot ? true : false;
             }
           }
