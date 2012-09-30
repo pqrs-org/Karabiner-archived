22347cdd2ed381099d2abed42b019ac84c0fa3ff
Takayama Fumihiko <tekezo@pqrs.org>
2012-09-30 21:17:43 +0900

    Change Vector_FilterValue* to Vector_FilterValue

--- src/core/kext/RemapFilter/InputSourceFilter.cpp
+++ src/core/kext/RemapFilter/InputSourceFilter.cpp
@@ -6,30 +6,20 @@
 namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFilter {
     InputSourceFilter::InputSourceFilter(unsigned int t) : type_(t)
-    {
-      targets_ = new Vector_FilterValue();
-    }
+    {}
 
     InputSourceFilter::~InputSourceFilter(void)
-    {
-      if (targets_) {
-        delete targets_;
-      }
-    }
+    {}
 
     void
     InputSourceFilter::add(unsigned int newval)
     {
-      if (! targets_) return;
-
-      targets_->push_back(newval);
+      targets_.push_back(newval);
     }
 
     bool
     InputSourceFilter::isblocked(void)
     {
-      if (! targets_) return false;
-
       unsigned int current = 0;
       switch (type_) {
         case BRIDGE_FILTERTYPE_INPUTSOURCE_NOT:
@@ -51,8 +41,8 @@ namespace org_pqrs_KeyRemap4MacBook {
         {
           bool isnot = (type_ == BRIDGE_FILTERTYPE_INPUTSOURCE_NOT || type_ == BRIDGE_FILTERTYPE_INPUTSOURCEDETAIL_NOT);
 
-          for (size_t i = 0; i < targets_->size(); ++i) {
-            if ((*targets_)[i] == current) {
+          for (size_t i = 0; i < targets_.size(); ++i) {
+            if (targets_[i] == current) {
               return isnot ? true : false;
             }
           }
