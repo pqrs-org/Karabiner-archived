7c2e96a1f6306cf66159c9c4748a1e578ad0ad15
Takayama Fumihiko <tekezo@pqrs.org>
2012-09-30 21:19:21 +0900

    Change Vector_FilterValue* to Vector_FilterValue

--- src/core/kext/RemapFilter/ModifierFilter.cpp
+++ src/core/kext/RemapFilter/ModifierFilter.cpp
@@ -6,30 +6,20 @@
 namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFilter {
     ModifierFilter::ModifierFilter(unsigned int t) : type_(t)
-    {
-      targets_ = new Vector_FilterValue();
-    }
+    {}
 
     ModifierFilter::~ModifierFilter(void)
-    {
-      if (targets_) {
-        delete targets_;
-      }
-    }
+    {}
 
     void
     ModifierFilter::add(unsigned int newval)
     {
-      if (! targets_) return;
-
-      targets_->push_back(newval);
+      targets_.push_back(newval);
     }
 
     bool
     ModifierFilter::isblocked(void)
     {
-      if (! targets_) return false;
-
       switch (type_) {
         case BRIDGE_FILTERTYPE_MODIFIER_NOT:
         case BRIDGE_FILTERTYPE_MODIFIER_ONLY:
@@ -37,8 +27,8 @@ namespace org_pqrs_KeyRemap4MacBook {
           bool isnot = (type_ == BRIDGE_FILTERTYPE_MODIFIER_NOT);
 
           Flags current = FlagStatus::makeFlags();
-          for (size_t i = 0; i < targets_->size(); ++i) {
-            Flags f((*targets_)[i]);
+          for (size_t i = 0; i < targets_.size(); ++i) {
+            Flags f(targets_[i]);
             if (current.isOn(f)) {
               return isnot ? true : false;
             }
