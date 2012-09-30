6d4cd48d9954cd8f9411da4da8c63e543bb01dc7
Takayama Fumihiko <tekezo@pqrs.org>
2012-09-30 21:14:41 +0900

    Change Vector_FilterValue* to Vector_FilterValue

--- src/core/kext/RemapFilter/ConfigFilter.cpp
+++ src/core/kext/RemapFilter/ConfigFilter.cpp
@@ -6,38 +6,28 @@
 namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFilter {
     ConfigFilter::ConfigFilter(unsigned int t) : type_(t)
-    {
-      targets_ = new Vector_FilterValue();
-    }
+    {}
 
     ConfigFilter::~ConfigFilter(void)
-    {
-      if (targets_) {
-        delete targets_;
-      }
-    }
+    {}
 
     void
     ConfigFilter::add(unsigned int newval)
     {
-      if (! targets_) return;
-
-      targets_->push_back(newval);
+      targets_.push_back(newval);
     }
 
     bool
     ConfigFilter::isblocked(void)
     {
-      if (! targets_) return false;
-
       switch (type_) {
         case BRIDGE_FILTERTYPE_CONFIG_NOT:
         case BRIDGE_FILTERTYPE_CONFIG_ONLY:
         {
           bool isnot = (type_ == BRIDGE_FILTERTYPE_CONFIG_NOT);
 
-          for (size_t i = 0; i < targets_->size(); ++i) {
-            if (RemapClassManager::isEnabled((*targets_)[i])) {
+          for (size_t i = 0; i < targets_.size(); ++i) {
+            if (RemapClassManager::isEnabled(targets_[i])) {
               return isnot ? true : false;
             }
           }
