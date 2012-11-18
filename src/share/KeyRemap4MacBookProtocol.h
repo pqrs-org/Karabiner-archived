4bba65b7384f907353dac0104aae6aa456044669
Takayama Fumihiko <tekezo@pqrs.org>
2012-11-18 10:20:11 +0900

    Added KeyRemap4MacBookProtocol.h

---
+++ src/share/KeyRemap4MacBookProtocol.h
@@ -0,0 +1,48 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+#import <Cocoa/Cocoa.h>
+
+@protocol org_pqrs_KeyRemap4MacBook_Protocol
+- (int) value:(NSString*)name;
+- (int) defaultValue:(NSString*)name;
+- (void) setValueForName:(int)newval forName:(NSString*)name;
+
+- (NSArray*) essential_config;
+- (NSDictionary*) changed;
+
+// --------------------------------------------------
+- (NSInteger)     configlist_selectedIndex;
+- (NSString*)     configlist_selectedName;
+- (NSString*)     configlist_selectedIdentifier;
+- (NSArray*)      configlist_getConfigList;
+- (NSUInteger)    configlist_count;
+- (NSDictionary*) configlist_dictionary:(NSInteger)rowIndex;
+- (NSString*)     configlist_name:(NSInteger)rowIndex;
+- (NSString*)     configlist_identifier:(NSInteger)rowIndex;
+- (void)          configlist_select:(NSInteger)newindex;
+- (void)          configlist_setName:(NSInteger)rowIndex name:(NSString*)name;
+- (void)          configlist_append;
+- (void)          configlist_delete:(NSInteger)rowIndex;
+
+- (BOOL) isStatusbarEnable;
+- (BOOL) isShowSettingNameInStatusBar;
+- (void) toggleStatusbarEnable;
+- (void) toggleShowSettingNameInStatusBar;
+
+- (NSInteger) checkForUpdatesMode;
+- (void) setCheckForUpdatesMode:(NSInteger)newval;
+
+- (void) configxml_reload;
+
+- (NSArray*) preferencepane_checkbox;
+- (NSArray*) preferencepane_number;
+- (int) preferencepane_enabled_count;
+- (NSString*) preferencepane_error_message;
+- (NSString*) preferencepane_get_private_xml_path;
+- (NSString*) preferencepane_version;
+
+- (void) checkForUpdates;
+
+- (NSArray*) device_information:(NSInteger)type;
+
+@end
