59eb34d8bb313c732d9ae384dd5afb96b09cd8b2
Takayama Fumihiko <tekezo@pqrs.org>
2012-12-04 14:18:53 +0900

    cleanup

--- src/share/KeyRemap4MacBookProtocol.h
+++ src/share/KeyRemap4MacBookProtocol.h
@@ -7,22 +7,12 @@
 - (int) defaultValue:(NSString*)name;
 - (void) setValueForName:(int)newval forName:(NSString*)name;
 
-- (NSArray*) essential_config;
 - (NSDictionary*) changed;
 
 // --------------------------------------------------
 - (NSInteger)     configlist_selectedIndex;
-- (NSString*)     configlist_selectedName;
-- (NSString*)     configlist_selectedIdentifier;
 - (NSArray*)      configlist_getConfigList;
-- (NSUInteger)    configlist_count;
-- (NSDictionary*) configlist_dictionary:(NSInteger)rowIndex;
-- (NSString*)     configlist_name:(NSInteger)rowIndex;
-- (NSString*)     configlist_identifier:(NSInteger)rowIndex;
 - (void)          configlist_select:(NSInteger)newindex;
-- (void)          configlist_setName:(NSInteger)rowIndex name:(NSString*)name;
-- (void)          configlist_append;
-- (void)          configlist_delete:(NSInteger)rowIndex;
 
 - (void) configxml_reload;
 
