9f4663df5330293d2566d17a3a4d6b02f95d977a
Takayama Fumihiko <tekezo@pqrs.org>
2012-12-07 16:42:14 +0900

    export and set to cli

--- src/util/cli/KeyRemap4MacBook_cliAppDelegate.m
+++ src/util/cli/KeyRemap4MacBook_cliAppDelegate.m
@@ -17,16 +17,20 @@
   [self output:@"  KeyRemap4MacBook_cli selected\n"];
   [self output:@"  KeyRemap4MacBook_cli changed\n"];
   [self output:@"  KeyRemap4MacBook_cli reloadxml\n"];
+  [self output:@"  KeyRemap4MacBook_cli export\n"];
   [self output:@"  KeyRemap4MacBook_cli select INDEX\n"];
-  [self output:@"  KeyRemap4MacBook_cli enable IDENTIFIER\n"];
-  [self output:@"  KeyRemap4MacBook_cli disable IDENTIFIER\n"];
+  [self output:@"  KeyRemap4MacBook_cli set IDENTIFIER VALUE\n"];
+  [self output:@"  KeyRemap4MacBook_cli enable IDENTIFIER (alias of set IDENTIFIER 1)\n"];
+  [self output:@"  KeyRemap4MacBook_cli disable IDENTIFIER (alias of set IDENTIFIER 0)\n"];
   [self output:@"\n"];
   [self output:@"Example:\n"];
   [self output:@"  KeyRemap4MacBook_cli list\n"];
   [self output:@"  KeyRemap4MacBook_cli selected\n"];
   [self output:@"  KeyRemap4MacBook_cli changed\n"];
   [self output:@"  KeyRemap4MacBook_cli reloadxml\n"];
+  [self output:@"  KeyRemap4MacBook_cli export\n"];
   [self output:@"  KeyRemap4MacBook_cli select 1\n"];
+  [self output:@"  KeyRemap4MacBook_cli set repeat.wait 30\n"];
   [self output:@"  KeyRemap4MacBook_cli enable remap.shiftL2commandL\n"];
   [self output:@"  KeyRemap4MacBook_cli disable remap.shiftL2commandL\n"];
 
@@ -64,12 +68,33 @@
     } else if ([command isEqualToString:@"reloadxml"]) {
       [[client_ proxy] configxml_reload];
 
+    } else if ([command isEqualToString:@"export"]) {
+      NSDictionary* dict = [[client_ proxy] changed];
+      if (dict) {
+        [self output:@"#!/bin/sh\n\n"];
+        [self output:[NSString stringWithFormat:@"cli=%@\n\n", [arguments objectAtIndex:0]]];
+
+        for (NSString* key in [dict allKeys]) {
+          if (! [key hasPrefix:@"notsave."]) {
+            [self output:[NSString stringWithFormat:@"$cli set %@ %@\n", key, [dict objectForKey:key]]];
+          }
+        }
+      }
+
     } else if ([command isEqualToString:@"select"]) {
       if ([arguments count] != 3) {
         [self usage];
       }
       NSString* value = [arguments objectAtIndex:2];
       [[client_ proxy] configlist_select:[value intValue]];
+
+    } else if ([command isEqualToString:@"set"]) {
+      if ([arguments count] != 4) {
+        [self usage];
+      }
+      NSString* identifier = [arguments objectAtIndex:2];
+      NSString* value = [arguments objectAtIndex:3];
+      [[client_ proxy] setValueForName:[value intValue] forName:identifier];
 
     } else if ([command isEqualToString:@"enable"]) {
       if ([arguments count] != 3) {
