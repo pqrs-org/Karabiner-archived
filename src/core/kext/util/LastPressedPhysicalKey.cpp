c416d7f25acd4c05ea361d9caa46a661d49a1f07
Takayama Fumihiko <tekezo@pqrs.org>
2013-03-31 14:49:53 +0900

    Added LastPressedPhysicalKey

---
+++ src/core/kext/util/LastPressedPhysicalKey.cpp
@@ -0,0 +1,58 @@
+#include <sys/systm.h>
+#include "LastPressedPhysicalKey.hpp"
+#include "ParamsUnion.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  void
+  LastPressedPhysicalKey::update(const ParamsUnion& newval)
+  {
+    switch (newval.type) {
+      case ParamsUnion::KEYBOARD:
+      {
+        Params_KeyboardEventCallBack* p = newval.params.params_KeyboardEventCallBack;
+
+        if (p && p->ex_iskeydown) {
+          datatype_ = BRIDGE_DATATYPE_KEYCODE;
+          value_ = (p->key).get();
+          update_time();
+        }
+        break;
+      }
+
+      case ParamsUnion::KEYBOARD_SPECIAL:
+      {
+        Params_KeyboardSpecialEventCallback* p = newval.params.params_KeyboardSpecialEventCallback;
+        if (p && p->ex_iskeydown) {
+          datatype_ = BRIDGE_DATATYPE_CONSUMERKEYCODE;
+          value_ = (p->key).get();
+          update_time();
+        }
+        break;
+      }
+
+      case ParamsUnion::RELATIVE_POINTER:
+      {
+        Params_RelativePointerEventCallback* p = newval.params.params_RelativePointerEventCallback;
+
+        if (p && p->ex_button != PointingButton::NONE && p->ex_isbuttondown) {
+          datatype_ = BRIDGE_DATATYPE_POINTINGBUTTON;
+          value_ = (p->ex_button).get();
+          update_time();
+        }
+        break;
+      }
+
+      case ParamsUnion::UPDATE_FLAGS:
+      case ParamsUnion::SCROLL_POINTER:
+      case ParamsUnion::WAIT:
+        // Do nothing
+        break;
+    }
+  }
+
+  void
+  LastPressedPhysicalKey::update_time(void)
+  {
+    clock_get_system_microtime(&secs_, &microsecs_);
+  }
+}
