4c71ec78bc87de8da9d623abfe3a1c5935956087
Takayama Fumihiko <tekezo@pqrs.org>
2013-04-15 11:26:47 +0900

    update EnvironmentChecker

--- src/core/server/Classes/EnvironmentChecker.m
+++ src/core/server/Classes/EnvironmentChecker.m
@@ -4,14 +4,30 @@
 
 + (BOOL) checkDoubleCommand
 {
-  NSString* path = @"/Library/StartupItems/DoubleCommand";
-  return [[NSFileManager defaultManager] fileExistsAtPath:path];
+  {
+    NSString* path = @"/Library/StartupItems/DoubleCommand";
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+      return YES;
+    }
+  }
+  return NO;
 }
 
 + (BOOL) checkSmoothMouse
 {
-  NSString* path = @"/System/Library/Extensions/SmoothMouse.kext";
-  return [[NSFileManager defaultManager] fileExistsAtPath:path];
+  {
+    NSString* path = @"/System/Library/Extensions/SmoothMouse.kext";
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+      return YES;
+    }
+  }
+  {
+    NSString* path = @"/Library/PreferencePanes/SmoothMouse.prefPane";
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+      return YES;
+    }
+  }
+  return NO;
 }
 
 @end
