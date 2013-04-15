e8f8867dce05a8b67a29961c1d2a436355f270c1
Takayama Fumihiko <tekezo@pqrs.org>
2013-04-15 11:06:08 +0900

    Added EnvironmentChecker

---
+++ src/core/server/Classes/EnvironmentChecker.h
@@ -0,0 +1,10 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+#import <Cocoa/Cocoa.h>
+
+@interface EnvironmentChecker : NSObject
+
++ (BOOL) checkDoubleCommand;
++ (BOOL) checkSmoothMouse;
+
+@end
