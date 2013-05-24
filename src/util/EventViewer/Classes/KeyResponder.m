1c9d5df006337c9530855575e633ed2d4281a0d0
Takayama Fumihiko <tekezo@pqrs.org>
2013-05-25 00:49:15 +0900

    refine Mouse Area

--- src/util/EventViewer/Classes/KeyResponder.m
+++ src/util/EventViewer/Classes/KeyResponder.m
@@ -5,6 +5,36 @@
 
 @implementation KeyResponder
 
+- (void) drawRect:(NSRect)dirtyRect
+{
+  [NSGraphicsContext saveGraphicsState];
+  {
+    NSRect bounds = [self bounds];
+
+    CGFloat w = bounds.size.width / 40;
+    NSRect text = NSMakeRect(w * 2,
+                             w * 2,
+                             bounds.size.width - w * 4,
+                             bounds.size.height - w * 4);
+
+    // Draw area
+    [[[NSColor whiteColor] colorWithAlphaComponent:0.8] set];
+    [[NSBezierPath bezierPathWithRoundedRect:bounds xRadius:10 yRadius:10] fill];
+
+    [[[NSColor blackColor] colorWithAlphaComponent:0.6] set];
+    NSBezierPath* path = [NSBezierPath bezierPathWithRoundedRect:bounds xRadius:10 yRadius:10];
+    [path setLineWidth:w];
+    [path stroke];
+
+    // Draw texts
+    NSDictionary* attributes = @ { NSFontAttributeName :[NSFont boldSystemFontOfSize:(text.size.width / 10)],
+                                   NSForegroundColorAttributeName :[[NSColor blackColor] colorWithAlphaComponent:0.6], };
+    [@"Mouse Area" drawInRect:text withAttributes:attributes];
+  }
+  [NSGraphicsContext restoreGraphicsState];
+}
+
+// ============================================================
 //
 // Key event handlers
 //
