bc556062078048208d80328f1c6303692a43927a
Takayama Fumihiko <tekezo@pqrs.org>
2013-06-06 14:30:52 +0900

    Updated Tests for suppress_replacement_warnings_

--- Tests/lib/xml_compiler/test.cpp
+++ Tests/lib/xml_compiler/test.cpp
@@ -579,7 +579,7 @@ TEST(pqrs_xml_compiler, reload_invalid_xml)
   {
     pqrs::xml_compiler xml_compiler("data/invalid_xml/replacementdef_invalid_name1", "data/private_xml");
     xml_compiler.reload();
-    EXPECT_EQ("<data/invalid_xml/replacementdef_invalid_name1/replacementdef.xml>(10): unexpected end of data",
+    EXPECT_EQ("<data/invalid_xml/replacementdef_invalid_name1/replacementdef.xml>(45): unexpected end of data",
               xml_compiler.get_error_information().get_message());
     EXPECT_EQ(1, xml_compiler.get_error_information().get_count());
   }
