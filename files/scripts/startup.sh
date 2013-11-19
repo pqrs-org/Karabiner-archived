cc7e5fa55d831736d106594a57ad9e3e3b4ddd18
Takayama Fumihiko <tekezo@pqrs.org>
2013-11-19 22:41:22 +0900

    remove sleep at startup.sh (no need anymore)

--- files/scripts/startup.sh
+++ files/scripts/startup.sh
@@ -8,12 +8,6 @@ argument="$1"
 case "$argument" in
     start)
         echo "Starting KeyRemap4MacBook"
-        sleep 10 # wait for avoid kext loading collision.
-        $basedir/load.sh
-        ;;
-
-    quickstart)
-        echo "Starting KeyRemap4MacBook"
         $basedir/load.sh
         ;;
 
