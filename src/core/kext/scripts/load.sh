e9fe5925543ff456a3cf6e1b6d5b553ca0440836
Takayama Fumihiko <tekezo@pqrs.org>
2013-11-20 17:36:12 +0900

    use codesign.sh

--- src/core/kext/scripts/load.sh
+++ src/core/kext/scripts/load.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-sudo cp -R build/Release/KeyRemap4MacBook.kext /tmp/
-sudo kextutil -t /tmp/KeyRemap4MacBook.kext
+PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+
+# --------------------------------------------------
+sudo rm -rf /tmp/KeyRemap4MacBook
+mkdir /tmp/KeyRemap4MacBook
+
+cp -R build/Release/KeyRemap4MacBook.kext /tmp/KeyRemap4MacBook/KeyRemap4MacBook.signed.kext
+bash ../../../files/extra/codesign.sh /tmp/KeyRemap4MacBook
+sudo chown -R root:wheel /tmp/KeyRemap4MacBook
+
+sudo kextutil -t /tmp/KeyRemap4MacBook/KeyRemap4MacBook.signed.kext
