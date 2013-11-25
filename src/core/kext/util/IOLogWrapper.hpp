225ce80b10c95dc8c4c9dd26a87c88f48767f9ea
Takayama Fumihiko <tekezo@pqrs.org>
2013-11-26 04:08:25 +0900

    Added IOLogWrapper::suppressed_

--- src/core/kext/util/IOLogWrapper.hpp
+++ src/core/kext/util/IOLogWrapper.hpp
@@ -1,24 +1,57 @@
 #ifndef IOLOGWRAPPER_HPP
 #define IOLOGWRAPPER_HPP
 
-#define IOLOG_DEBUG(...) {                              \
-    if (Config::get_debug()) {                          \
-      IOLog("KeyRemap4MacBook --Debug-- " __VA_ARGS__); \
-    }                                                   \
+#define IOLOG_DEBUG(...) {                                         \
+    if (! org_pqrs_KeyRemap4MacBook::IOLogWrapper::suppressed()) { \
+      if (Config::get_debug()) {                                   \
+        IOLog("KeyRemap4MacBook --Debug-- " __VA_ARGS__);          \
+      }                                                            \
+    }                                                              \
 }
-#define IOLOG_DEBUG_POINTING(...) {                     \
-    if (Config::get_debug_pointing()) {                 \
-      IOLog("KeyRemap4MacBook --Debug-- " __VA_ARGS__); \
-    }                                                   \
+#define IOLOG_DEBUG_POINTING(...) {                                \
+    if (! org_pqrs_KeyRemap4MacBook::IOLogWrapper::suppressed()) { \
+      if (Config::get_debug_pointing()) {                          \
+        IOLog("KeyRemap4MacBook --Debug-- " __VA_ARGS__);          \
+      }                                                            \
+    }                                                              \
 }
-#define IOLOG_DEVEL(...) {                              \
-    if (Config::get_debug_devel()) {                    \
-      IOLog("KeyRemap4MacBook --Devel-- " __VA_ARGS__); \
-    }                                                   \
+#define IOLOG_DEVEL(...) {                                         \
+    if (! org_pqrs_KeyRemap4MacBook::IOLogWrapper::suppressed()) { \
+      if (Config::get_debug_devel()) {                             \
+        IOLog("KeyRemap4MacBook --Devel-- " __VA_ARGS__);          \
+      }                                                            \
+    }                                                              \
 }
 
-#define IOLOG_ERROR(...)  IOLog("KeyRemap4MacBook --Error-- " __VA_ARGS__)
-#define IOLOG_INFO(...)   IOLog("KeyRemap4MacBook --Info-- "  __VA_ARGS__)
-#define IOLOG_WARN(...)   IOLog("KeyRemap4MacBook --Warn-- "  __VA_ARGS__)
+#define IOLOG_ERROR(...) {                                         \
+    if (! org_pqrs_KeyRemap4MacBook::IOLogWrapper::suppressed()) { \
+      IOLog("KeyRemap4MacBook --Error-- " __VA_ARGS__);            \
+    }                                                              \
+}
+
+#define IOLOG_INFO(...) {                                          \
+    if (! org_pqrs_KeyRemap4MacBook::IOLogWrapper::suppressed()) { \
+      IOLog("KeyRemap4MacBook --Info-- "  __VA_ARGS__);            \
+    }                                                              \
+}
+
+#define IOLOG_WARN(...) {                                          \
+    if (! org_pqrs_KeyRemap4MacBook::IOLogWrapper::suppressed()) { \
+      IOLog("KeyRemap4MacBook --Warn-- "  __VA_ARGS__);            \
+    }                                                              \
+}
+
+
+// ------------------------------------------------------------
+namespace org_pqrs_KeyRemap4MacBook {
+  class IOLogWrapper {
+  public:
+    static bool suppressed(void) { return suppressed_; }
+    static void suppress(bool v) { suppressed_ = v; }
+
+  private:
+    static bool suppressed_;
+  };
+}
 
 #endif
