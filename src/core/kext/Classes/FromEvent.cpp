ba6d7213773f8cf563c2c45c8361c65dbbeebcac
Takayama Fumihiko <tekezo@pqrs.org>
2013-12-13 21:06:17 +0900

    Supported ConsumerKeyCode at from keys of __SimultaneousKeyPresses__.

---
+++ src/core/kext/Classes/FromEvent.cpp
@@ -0,0 +1,74 @@
+#include <IOKit/IOLib.h>
+
+#include "EventInputQueue.hpp"
+#include "FromEvent.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  bool
+  FromEvent::isTarget(bool& isDown, const EventInputQueue::Item& item) const
+  {
+    switch (type_) {
+      case Type::NONE:
+      {
+        return false;
+      }
+
+      case Type::KEY:
+      {
+        if (item.params.type != ParamsUnion::KEYBOARD) return false;
+
+        Params_KeyboardEventCallBack* params = item.params.params.params_KeyboardEventCallBack;
+        if (! params) return false;
+
+        if (params->key != key_) return false;
+
+        isDown = params->ex_iskeydown;
+        return true;
+      }
+
+      case Type::CONSUMER_KEY:
+      {
+        if (item.params.type != ParamsUnion::KEYBOARD_SPECIAL) return false;
+
+        Params_KeyboardSpecialEventCallback* params = item.params.params.params_KeyboardSpecialEventCallback;
+        if (! params) return false;
+
+        if (params->key != consumer_) return false;
+
+        isDown = params->ex_iskeydown;
+        return true;
+      }
+
+      case Type::POINTING_BUTTON:
+      {
+        if (item.params.type != ParamsUnion::RELATIVE_POINTER) return false;
+
+        Params_RelativePointerEventCallback* params = item.params.params.params_RelativePointerEventCallback;
+        if (! params) return false;
+
+        if (params->ex_button != button_) return false;
+
+        isDown = params->ex_isbuttondown;
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  bool
+  FromEvent::isTargetDown(const EventInputQueue::Item& item) const
+  {
+    bool isDown = false;
+    if (! isTarget(isDown, item)) return false;
+    return isDown;
+  }
+
+  bool
+  FromEvent::isTargetUp(const EventInputQueue::Item& item) const
+  {
+    bool isDown = false;
+    if (! isTarget(isDown, item)) return false;
+    return ! isDown;
+  }
+}
---
+++ src/core/kext/Classes/FromEvent.hpp
@@ -0,0 +1,37 @@
+#ifndef FROMEVENT_HPP
+#define FROMEVENT_HPP
+
+#include "KeyCode.hpp"
+
+namespace org_pqrs_KeyRemap4MacBook {
+  class FromEvent {
+  public:
+    class Type {
+    public:
+      enum Value {
+        NONE,
+        KEY,
+        CONSUMER_KEY, // Mute, VolumeIncrement, VolumeDecrement, etcetc.
+        POINTING_BUTTON,
+      };
+    };
+
+    FromEvent(void)              : type_(Type::NONE)                          {}
+    FromEvent(KeyCode v)         : type_(Type::KEY),             key_(v)      {}
+    FromEvent(ConsumerKeyCode v) : type_(Type::CONSUMER_KEY),    consumer_(v) {}
+    FromEvent(PointingButton v)  : type_(Type::POINTING_BUTTON), button_(v)   {}
+
+    bool isTargetDown(const EventInputQueue::Item& item) const;
+    bool isTargetUp(const EventInputQueue::Item& item) const;
+
+  private:
+    bool isTarget(bool& isDown, const EventInputQueue::Item& item) const;
+
+    Type::Value type_;
+    KeyCode key_;
+    ConsumerKeyCode consumer_;
+    PointingButton button_;
+  };
+}
+
+#endif
