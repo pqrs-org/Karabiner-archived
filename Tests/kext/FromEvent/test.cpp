4a62650d2252daaae3ea9ed02d12f1dfd8ce2e42
Takayama Fumihiko <tekezo@pqrs.org>
2013-12-15 01:09:18 +0900

    update Tests

--- Tests/kext/FromEvent/test.cpp
+++ Tests/kext/FromEvent/test.cpp
@@ -22,3 +22,75 @@ TEST(Generic, getModifierFlag) {
     EXPECT_EQ(ModifierFlag::NONE, fe.getModifierFlag());
   }
 }
+
+TEST(Generic, isTargetDownEvent) {
+  Params_KeyboardEventCallBack::auto_ptr down_return(
+    Params_KeyboardEventCallBack::alloc(
+      EventType::DOWN,
+      Flags(0),
+      KeyCode::RETURN,
+      CharCode(0),
+      CharSet(0),
+      OrigCharCode(0),
+      OrigCharSet(0),
+      KeyboardType(0),
+      false));
+
+  Params_KeyboardEventCallBack::auto_ptr up_return(
+    Params_KeyboardEventCallBack::alloc(
+      EventType::UP,
+      Flags(0),
+      KeyCode::RETURN,
+      CharCode(0),
+      CharSet(0),
+      OrigCharCode(0),
+      OrigCharSet(0),
+      KeyboardType(0),
+      false));
+
+  Params_KeyboardEventCallBack::auto_ptr down_shift(
+    Params_KeyboardEventCallBack::alloc(
+      EventType::MODIFY,
+      Flags(ModifierFlag::SHIFT_L),
+      KeyCode::SHIFT_L,
+      CharCode(0),
+      CharSet(0),
+      OrigCharCode(0),
+      OrigCharSet(0),
+      KeyboardType(0),
+      false));
+
+  Params_KeyboardEventCallBack::auto_ptr up_shift(
+    Params_KeyboardEventCallBack::alloc(
+      EventType::MODIFY,
+      Flags(0),
+      KeyCode::SHIFT_L,
+      CharCode(0),
+      CharSet(0),
+      OrigCharCode(0),
+      OrigCharSet(0),
+      KeyboardType(0),
+      false));
+
+  {
+    FromEvent fe(KeyCode::RETURN);
+    EXPECT_EQ(true,  fe.isTargetDownEvent(*down_return));
+    EXPECT_EQ(false, fe.isTargetUpEvent(*down_return));
+    EXPECT_EQ(false, fe.isTargetDownEvent(*up_return));
+    EXPECT_EQ(true,  fe.isTargetUpEvent(*up_return));
+  }
+  {
+    FromEvent fe(KeyCode::SPACE);
+    EXPECT_EQ(false, fe.isTargetDownEvent(*down_return));
+    EXPECT_EQ(false, fe.isTargetUpEvent(*down_return));
+    EXPECT_EQ(false, fe.isTargetDownEvent(*up_return));
+    EXPECT_EQ(false, fe.isTargetUpEvent(*up_return));
+  }
+  {
+    FromEvent fe(KeyCode::SHIFT_L);
+    EXPECT_EQ(true,  fe.isTargetDownEvent(*down_shift));
+    EXPECT_EQ(false, fe.isTargetUpEvent(*down_shift));
+    EXPECT_EQ(false,  fe.isTargetDownEvent(*up_shift));
+    EXPECT_EQ(true, fe.isTargetUpEvent(*up_shift));
+  }
+}
