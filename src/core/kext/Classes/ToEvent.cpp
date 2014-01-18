62521b4050f5cde1853711abd4edba7f81cc173d
Takayama Fumihiko <tekezo@pqrs.org>
2014-01-19 01:28:25 +0900

    Added ToEvent::fire_downup

--- src/core/kext/Classes/ToEvent.cpp
+++ src/core/kext/Classes/ToEvent.cpp
@@ -1,3 +1,6 @@
+#include "CommonData.hpp"
+#include "EventOutputQueue.hpp"
+#include "KeyboardRepeat.hpp"
 #include "ToEvent.hpp"
 #include "VirtualKey.hpp"
 
@@ -10,6 +13,34 @@ namespace org_pqrs_KeyRemap4MacBook {
       case Type::KEY:             return VirtualKey::isKeyLikeModifier(key_);
       case Type::CONSUMER_KEY:    return false;
       case Type::POINTING_BUTTON: return true;
+    }
+  }
+
+  void
+  ToEvent::fire_downup(Flags flags, bool add_to_keyrepeat)
+  {
+    switch (type_) {
+      case Type::NONE:
+        break;
+
+      case Type::KEY:
+      {
+        KeyboardType keyboardType = CommonData::getcurrent_keyboardType();
+
+        EventOutputQueue::FireKey::fire_downup(flags, key_, keyboardType);
+        if (add_to_keyrepeat) {
+          KeyboardRepeat::primitive_add_downup(flags, key_, keyboardType);
+        }
+        break;
+      }
+
+      case Type::CONSUMER_KEY:
+        // XXX
+        break;
+
+      case Type::POINTING_BUTTON:
+        // XXX
+        break;
     }
   }
 }
