c94c3c54ec281810cd84311cb568673110496488
Takayama Fumihiko <tekezo@pqrs.org>
2014-01-21 01:56:00 +0900

    update ToEvent::fire_downup

--- src/core/kext/Classes/ToEvent.cpp
+++ src/core/kext/Classes/ToEvent.cpp
@@ -35,12 +35,40 @@ namespace org_pqrs_KeyRemap4MacBook {
       }
 
       case Type::CONSUMER_KEY:
-        // XXX
+      {
+        {
+          Params_KeyboardSpecialEventCallback::auto_ptr ptr(Params_KeyboardSpecialEventCallback::alloc(EventType::DOWN, flags, consumer_, false));
+          if (ptr) {
+            EventOutputQueue::FireConsumer::fire(*ptr);
+          }
+        }
+        {
+          Params_KeyboardSpecialEventCallback::auto_ptr ptr(Params_KeyboardSpecialEventCallback::alloc(EventType::UP, flags, consumer_, false));
+          if (ptr) {
+            EventOutputQueue::FireConsumer::fire(*ptr);
+          }
+        }
+        if (add_to_keyrepeat) {
+          KeyboardRepeat::primitive_add(EventType::DOWN, flags, consumer_);
+          KeyboardRepeat::primitive_add(EventType::UP,   flags, consumer_);
+        }
+
         break;
+      }
 
       case Type::POINTING_BUTTON:
-        // XXX
+      {
+        FlagStatus::ScopedTemporaryFlagsChanger stfc(flags);
+
+        ButtonStatus::increase(button_);
+        EventOutputQueue::FireRelativePointer::fire(ButtonStatus::makeButtons());
+        ButtonStatus::decrease(button_);
+        EventOutputQueue::FireRelativePointer::fire(ButtonStatus::makeButtons());
+
+        // XXX: handle add_to_keyrepeat
+
         break;
+      }
     }
   }
 }
