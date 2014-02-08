d9788ae89a26a2e61e1167a9d80283e45f3bc8dd
Takayama Fumihiko <tekezo@pqrs.org>
2014-02-08 15:02:22 +0900

    public -> private @ ParamsUnion.params_

--- src/core/kext/Classes/ParamsUnion.cpp
+++ src/core/kext/Classes/ParamsUnion.cpp
@@ -4,32 +4,32 @@
 namespace org_pqrs_KeyRemap4MacBook {
   ParamsUnion::ParamsUnion(const Params_KeyboardEventCallBack& p) : type(KEYBOARD)
   {
-    params.params_KeyboardEventCallBack = Params_KeyboardEventCallBack::alloc(p);
+    params_.params_KeyboardEventCallBack = Params_KeyboardEventCallBack::alloc(p);
   }
 
   ParamsUnion::ParamsUnion(const Params_UpdateEventFlagsCallback& p) : type(UPDATE_FLAGS)
   {
-    params.params_UpdateEventFlagsCallback = Params_UpdateEventFlagsCallback::alloc(p);
+    params_.params_UpdateEventFlagsCallback = Params_UpdateEventFlagsCallback::alloc(p);
   }
 
   ParamsUnion::ParamsUnion(const Params_KeyboardSpecialEventCallback& p) : type(KEYBOARD_SPECIAL)
   {
-    params.params_KeyboardSpecialEventCallback = Params_KeyboardSpecialEventCallback::alloc(p);
+    params_.params_KeyboardSpecialEventCallback = Params_KeyboardSpecialEventCallback::alloc(p);
   }
 
   ParamsUnion::ParamsUnion(const Params_RelativePointerEventCallback& p) : type(RELATIVE_POINTER)
   {
-    params.params_RelativePointerEventCallback = Params_RelativePointerEventCallback::alloc(p);
+    params_.params_RelativePointerEventCallback = Params_RelativePointerEventCallback::alloc(p);
   }
 
   ParamsUnion::ParamsUnion(const Params_ScrollWheelEventCallback& p) : type(SCROLL_WHEEL)
   {
-    params.params_ScrollWheelEventCallback = Params_ScrollWheelEventCallback::alloc(p);
+    params_.params_ScrollWheelEventCallback = Params_ScrollWheelEventCallback::alloc(p);
   }
 
   ParamsUnion::ParamsUnion(const Params_Wait& p) : type(WAIT)
   {
-    params.params_Wait = Params_Wait::alloc(p);
+    params_.params_Wait = Params_Wait::alloc(p);
   }
 
   ParamsUnion::~ParamsUnion(void)
@@ -42,22 +42,22 @@ namespace org_pqrs_KeyRemap4MacBook {
 
     switch (type) {
       case KEYBOARD:
-        DELETE_PARAMS(params.params_KeyboardEventCallBack);
+        DELETE_PARAMS(params_.params_KeyboardEventCallBack);
         break;
       case UPDATE_FLAGS:
-        DELETE_PARAMS(params.params_UpdateEventFlagsCallback);
+        DELETE_PARAMS(params_.params_UpdateEventFlagsCallback);
         break;
       case KEYBOARD_SPECIAL:
-        DELETE_PARAMS(params.params_KeyboardSpecialEventCallback);
+        DELETE_PARAMS(params_.params_KeyboardSpecialEventCallback);
         break;
       case RELATIVE_POINTER:
-        DELETE_PARAMS(params.params_RelativePointerEventCallback);
+        DELETE_PARAMS(params_.params_RelativePointerEventCallback);
         break;
       case SCROLL_WHEEL:
-        DELETE_PARAMS(params.params_ScrollWheelEventCallback);
+        DELETE_PARAMS(params_.params_ScrollWheelEventCallback);
         break;
       case WAIT:
-        DELETE_PARAMS(params.params_Wait);
+        DELETE_PARAMS(params_.params_Wait);
         break;
       default:
         IOLOG_ERROR("ParamsUnion::~ParamsUnion() unknown type\n");
