961e43ba44296feac6fcf7247142ebb57d594bb4
Takayama Fumihiko <tekezo@pqrs.org>
2014-03-01 12:50:23 +0900

    add notification @ ReloadXML.app

--- files/Utilities/ReloadXML.app/Contents/MacOS/core.sh
+++ files/Utilities/ReloadXML.app/Contents/MacOS/core.sh
@@ -2,4 +2,6 @@
 
 /Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli \
     reloadxml
+/usr/bin/osascript -e 'display notification "Reloaded" with title "KeyRemap4MacBook"';
+
 exit 0
