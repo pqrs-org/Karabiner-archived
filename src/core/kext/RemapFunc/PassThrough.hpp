28eee9a45bc163918e2184b611b91e81771b7dea
Takayama Fumihiko <tekezo@pqrs.org>
2014-03-06 00:11:16 +0900

    Added PassThrough.hpp

---
+++ src/core/kext/RemapFunc/PassThrough.hpp
@@ -0,0 +1,14 @@
+#ifndef PASSTHROUGH_HPP
+#define PASSTHROUGH_HPP
+
+namespace org_pqrs_KeyRemap4MacBook {
+  namespace RemapFunc {
+    class PassThrough {
+    public:
+      void add(unsigned int datatype, unsigned int newval) {}
+    };
+  }
+}
+
+#endif
+
