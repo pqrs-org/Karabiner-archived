c8de4ded9430a0dc9d73188af0bba9af81341dd2
Takayama Fumihiko <tekezo@pqrs.org>
2014-03-15 03:20:06 +0900

    improved warp-mouse-cursor-position

--- src/util/warp-mouse-cursor-position/warp-mouse-cursor-position/main.m
+++ src/util/warp-mouse-cursor-position/warp-mouse-cursor-position/main.m
@@ -17,14 +17,49 @@
 - (void) usage
 {
   [self output:@"Usage:\n"];
-  [self output:@"  warp-mouse-cursor-position screen NUM TOP LEFT\n"];
-  [self output:@"  warp-mouse-cursor-position front_window TOP LEFT\n"];
+  [self output:@"  warp-mouse-cursor-position screen NUM VERTICAL X_ADJUSTMENT HORIZONTAL Y_ADJUSTMENT\n"];
+  [self output:@"  warp-mouse-cursor-position front_window VERTICAL X_ADJUSTMENT HORIZONTAL Y_ADJUSTMENT\n"];
   [self output:@"\n"];
   [self output:@"Example:\n"];
-  [self output:@"  warp-mouse-cursor-position screen 0 300 400\n"];
-  [self output:@"  warp-mouse-cursor-position front_window 100 10\n"];
+  [self output:@"  warp-mouse-cursor-position screen 0 top +10 left +20\n"];
+  [self output:@"  warp-mouse-cursor-position screen 0 middle +0 center +0\n"];
+  [self output:@"  warp-mouse-cursor-position screen 0 bottom -10 right -20\n"];
+  [self output:@"  warp-mouse-cursor-position front_window top +10 left +20\n"];
+  [self output:@"  warp-mouse-cursor-position front_window middle +0 center +0\n"];
+  [self output:@"  warp-mouse-cursor-position front_window bottom -10 right -20\n"];
+  [self output:@"\n"];
+  [self output:@"VERTICAL:\n"];
+  [self output:@"  top middle bottom\n"];
+  [self output:@"\n"];
+  [self output:@"HORIZONTAL:\n"];
+  [self output:@"  left center right\n"];
 
   [[NSApplication sharedApplication] terminate:nil];
+}
+
+- (CGPoint) position:(CGRect)frame vertical:(NSString*)vertical x:(CGFloat)x horizontal:(NSString*)horizontal y:(CGFloat)y
+{
+  CGPoint position = CGPointZero;
+
+  if ([vertical isEqualToString:@"middle"]) {
+    position.y = frame.origin.y + (frame.size.height / 2);
+  } else if ([vertical isEqualToString:@"bottom"]) {
+    position.y = frame.origin.y + frame.size.height;
+  } else {
+    position.y = frame.origin.y;
+  }
+  position.y += y;
+
+  if ([horizontal isEqualToString:@"center"]) {
+    position.x = frame.origin.x + (frame.size.width / 2);
+  } else if ([horizontal isEqualToString:@"right"]) {
+    position.x = frame.origin.x + frame.size.width;
+  } else {
+    position.x = frame.origin.x;
+  }
+  position.x += x;
+
+  return position;
 }
 
 - (int) main
@@ -37,18 +72,25 @@
     @try {
       NSString* command = arguments[1];
       if ([command isEqualToString:@"screen"]) {
-        if ([arguments count] != 5) {
+        if ([arguments count] != 7) {
           [self usage];
         } else {
-          NSUInteger number = [arguments[2] integerValue];
-          CGFloat top       = [arguments[3] floatValue];
-          CGFloat left      = [arguments[4] floatValue];
+          NSUInteger number    = [arguments[2] integerValue];
+          NSString* vertical   = arguments[3];
+          CGFloat x            = [arguments[4] floatValue];
+          NSString* horizontal = arguments[5];
+          CGFloat y            = [arguments[6] floatValue];
 
           NSArray* screens = [NSScreen screens];
           if (number <= [screens count] - 1) {
             NSScreen* screen = screens[number];
-            CGWarpMouseCursorPosition(CGPointMake(screen.frame.origin.x + top,
-                                                  screen.frame.origin.y + left));
+            CGPoint position = [self position:screen.frame
+                                     vertical:vertical
+                                            x:x
+                                   horizontal:horizontal
+                                            y:y];
+            CGWarpMouseCursorPosition(position);
+
           } else {
             [self output:[NSString stringWithFormat:@"screen number should be 0-%ld\n", [screens count] - 1]];
             return 1;
@@ -56,11 +98,13 @@
         }
 
       } else if ([command isEqualToString:@"front_window"]) {
-        if ([arguments count] != 4) {
+        if ([arguments count] != 6) {
           [self usage];
         } else {
-          CGFloat top  = [arguments[2] floatValue];
-          CGFloat left = [arguments[3] floatValue];
+          NSString* vertical   = arguments[2];
+          CGFloat x            = [arguments[3] floatValue];
+          NSString* horizontal = arguments[4];
+          CGFloat y            = [arguments[5] floatValue];
 
           pid_t pid = [[[NSWorkspace sharedWorkspace] frontmostApplication] processIdentifier];
 
@@ -79,9 +123,14 @@
                 continue;
               }
 
-              NSDictionary* rect = window[(__bridge NSString*)(kCGWindowBounds)];
-              CGWarpMouseCursorPosition(CGPointMake([rect[@"X"] floatValue] + top,
-                                                    [rect[@"Y"] floatValue] + left));
+              CGRect frame = CGRectZero;
+              CGRectMakeWithDictionaryRepresentation((__bridge CFDictionaryRef)(window[(__bridge NSString*)(kCGWindowBounds)]), &frame);
+              CGPoint position = [self position:frame
+                                       vertical:vertical
+                                              x:x
+                                     horizontal:horizontal
+                                              y:y];
+              CGWarpMouseCursorPosition(position);
               break;
             }
           }
