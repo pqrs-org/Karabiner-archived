59adf43116fa7e3e07cf5768f6fe0f20310b9ef9
Takayama Fumihiko <tekezo@pqrs.org>
2014-03-28 00:26:22 +0900

    implement KeyUpEventToKey

--- src/core/kext/RemapFunc/KeyUpEventToKey.cpp
+++ src/core/kext/RemapFunc/KeyUpEventToKey.cpp
@@ -2,15 +2,65 @@
 
 namespace org_pqrs_KeyRemap4MacBook {
   namespace RemapFunc {
+    KeyUpEventToKey::KeyUpEventToKey(void)
+    {
+      keytokey_.add(KeyCode::VK_PSEUDO_KEY);
+    }
+
     void
     KeyUpEventToKey::add(unsigned int datatype, unsigned int newval)
     {
-      keytokey_.add(datatype, newval);
+      switch (datatype) {
+        case BRIDGE_DATATYPE_KEYCODE:
+        case BRIDGE_DATATYPE_CONSUMERKEYCODE:
+        case BRIDGE_DATATYPE_POINTINGBUTTON:
+        {
+          if (fromEvent_.getType() == FromEvent::Type::NONE) {
+            fromEvent_ = FromEvent(datatype, newval);
+          } else {
+            keytokey_.add(datatype, newval);
+          }
+          break;
+        }
+
+        case BRIDGE_DATATYPE_FLAGS:
+        {
+          if (keytokey_.toKeysSize() == 0) {
+            fromFlags_ = Flags(newval);
+          } else {
+            keytokey_.add(datatype, newval);
+          }
+          break;
+        }
+
+        default:
+          keytokey_.add(datatype, newval);
+          break;
+      }
     }
 
     bool
     KeyUpEventToKey::remap(RemapParams& remapParams)
     {
+      if (remapParams.isremapped) return false;
+
+      if (fromEvent_.isTargetDownEvent(remapParams.paramsUnion)) {
+        remapParams.isremapped = true;
+        return true;
+      }
+
+      if (fromEvent_.isTargetUpEvent(remapParams.paramsUnion) &&
+          FlagStatus::makeFlags().isOn(fromFlags_)) {
+        remapParams.isremapped = true;
+
+        FlagStatus::decrease(fromFlags_);
+        keytokey_.call_remap_with_VK_PSEUDO_KEY(EventType::DOWN);
+        keytokey_.call_remap_with_VK_PSEUDO_KEY(EventType::UP);
+        FlagStatus::increase(fromFlags_);
+
+        return true;
+      }
+
       return false;
     }
   }
