4495e2a1fd24a5540652b6ff685922d8375dcbb3
Takayama Fumihiko <tekezo@pqrs.org>
2014-04-18 00:47:13 +0900

    use KeyCodeModifierFlagPairs @ VK_LOCK, VK_STICKY

--- src/core/kext/VirtualKey/VK_LOCK.cpp
+++ src/core/kext/VirtualKey/VK_LOCK.cpp
@@ -2,80 +2,57 @@
 
 #include "EventOutputQueue.hpp"
 #include "FlagStatus.hpp"
+#include "KeyCodeModifierFlagPairs.hpp"
 #include "VK_LOCK.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
   bool
   VirtualKey::VK_LOCK::handle(const Params_KeyboardEventCallBack& params)
   {
-    ModifierFlag modifierFlag;
-    bool force_on = false;
-    bool force_off = false;
+    bool isFirstKeyDownEvent = (params.ex_iskeydown && params.repeat == false);
 
-    /**/ if (params.key == KeyCode::VK_LOCK_COMMAND_L) { modifierFlag = ModifierFlag::COMMAND_L; }
-    else if (params.key == KeyCode::VK_LOCK_COMMAND_R) { modifierFlag = ModifierFlag::COMMAND_R; }
-    else if (params.key == KeyCode::VK_LOCK_CONTROL_L) { modifierFlag = ModifierFlag::CONTROL_L; }
-    else if (params.key == KeyCode::VK_LOCK_CONTROL_R) { modifierFlag = ModifierFlag::CONTROL_R; }
-    else if (params.key == KeyCode::VK_LOCK_FN)        { modifierFlag = ModifierFlag::FN;        }
-    else if (params.key == KeyCode::VK_LOCK_OPTION_L)  { modifierFlag = ModifierFlag::OPTION_L;  }
-    else if (params.key == KeyCode::VK_LOCK_OPTION_R)  { modifierFlag = ModifierFlag::OPTION_R;  }
-    else if (params.key == KeyCode::VK_LOCK_SHIFT_L)   { modifierFlag = ModifierFlag::SHIFT_L;   }
-    else if (params.key == KeyCode::VK_LOCK_SHIFT_R)   { modifierFlag = ModifierFlag::SHIFT_R;   }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA1)    { modifierFlag = ModifierFlag::EXTRA1;    }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA2)    { modifierFlag = ModifierFlag::EXTRA2;    }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA3)    { modifierFlag = ModifierFlag::EXTRA3;    }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA4)    { modifierFlag = ModifierFlag::EXTRA4;    }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA5)    { modifierFlag = ModifierFlag::EXTRA5;    }
-
-    else if (params.key == KeyCode::VK_LOCK_COMMAND_L_FORCE_ON) { modifierFlag = ModifierFlag::COMMAND_L; force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_COMMAND_R_FORCE_ON) { modifierFlag = ModifierFlag::COMMAND_R; force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_CONTROL_L_FORCE_ON) { modifierFlag = ModifierFlag::CONTROL_L; force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_CONTROL_R_FORCE_ON) { modifierFlag = ModifierFlag::CONTROL_R; force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_FN_FORCE_ON)        { modifierFlag = ModifierFlag::FN;        force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_OPTION_L_FORCE_ON)  { modifierFlag = ModifierFlag::OPTION_L;  force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_OPTION_R_FORCE_ON)  { modifierFlag = ModifierFlag::OPTION_R;  force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_SHIFT_L_FORCE_ON)   { modifierFlag = ModifierFlag::SHIFT_L;   force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_SHIFT_R_FORCE_ON)   { modifierFlag = ModifierFlag::SHIFT_R;   force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA1_FORCE_ON)    { modifierFlag = ModifierFlag::EXTRA1;    force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA2_FORCE_ON)    { modifierFlag = ModifierFlag::EXTRA2;    force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA3_FORCE_ON)    { modifierFlag = ModifierFlag::EXTRA3;    force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA4_FORCE_ON)    { modifierFlag = ModifierFlag::EXTRA4;    force_on = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA5_FORCE_ON)    { modifierFlag = ModifierFlag::EXTRA5;    force_on = true; }
-
-    else if (params.key == KeyCode::VK_LOCK_COMMAND_L_FORCE_OFF) { modifierFlag = ModifierFlag::COMMAND_L; force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_COMMAND_R_FORCE_OFF) { modifierFlag = ModifierFlag::COMMAND_R; force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_CONTROL_L_FORCE_OFF) { modifierFlag = ModifierFlag::CONTROL_L; force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_CONTROL_R_FORCE_OFF) { modifierFlag = ModifierFlag::CONTROL_R; force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_FN_FORCE_OFF)        { modifierFlag = ModifierFlag::FN;        force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_OPTION_L_FORCE_OFF)  { modifierFlag = ModifierFlag::OPTION_L;  force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_OPTION_R_FORCE_OFF)  { modifierFlag = ModifierFlag::OPTION_R;  force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_SHIFT_L_FORCE_OFF)   { modifierFlag = ModifierFlag::SHIFT_L;   force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_SHIFT_R_FORCE_OFF)   { modifierFlag = ModifierFlag::SHIFT_R;   force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA1_FORCE_OFF)    { modifierFlag = ModifierFlag::EXTRA1;    force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA2_FORCE_OFF)    { modifierFlag = ModifierFlag::EXTRA2;    force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA3_FORCE_OFF)    { modifierFlag = ModifierFlag::EXTRA3;    force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA4_FORCE_OFF)    { modifierFlag = ModifierFlag::EXTRA4;    force_off = true; }
-    else if (params.key == KeyCode::VK_LOCK_EXTRA5_FORCE_OFF)    { modifierFlag = ModifierFlag::EXTRA5;    force_off = true; }
-
-    else if (params.key == KeyCode::VK_LOCK_ALL_FORCE_OFF) {
-      FlagStatus::globalFlagStatus().lock_clear();
-      EventOutputQueue::FireModifiers::fire();
-      return true;
-    } else {
-      return false;
-    }
-
-    if (params.ex_iskeydown && params.repeat == false) {
-      if (force_off) {
-        FlagStatus::globalFlagStatus().lock_decrease(modifierFlag);
-      } else if (force_on) {
-        FlagStatus::globalFlagStatus().lock_increase(modifierFlag);
-      } else {
-        FlagStatus::globalFlagStatus().lock_toggle(modifierFlag);
+    {
+      ModifierFlag modifierFlag =
+        KeyCodeModifierFlagPairs::getModifierFlag(params.key, KeyCodeModifierFlagPairs::KeyCodeType::VK_LOCK);
+      if (modifierFlag != ModifierFlag::ZERO) {
+        if (isFirstKeyDownEvent) {
+          FlagStatus::globalFlagStatus().lock_toggle(modifierFlag);
+        }
+        goto remapped;
       }
-      EventOutputQueue::FireModifiers::fire();
+    }
+    {
+      ModifierFlag modifierFlag =
+        KeyCodeModifierFlagPairs::getModifierFlag(params.key, KeyCodeModifierFlagPairs::KeyCodeType::VK_LOCK_FORCE_ON);
+      if (modifierFlag != ModifierFlag::ZERO) {
+        if (isFirstKeyDownEvent) {
+          FlagStatus::globalFlagStatus().lock_increase(modifierFlag);
+        }
+        goto remapped;
+      }
+    }
+    {
+      ModifierFlag modifierFlag =
+        KeyCodeModifierFlagPairs::getModifierFlag(params.key, KeyCodeModifierFlagPairs::KeyCodeType::VK_LOCK_FORCE_OFF);
+      if (modifierFlag != ModifierFlag::ZERO) {
+        if (isFirstKeyDownEvent) {
+          FlagStatus::globalFlagStatus().lock_decrease(modifierFlag);
+        }
+        goto remapped;
+      }
     }
 
+    if (params.key == KeyCode::VK_LOCK_ALL_FORCE_OFF) {
+      if (isFirstKeyDownEvent) {
+        FlagStatus::globalFlagStatus().lock_clear();
+      }
+      goto remapped;
+    }
+
+    return false;
+
+  remapped:
+    EventOutputQueue::FireModifiers::fire();
     return true;
   }
 }
--- src/core/kext/VirtualKey/VK_STICKY.cpp
+++ src/core/kext/VirtualKey/VK_STICKY.cpp
@@ -2,76 +2,57 @@
 
 #include "EventOutputQueue.hpp"
 #include "FlagStatus.hpp"
+#include "KeyCodeModifierFlagPairs.hpp"
 #include "VK_STICKY.hpp"
 
 namespace org_pqrs_KeyRemap4MacBook {
   bool
   VirtualKey::VK_STICKY::handle(const Params_KeyboardEventCallBack& params)
   {
-    ModifierFlag flag = ModifierFlag::ZERO;
-    bool force_on = false;
-    bool force_off = false;
+    bool isFirstKeyDownEvent = (params.ex_iskeydown && params.repeat == false);
 
-    /**/ if (params.key == KeyCode::VK_STICKY_COMMAND_L)           { flag = ModifierFlag::COMMAND_L; }
-    else if (params.key == KeyCode::VK_STICKY_COMMAND_R)           { flag = ModifierFlag::COMMAND_R; }
-    else if (params.key == KeyCode::VK_STICKY_CONTROL_L)           { flag = ModifierFlag::CONTROL_L; }
-    else if (params.key == KeyCode::VK_STICKY_CONTROL_R)           { flag = ModifierFlag::CONTROL_R; }
-    else if (params.key == KeyCode::VK_STICKY_FN)                  { flag = ModifierFlag::FN;        }
-    else if (params.key == KeyCode::VK_STICKY_OPTION_L)            { flag = ModifierFlag::OPTION_L;  }
-    else if (params.key == KeyCode::VK_STICKY_OPTION_R)            { flag = ModifierFlag::OPTION_R;  }
-    else if (params.key == KeyCode::VK_STICKY_SHIFT_L)             { flag = ModifierFlag::SHIFT_L;   }
-    else if (params.key == KeyCode::VK_STICKY_SHIFT_R)             { flag = ModifierFlag::SHIFT_R;   }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA1)              { flag = ModifierFlag::EXTRA1;    }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA2)              { flag = ModifierFlag::EXTRA2;    }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA3)              { flag = ModifierFlag::EXTRA3;    }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA4)              { flag = ModifierFlag::EXTRA4;    }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA5)              { flag = ModifierFlag::EXTRA5;    }
-
-    else if (params.key == KeyCode::VK_STICKY_COMMAND_L_FORCE_ON)  { flag = ModifierFlag::COMMAND_L; force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_COMMAND_R_FORCE_ON)  { flag = ModifierFlag::COMMAND_R; force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_CONTROL_L_FORCE_ON)  { flag = ModifierFlag::CONTROL_L; force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_CONTROL_R_FORCE_ON)  { flag = ModifierFlag::CONTROL_R; force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_FN_FORCE_ON)         { flag = ModifierFlag::FN;        force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_OPTION_L_FORCE_ON)   { flag = ModifierFlag::OPTION_L;  force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_OPTION_R_FORCE_ON)   { flag = ModifierFlag::OPTION_R;  force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_SHIFT_L_FORCE_ON)    { flag = ModifierFlag::SHIFT_L;   force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_SHIFT_R_FORCE_ON)    { flag = ModifierFlag::SHIFT_R;   force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA1_FORCE_ON)     { flag = ModifierFlag::EXTRA1;    force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA2_FORCE_ON)     { flag = ModifierFlag::EXTRA2;    force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA3_FORCE_ON)     { flag = ModifierFlag::EXTRA3;    force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA4_FORCE_ON)     { flag = ModifierFlag::EXTRA4;    force_on = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA5_FORCE_ON)     { flag = ModifierFlag::EXTRA5;    force_on = true; }
-
-    else if (params.key == KeyCode::VK_STICKY_COMMAND_L_FORCE_OFF) { flag = ModifierFlag::COMMAND_L; force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_COMMAND_R_FORCE_OFF) { flag = ModifierFlag::COMMAND_R; force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_CONTROL_L_FORCE_OFF) { flag = ModifierFlag::CONTROL_L; force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_CONTROL_R_FORCE_OFF) { flag = ModifierFlag::CONTROL_R; force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_FN_FORCE_OFF)        { flag = ModifierFlag::FN;        force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_OPTION_L_FORCE_OFF)  { flag = ModifierFlag::OPTION_L;  force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_OPTION_R_FORCE_OFF)  { flag = ModifierFlag::OPTION_R;  force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_SHIFT_L_FORCE_OFF)   { flag = ModifierFlag::SHIFT_L;   force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_SHIFT_R_FORCE_OFF)   { flag = ModifierFlag::SHIFT_R;   force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA1_FORCE_OFF)    { flag = ModifierFlag::EXTRA1;    force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA2_FORCE_OFF)    { flag = ModifierFlag::EXTRA2;    force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA3_FORCE_OFF)    { flag = ModifierFlag::EXTRA3;    force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA4_FORCE_OFF)    { flag = ModifierFlag::EXTRA4;    force_off = true; }
-    else if (params.key == KeyCode::VK_STICKY_EXTRA5_FORCE_OFF)    { flag = ModifierFlag::EXTRA5;    force_off = true; }
-
-    else {
-      return false;
-    }
-
-    if (params.ex_iskeydown && params.repeat == false) {
-      if (force_off) {
-        FlagStatus::globalFlagStatus().sticky_decrease(flag);
-      } else if (force_on) {
-        FlagStatus::globalFlagStatus().sticky_increase(flag);
-      } else {
-        FlagStatus::globalFlagStatus().sticky_toggle(flag);
+    {
+      ModifierFlag modifierFlag =
+        KeyCodeModifierFlagPairs::getModifierFlag(params.key, KeyCodeModifierFlagPairs::KeyCodeType::VK_STICKY);
+      if (modifierFlag != ModifierFlag::ZERO) {
+        if (isFirstKeyDownEvent) {
+          FlagStatus::globalFlagStatus().sticky_toggle(modifierFlag);
+        }
+        goto remapped;
       }
-      EventOutputQueue::FireModifiers::fire();
+    }
+    {
+      ModifierFlag modifierFlag =
+        KeyCodeModifierFlagPairs::getModifierFlag(params.key, KeyCodeModifierFlagPairs::KeyCodeType::VK_STICKY_FORCE_ON);
+      if (modifierFlag != ModifierFlag::ZERO) {
+        if (isFirstKeyDownEvent) {
+          FlagStatus::globalFlagStatus().sticky_increase(modifierFlag);
+        }
+        goto remapped;
+      }
+    }
+    {
+      ModifierFlag modifierFlag =
+        KeyCodeModifierFlagPairs::getModifierFlag(params.key, KeyCodeModifierFlagPairs::KeyCodeType::VK_STICKY_FORCE_OFF);
+      if (modifierFlag != ModifierFlag::ZERO) {
+        if (isFirstKeyDownEvent) {
+          FlagStatus::globalFlagStatus().sticky_decrease(modifierFlag);
+        }
+        goto remapped;
+      }
     }
 
+    if (params.key == KeyCode::VK_STICKY_ALL_FORCE_OFF) {
+      if (isFirstKeyDownEvent) {
+        FlagStatus::globalFlagStatus().sticky_clear();
+      }
+      goto remapped;
+    }
+
+    return false;
+
+  remapped:
+    EventOutputQueue::FireModifiers::fire();
     return true;
   }
 }
