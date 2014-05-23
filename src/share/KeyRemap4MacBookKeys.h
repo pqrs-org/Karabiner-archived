86d428406c3278ee1f9691c7d5ee7b166286bac9
Takayama Fumihiko <tekezo@pqrs.org>
2014-05-23 15:50:32 +0900

    send kKeyRemap4MacBookServerDidLaunchNotification

--- src/share/KeyRemap4MacBookKeys.h
+++ src/share/KeyRemap4MacBookKeys.h
@@ -1,6 +1,9 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
 // ------------------------------------------------------------
+// NSDistributedNotificationCenter for EventViewer.app, AXNotifier.app, multitouchextension.app.
+#define kKeyRemap4MacBookServerDidLaunchNotification @"org.pqrs.KeyRemap4MacBook.notification.serverDidLaunch"
+
 // NSDistributedNotificationCenter for EventViewer.app
 #define kKeyRemap4MacBookApplicationChangedNotification @"org.pqrs.KeyRemap4MacBook.notification.applicationChanged"
 #define kKeyRemap4MacBookInputSourceChangedNotification @"org.pqrs.KeyRemap4MacBook.notification.inputSourceChanged"
