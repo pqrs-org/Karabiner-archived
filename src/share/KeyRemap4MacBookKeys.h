631415e872560eba66b117b7bf6ef9f5e07e260b
Takayama Fumihiko <tekezo@pqrs.org>
2014-05-25 10:16:38 +0900

    remove kKeyRemap4MacBookAXTitleChangedNotification

--- src/share/KeyRemap4MacBookKeys.h
+++ src/share/KeyRemap4MacBookKeys.h
@@ -8,9 +8,6 @@
 #define kKeyRemap4MacBookApplicationChangedNotification @"org.pqrs.KeyRemap4MacBook.notification.applicationChanged"
 #define kKeyRemap4MacBookInputSourceChangedNotification @"org.pqrs.KeyRemap4MacBook.notification.inputSourceChanged"
 
-// NSDistributedNotificationCenter from AXNotifier.app
-#define kKeyRemap4MacBookAXTitleChangedNotification @"org.pqrs.KeyRemap4MacBook.notification.AXTitleChanged"
-
 // ------------------------------------------------------------
 // NSConnection
 #define kKeyRemap4MacBookConnectionName                 @"org.pqrs.KeyRemap4MacBook.server"
