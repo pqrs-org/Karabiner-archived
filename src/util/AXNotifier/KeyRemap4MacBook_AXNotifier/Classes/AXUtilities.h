ee51b492a3b721320249e9c4b6bbadb7739098a9
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-06 00:48:37 +0900

    Added subroleOfUIElement

--- src/util/AXNotifier/KeyRemap4MacBook_AXNotifier/Classes/AXUtilities.h
+++ src/util/AXNotifier/KeyRemap4MacBook_AXNotifier/Classes/AXUtilities.h
@@ -10,4 +10,13 @@
 + (NSString*) titleOfUIElement:(AXUIElementRef)element;
 + (NSString*) roleOfUIElement:(AXUIElementRef)element;
 
+/*
+ * About NSAccessibilitySubroleAttribute:
+ *
+ * We cannot use subrole to know whether user are editing text or not in web browser (eg. Google Chrome).
+ * The subrole is same value (AXStandardWindow) in browser
+ * even if the focused element is <textarea>, <input> or other elements.
+ */
++ (NSString*) subroleOfUIElement:(AXUIElementRef)element;
+
 @end
