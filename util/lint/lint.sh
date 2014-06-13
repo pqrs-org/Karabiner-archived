2dc30311c483c729c3a83affd0ce1440888b8ebe
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-13 16:29:24 +0900

    clean up lint.sh

--- util/lint/lint.sh
+++ util/lint/lint.sh
@@ -5,19 +5,8 @@ basedir=`dirname $0`
 ############################################################
 # xcodeproj
 for f in `find $basedir/../../* -name 'project.pbxproj'`; do
-    if [ "$(basename $(dirname $f))" = "KeyRemap4MacBookTest.xcodeproj" ]; then
-        echo "Skip $f"
-        continue
-    fi
-
     echo "Check $f"
-
-    if [ "$(basename $(dirname $(dirname $f)))/$(basename $(dirname $f))" = "prefpane/KeyRemap4MacBook.xcodeproj" ]; then
-        # prefpane needs GC.
-        "$basedir/xcodeproj.rb" GCC_ENABLE_OBJC_GC < $f || exit 1
-    else
-        "$basedir/xcodeproj.rb" < $f || exit 1
-    fi
+    "$basedir/xcodeproj.rb" < $f || exit 1
 done
 
 ############################################################
