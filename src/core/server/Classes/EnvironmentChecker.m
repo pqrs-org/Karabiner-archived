cb7dc7531ee43524f7465b26fca19cdb73d0885c
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-17 21:22:09 +0900

    improved EnvironmentChecker

--- src/core/server/Classes/EnvironmentChecker.m
+++ src/core/server/Classes/EnvironmentChecker.m
@@ -2,74 +2,79 @@
 
 @implementation EnvironmentChecker
 
-static NSMutableDictionary* cache_;
-
-+ (void) initialize
-{
-  cache_ = [NSMutableDictionary new];
-}
-
 + (BOOL) checkDoubleCommand
 {
-  NSString* cachekey = @"DoubleCommand";
-  if (cache_[cachekey] != nil) {
-    return cache_[cachekey];
+  @synchronized(self) {
+    static dispatch_once_t onceToken = 0;
+    static BOOL result = NO;
+
+    dispatch_once(&onceToken, ^{
+      NSLog(@"checkDoubleCommand");
+
+      // Check file system.
+      // (Do not determine by kextstat because loading kext might be lazy loading.)
+      NSArray* paths = @[ @"/Library/StartupItems/DoubleCommand" ];
+      for (NSString* path in paths) {
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+          result = YES;
+          break;
+        }
+      }
+    });
+
+    return result;
   }
-
-  // ----------------------------------------
-  cache_[cachekey] = @NO;
-
-  NSArray* paths = @[ @"/Library/StartupItems/DoubleCommand" ];
-  for (NSString* path in paths) {
-    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-      cache_[cachekey] = @YES;
-      break;
-    }
-  }
-
-  return cache_[cachekey];
 }
 
 + (BOOL) checkKeyRemap4MacBook
 {
-  NSString* cachekey = @"KeyRemap4MacBook";
-  if (cache_[cachekey] != nil) {
-    return cache_[cachekey];
+  @synchronized(self) {
+    static dispatch_once_t onceToken = 0;
+    static BOOL result = NO;
+
+    dispatch_once(&onceToken, ^{
+      NSLog(@"checkKeyRemap4MacBook");
+
+      // Check kextstat.
+      //
+      // KeyRemap4MacBook and Karabiner are running together only when upgrading
+      // (KeyRemap4MacBook -> Karabiner) and failed to unload kext in installer.
+      //
+      // So, KeyRemap4MacBook kext has been removed from file system by installer.
+      // We have to check kext by kextstat.
+
+      if (system("/bin/test -n \"`/usr/sbin/kextstat -l -b org.pqrs.driver.KeyRemap4MacBook`\"") == 0) {
+        result = YES;
+      }
+    });
+
+    return result;
   }
-
-  // ----------------------------------------
-  cache_[cachekey] = @NO;
-
-  NSString* command = @"/bin/test -n \"`/usr/sbin/kextstat -l -b org.pqrs.driver.KeyRemap4MacBook`\"";
-  NSTask* task = [NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:@[@"-c", command]];
-  [task waitUntilExit];
-  if ([task terminationStatus] == 0) {
-    cache_[cachekey] = @YES;
-  }
-
-  return cache_[cachekey];
 }
 
 + (BOOL) checkSmoothMouse
 {
-  NSString* cachekey = @"SmoothMouse";
-  if (cache_[cachekey] != nil) {
-    return cache_[cachekey];
+  @synchronized(self) {
+    static dispatch_once_t onceToken = 0;
+    static BOOL result = NO;
+
+    dispatch_once(&onceToken, ^{
+      NSLog(@"checkSmoothMouse");
+
+      // Check file system.
+      // (Do not determine by kextstat because loading kext might be lazy loading.)
+      NSArray* paths = @[ @"/System/Library/Extensions/SmoothMouse.kext",
+                          @"/Library/PreferencePanes/SmoothMouse.prefPane" ];
+      for (NSString* path in paths) {
+        if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+          result = YES;
+          break;
+        }
+      }
+    });
+
+    return result;
   }
-
-  // ----------------------------------------
-  cache_[cachekey] = @NO;
-
-  NSArray* paths = @[ @"/System/Library/Extensions/SmoothMouse.kext",
-                       @"/Library/PreferencePanes/SmoothMouse.prefPane" ];
-  for (NSString* path in paths) {
-    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
-      cache_[cachekey] = @YES;
-      break;
-    }
-  }
-
-  return cache_[cachekey];
 }
 
 @end
