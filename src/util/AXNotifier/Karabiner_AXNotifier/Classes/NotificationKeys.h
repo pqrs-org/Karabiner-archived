5929f2511a0a890d3420dff63825ef218524fc7b
Takayama Fumihiko <tekezo@pqrs.org>
2014-06-29 23:04:10 +0900

    Add NotificationKeys

---
+++ src/util/AXNotifier/Karabiner_AXNotifier/Classes/NotificationKeys.h
@@ -0,0 +1,4 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+#define kFocusedUIElementChanged @ "kFocusedUIElementChanged"
+#define kWindowVisibilityChanged @ "kWindowVisibilityChanged"
