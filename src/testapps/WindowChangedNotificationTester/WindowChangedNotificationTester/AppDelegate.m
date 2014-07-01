72a8593028bc54bda1dc233b460ca5ee5873a923
Takayama Fumihiko <tekezo@pqrs.org>
2014-07-01 09:15:39 +0900

    Added WindowChangedNotificationTester

---
+++ src/testapps/WindowChangedNotificationTester/WindowChangedNotificationTester/AppDelegate.m
@@ -0,0 +1,39 @@
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void) timerFireMethod:(NSTimer*)timer
+{
+  dispatch_async(dispatch_get_main_queue(), ^{
+    @synchronized(self) {
+      static int counter = 0;
+      ++counter;
+      if (counter % 2 == 0) {
+        [self.window1 makeKeyAndOrderFront:self];
+      } else {
+        [self.window2 makeKeyAndOrderFront:self];
+      }
+    }
+  });
+}
+
+- (void) applicationDidFinishLaunching:(NSNotification*)aNotification
+{
+  NSRect rect = [[NSScreen mainScreen] frame];
+
+  [self.window1 setTitle:@"window1"];
+  [self.window1 setLevel:NSFloatingWindowLevel];
+  [self.window1 setFrameTopLeftPoint:NSMakePoint(100, rect.size.height - 100)];
+
+  [self.window2 setTitle:@"window2"];
+  [self.window2 setLevel:NSFloatingWindowLevel];
+  [self.window2 setFrameTopLeftPoint:NSMakePoint(200, rect.size.height - 400)];
+
+  [NSTimer scheduledTimerWithTimeInterval:1
+                                   target:self
+                                 selector:@selector(timerFireMethod:)
+                                 userInfo:nil
+                                  repeats:YES];
+}
+
+@end
