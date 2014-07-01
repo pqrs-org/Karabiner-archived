3d6aefd9b73a409acb6247780d175356b4a4c0cd
Takayama Fumihiko <tekezo@pqrs.org>
2014-07-01 09:15:39 +0900

    Added WindowChangedNotificationTester

---
+++ src/testapps/WindowChangedNotificationTester/WindowChangedNotificationTester/AppDelegate.h
@@ -0,0 +1,10 @@
+// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow* window1;
+@property (assign) IBOutlet NSWindow* window2;
+
+@end
