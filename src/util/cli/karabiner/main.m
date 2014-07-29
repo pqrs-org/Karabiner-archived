12d8611314b0e51086526f048aa9d7e711e1dacb
Takayama Fumihiko <tekezo@pqrs.org>
2014-07-29 20:32:49 +0900

    add append,rename,delete into cli

--- src/util/cli/karabiner/main.m
+++ src/util/cli/karabiner/main.m
@@ -20,8 +20,11 @@
   [self output:@"Usage:\n"];
   [self output:@"  Profile operations:\n"];
   [self output:@"    $ karabiner list\n"];
-  [self output:@"    $ karabiner select INDEX\n"];
+  [self output:@"    $ karabiner select INDEX (INDEX starts at 0)\n"];
   [self output:@"    $ karabiner selected\n"];
+  [self output:@"    $ karabiner append NAME\n"];
+  [self output:@"    $ karabiner rename INDEX NEWNAME (INDEX starts at 0)\n"];
+  [self output:@"    $ karabiner delete INDEX (INDEX starts at 0)\n"];
   [self output:@"  Settings:\n"];
   [self output:@"    $ karabiner set IDENTIFIER VALUE\n"];
   [self output:@"    $ karabiner enable IDENTIFIER (alias of set IDENTIFIER 1)\n"];
@@ -36,6 +39,9 @@
   [self output:@"  $ karabiner list\n"];
   [self output:@"  $ karabiner select 1\n"];
   [self output:@"  $ karabiner selected\n"];
+  [self output:@"  $ karabiner append \"For external keyboard\"\n"];
+  [self output:@"  $ karabiner rename 1 \"Empty Setting\"\n"];
+  [self output:@"  $ karabiner delete 1\n"];
   [self output:@"\n"];
   [self output:@"  $ karabiner set repeat.wait 30\n"];
   [self output:@"  $ karabiner enable remap.shiftL2commandL\n"];
@@ -46,7 +52,7 @@
   [self output:@"  $ karabiner reloadxml\n"];
   [self output:@"  $ karabiner relaunch\n"];
 
-  [[NSApplication sharedApplication] terminate:nil];
+  exit(2);
 }
 
 - (void) main
@@ -63,9 +69,10 @@
 
       /*  */ if ([command isEqualToString:@"list"]) {
         NSArray* a = [[client proxy] configlist_getConfigList];
+        int index = 0;
         for (NSDictionary* dict in a) {
-          [self output:dict[@"name"]];
-          [self output:@"\n"];
+          [self output:[NSString stringWithFormat:@"%d: %@\n", index, dict[@"name"]]];
+          ++index;
         }
 
       } else if ([command isEqualToString:@"selected"]) {
@@ -107,6 +114,41 @@
         NSString* value = arguments[2];
         [[client proxy] configlist_select:[value intValue]];
 
+        if ([[client proxy] configlist_selectedIndex] != [value integerValue]) {
+          [self output:@"Failed to select.\n"];
+          exit(1);
+        }
+
+      } else if ([command isEqualToString:@"append"]) {
+        if ([arguments count] != 3) {
+          [self usage];
+        }
+        NSString* value = arguments[2];
+        [[client proxy] configlist_append];
+        NSInteger index = [[[client proxy] configlist_getConfigList] count] - 1;
+        [[client proxy] configlist_setName:index name:value];
+
+      } else if ([command isEqualToString:@"rename"]) {
+        if ([arguments count] != 4) {
+          [self usage];
+        }
+        NSString* index = arguments[2];
+        NSString* value = arguments[3];
+        [[client proxy] configlist_setName:[index intValue] name:value];
+
+      } else if ([command isEqualToString:@"delete"]) {
+        if ([arguments count] != 3) {
+          [self usage];
+        }
+        NSInteger selected = [[client proxy] configlist_selectedIndex];
+        NSString* index = arguments[2];
+        if (selected == [index integerValue]) {
+          [self output:@"You cannot delete selected profile.\n"];
+          exit(1);
+        } else {
+          [[client proxy] configlist_delete:[index intValue]];
+        }
+
       } else if ([command isEqualToString:@"set"]) {
         if ([arguments count] != 4) {
           [self usage];
@@ -128,7 +170,12 @@
         }
         NSString* value = arguments[2];
         [[client proxy] setValue:0 forName:value];
+
+      } else {
+        [self output:[NSString stringWithFormat:@"Unknown argument: %@\n", command]];
+        exit(1);
       }
+
     } @catch (NSException* exception) {
       NSLog(@"%@", exception);
     }
