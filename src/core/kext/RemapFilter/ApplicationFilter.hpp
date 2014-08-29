072ac702d1e76bd6f0416cd7ad95edc13f501e77
Takayama Fumihiko <tekezo@pqrs.org>
2014-08-29 23:01:14 +0900

    remove ApplicationFilter.cpp

--- src/core/kext/RemapFilter/ApplicationFilter.hpp
+++ src/core/kext/RemapFilter/ApplicationFilter.hpp
@@ -1,7 +1,10 @@
 #ifndef APPLICATIONFILTER_HPP
 #define APPLICATIONFILTER_HPP
 
+#include "bridge.h"
 #include "RemapFilterBase.hpp"
+#include "IOLogWrapper.hpp"
+#include "CommonData.hpp"
 
 namespace org_pqrs_Karabiner {
   namespace RemapFilter {
@@ -9,8 +12,36 @@ namespace org_pqrs_Karabiner {
     public:
       ApplicationFilter(unsigned int type) : RemapFilterBase(type) {}
 
-      void initialize(const unsigned int* vec, size_t length);
-      bool isblocked(void);
+      void initialize(const unsigned int* vec, size_t length) {
+        for (size_t i = 0; i < length; ++i) {
+          targets_.push_back(AddValue(vec[i]));
+        }
+      }
+
+      bool isblocked(void) {
+        unsigned int current = CommonData::getcurrent_workspacedata().applicationtype;
+
+        switch (get_type()) {
+          case BRIDGE_FILTERTYPE_APPLICATION_NOT:
+          case BRIDGE_FILTERTYPE_APPLICATION_ONLY:
+          {
+            bool isnot = (get_type() == BRIDGE_FILTERTYPE_APPLICATION_NOT);
+
+            for (size_t i = 0; i < targets_.size(); ++i) {
+              if (targets_[i] == current) {
+                return isnot ? true : false;
+              }
+            }
+            return isnot ? false : true;
+          }
+
+          default:
+            IOLOG_ERROR("ApplicationFilter::isblocked unknown type_(%d)\n", get_type());
+            break;
+        }
+
+        return false;
+      }
 
     private:
       Vector_FilterValue targets_;
