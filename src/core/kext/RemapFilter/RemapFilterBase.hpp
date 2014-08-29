e1919b1ea924bba114f614568efa01c197b02169
Takayama Fumihiko <tekezo@pqrs.org>
2014-08-30 00:21:12 +0900

    remove FilterValue, FilterValueWithDataType

--- src/core/kext/RemapFilter/RemapFilterBase.hpp
+++ src/core/kext/RemapFilter/RemapFilterBase.hpp
@@ -13,18 +13,6 @@ namespace org_pqrs_Karabiner {
       RemapFilterBase(unsigned int type) : type_(type) {}
       virtual ~RemapFilterBase(void) {}
 
-      typedef unsigned int FilterValue;
-      DECLARE_VECTOR(FilterValue);
-
-      class FilterValueWithDataType {
-      public:
-        FilterValueWithDataType(void) : datatype(BRIDGE_DATATYPE_NONE), value(0) {}
-        FilterValueWithDataType(unsigned int d, unsigned int v) : datatype(d), value(v) {}
-        unsigned int datatype;
-        unsigned int value;
-      };
-      DECLARE_VECTOR(FilterValueWithDataType);
-
       virtual void initialize(const unsigned int* vec, size_t length) = 0;
       virtual bool isblocked(void) = 0;
 
