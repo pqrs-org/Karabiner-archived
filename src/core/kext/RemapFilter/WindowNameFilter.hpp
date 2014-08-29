b008e51d3be2b935687cf1274c7e01dfd53ad49c
Takayama Fumihiko <tekezo@pqrs.org>
2014-08-30 00:16:16 +0900

    remove WindowNameFilter.cpp

--- src/core/kext/RemapFilter/WindowNameFilter.hpp
+++ src/core/kext/RemapFilter/WindowNameFilter.hpp
@@ -9,11 +9,35 @@ namespace org_pqrs_Karabiner {
     public:
       WindowNameFilter(unsigned int type) : RemapFilterBase(type) {}
 
-      void initialize(const unsigned int* vec, size_t length);
-      bool isblocked(void);
+      void initialize(const unsigned int* vec, size_t length) {
+        for (size_t i = 0; i < length; ++i) {
+          targets_.push_back(AddValue(vec[i]));
+        }
+      }
+
+      bool isblocked(void) {
+        if (get_type() == BRIDGE_FILTERTYPE_WINDOWNAME_NOT ||
+            get_type() == BRIDGE_FILTERTYPE_WINDOWNAME_ONLY) {
+
+          unsigned int current = CommonData::getcurrent_workspacedata().windowname;
+
+          bool isnot = (get_type() == BRIDGE_FILTERTYPE_WINDOWNAME_NOT);
+
+          for (size_t i = 0; i < targets_.size(); ++i) {
+            if (targets_[i] == current) {
+              return isnot ? true : false;
+            }
+          }
+
+          return isnot ? false : true;
+        }
+
+        IOLOG_ERROR("WindowNameFilter::isblocked unknown type(%d)\n", get_type());
+        return false;
+      }
 
     private:
-      Vector_FilterValue targets_;
+      Vector_AddValue targets_;
     };
   }
 }
