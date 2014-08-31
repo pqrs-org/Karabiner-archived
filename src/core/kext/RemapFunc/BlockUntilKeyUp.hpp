d7e922e69acc96e09f0f135b158de3d834da22e8
Takayama Fumihiko <tekezo@pqrs.org>
2014-08-31 19:54:46 +0900

    use RemapFuncBase

--- src/core/kext/RemapFunc/BlockUntilKeyUp.hpp
+++ src/core/kext/RemapFunc/BlockUntilKeyUp.hpp
@@ -1,14 +1,16 @@
 #ifndef BLOCKUNTILKEYUP_HPP
 #define BLOCKUNTILKEYUP_HPP
 
-#include "FromEvent.hpp"
-#include "ParamsUnion.hpp"
-#include "Types.hpp"
+#include "RemapFuncBase.hpp"
 
 namespace org_pqrs_Karabiner {
   namespace RemapFunc {
-    class BlockUntilKeyUp {
+    class BlockUntilKeyUp : RemapFuncBase {
     public:
+      BlockUntilKeyUp(void) :
+        RemapFuncBase(BRIDGE_REMAPTYPE_BLOCKUNTILKEYUP)
+      {}
+
       void add(AddDataType datatype, AddValue newval);
       const FromEvent& getFromEvent(void) const { return fromEvent_; }
 
