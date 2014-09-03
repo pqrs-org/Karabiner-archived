35b8aaa1bffaaf6f3cd8169a20bbf2b37e879082
Takayama Fumihiko <tekezo@pqrs.org>
2014-09-03 23:54:01 +0900

    use RemapFuncBase

--- src/core/kext/RemapFunc/PassThrough.hpp
+++ src/core/kext/RemapFunc/PassThrough.hpp
@@ -1,10 +1,16 @@
 #ifndef PASSTHROUGH_HPP
 #define PASSTHROUGH_HPP
 
+#include "RemapFuncBase.hpp"
+
 namespace org_pqrs_Karabiner {
   namespace RemapFunc {
-    class PassThrough {
+    class PassThrough : RemapFuncBase {
     public:
+      PassThrough(void) :
+        RemapFuncBase(BRIDGE_REMAPTYPE_PASSTHROUGH)
+      {}
+
       void add(AddDataType datatype, AddValue newval) {}
     };
   }
