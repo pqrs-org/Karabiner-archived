2e8a6941926ccb7f994064ee8db4a91e750108ea
Takayama Fumihiko <tekezo@pqrs.org>
2014-09-15 23:47:45 +0900

    use Params_Base in ParamsUnion

--- src/core/kext/Classes/LastPressedPhysicalKey.cpp
+++ src/core/kext/Classes/LastPressedPhysicalKey.cpp
@@ -5,47 +5,32 @@ namespace org_pqrs_Karabiner {
   void
   LastPressedPhysicalKey::update(const ParamsUnion& newval)
   {
-    switch (newval.type) {
-      case ParamsUnion::KEYBOARD:
-      {
-        auto p = newval.get_Params_KeyboardEventCallBack();
+    {
+      auto p = newval.get_Params_KeyboardEventCallBack();
 
-        if (p && p->ex_iskeydown) {
-          datatype_ = BRIDGE_DATATYPE_KEYCODE;
-          value_ = (p->key).get();
-          ic_.begin();
-        }
-        break;
+      if (p && p->ex_iskeydown) {
+        datatype_ = BRIDGE_DATATYPE_KEYCODE;
+        value_ = (p->key).get();
+        ic_.begin();
       }
+    }
+    {
+      auto p = newval.get_Params_KeyboardSpecialEventCallback();
 
-      case ParamsUnion::KEYBOARD_SPECIAL:
-      {
-        auto p = newval.get_Params_KeyboardSpecialEventCallback();
-        if (p && p->ex_iskeydown) {
-          datatype_ = BRIDGE_DATATYPE_CONSUMERKEYCODE;
-          value_ = (p->key).get();
-          ic_.begin();
-        }
-        break;
+      if (p && p->ex_iskeydown) {
+        datatype_ = BRIDGE_DATATYPE_CONSUMERKEYCODE;
+        value_ = (p->key).get();
+        ic_.begin();
       }
+    }
+    {
+      auto p = newval.get_Params_RelativePointerEventCallback();
 
-      case ParamsUnion::RELATIVE_POINTER:
-      {
-        auto p = newval.get_Params_RelativePointerEventCallback();
-
-        if (p && p->ex_button != PointingButton::NONE && p->ex_isbuttondown) {
-          datatype_ = BRIDGE_DATATYPE_POINTINGBUTTON;
-          value_ = (p->ex_button).get();
-          ic_.begin();
-        }
-        break;
+      if (p && p->ex_button != PointingButton::NONE && p->ex_isbuttondown) {
+        datatype_ = BRIDGE_DATATYPE_POINTINGBUTTON;
+        value_ = (p->ex_button).get();
+        ic_.begin();
       }
-
-      case ParamsUnion::UPDATE_FLAGS:
-      case ParamsUnion::SCROLL_WHEEL:
-      case ParamsUnion::WAIT:
-        // Do nothing
-        break;
     }
   }
 }
