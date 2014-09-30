0a1d1a3c631368a70f2a402c493a8fd41bf369fe
Takayama Fumihiko <tekezo@pqrs.org>
2014-09-30 09:21:19 +0900

    remove util/uncrustify

--- util/uncrustify/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-basedir=`dirname $0`
-
-find ../../* -name '*.cpp' -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
-find ../../* -name '*.hpp' -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
-find ../../* -name '*.m'   -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup
-find ../../* -name '*.h'   -type f ! -ipath '*/Pods/*' ! -ipath '*/build/*' | xargs uncrustify -c "$basedir/uncrustify.cfg" --no-backup -l OC
