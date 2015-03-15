18c03b403072375dc9ab71f15fd660207433d2fa
Takayama Fumihiko <tekezo@pqrs.org>
2015-03-15 15:08:09 +0900

    add WeakPointer

---
+++ Tests/kext/WeakPointer/test.cpp
@@ -0,0 +1,43 @@
+#include <ostream>
+#include <gtest/gtest.h>
+#include "WeakPointer.hpp"
+
+using namespace org_pqrs_Karabiner;
+
+DECLARE_WEAKPOINTER(TestItem);
+
+class TestItem final {
+public:
+  TestItem(void) {
+    WeakPointer_TestItem::add(this);
+  };
+  ~TestItem(void) {
+    WeakPointer_TestItem::remove(this);
+  }
+};
+
+DEFINE_WEAKPOINTER(TestItem);
+
+TEST(WeakPointer, expired) {
+  auto p1 = new TestItem();
+  auto p2 = new TestItem();
+
+  EXPECT_FALSE(WeakPointer_TestItem::expired(p1));
+  EXPECT_FALSE(WeakPointer_TestItem::expired(p2));
+
+  delete p1;
+
+  EXPECT_TRUE(WeakPointer_TestItem::expired(p1));
+  EXPECT_FALSE(WeakPointer_TestItem::expired(p2));
+
+  delete p2;
+
+  EXPECT_TRUE(WeakPointer_TestItem::expired(p1));
+  EXPECT_TRUE(WeakPointer_TestItem::expired(p2));
+}
+
+int
+main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
---
+++ src/core/kext/Classes/WeakPointer.hpp
@@ -0,0 +1,56 @@
+#ifndef WEAKPOINTER_HPP
+#define WEAKPOINTER_HPP
+
+#include "List.hpp"
+
+namespace org_pqrs_Karabiner {
+
+#define DECLARE_WEAKPOINTER(TYPENAME)                                                                   \
+  class TYPENAME;                                                                                       \
+  class WeakPointer_##TYPENAME final {                                                                  \
+  public:                                                                                               \
+    static void add(TYPENAME* p) {                                                                      \
+      auto item = new Item(p);                                                                          \
+      if (item) {                                                                                       \
+        list_.push_back(item);                                                                          \
+      }                                                                                                 \
+    }                                                                                                   \
+                                                                                                        \
+    static void remove(TYPENAME* pointer) {                                                             \
+      Item* p = static_cast<Item*>(list_.safe_front());                                                 \
+      for (;;) {                                                                                        \
+        if (!p) break;                                                                                  \
+        if (p->pointer == pointer) {                                                                    \
+          p = static_cast<Item*>(list_.erase_and_delete(p));                                            \
+        } else {                                                                                        \
+          p = static_cast<Item*>(p->getnext());                                                         \
+        }                                                                                               \
+      }                                                                                                 \
+    }                                                                                                   \
+                                                                                                        \
+    static bool expired(TYPENAME* pointer) {                                                            \
+      for (Item* p = static_cast<Item*>(list_.safe_front()); p; p = static_cast<Item*>(p->getnext())) { \
+        if (p->pointer == pointer) {                                                                    \
+          return false;                                                                                 \
+        }                                                                                               \
+      }                                                                                                 \
+      return true;                                                                                      \
+    }                                                                                                   \
+                                                                                                        \
+  private:                                                                                              \
+    class Item : public List::Item {                                                                    \
+    public:                                                                                             \
+      Item(TYPENAME* p) : pointer(p) {}                                                                 \
+      virtual ~Item(void) {}                                                                            \
+                                                                                                        \
+      TYPENAME* pointer;                                                                                \
+    };                                                                                                  \
+                                                                                                        \
+    static List list_;                                                                                  \
+  };
+}
+
+#define DEFINE_WEAKPOINTER(TYPENAME) \
+  List WeakPointer_##TYPENAME::list_;
+
+#endif
