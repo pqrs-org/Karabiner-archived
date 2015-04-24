c41109c1face036e0e8fc597b2f1be12190b2e72
Takayama Fumihiko <tekezo@pqrs.org>
2015-04-24 12:33:07 +0900

    set AutogenId::maxValue() for keytokey_interrupted_

--- src/core/kext/RemapFunc/KeyDownUpToKey.hpp
+++ src/core/kext/RemapFunc/KeyDownUpToKey.hpp
@@ -12,7 +12,14 @@ public:
                                         indexType_(INDEXTYPE_NONE),
                                         keytokey_from_(autogenId),
                                         keytokey_downup_(autogenId),
-                                        keytokey_interrupted_(autogenId),
+                                        // Use AutogenId::maxValue for interrupted keys because interrupted keys are not changed by Karabiner.
+                                        // That needs for __DropAllKeys__ with __KeyDownUpToKey__.
+                                        //
+                                        // For example, when we use `remap.launcher_mode_v2`,
+                                        // [O down, A down, Z down] should be canceled by __DropAllKeys__.
+                                        //
+                                        // We need to send A,Z (interrupted keys) with AutogenId::maxValue() to cancel them in __DropAllKeys__.
+                                        keytokey_interrupted_(AutogenId::maxValue()),
                                         needClearInterruptedToKeys_(false),
                                         interrupted_(false) {}
 
