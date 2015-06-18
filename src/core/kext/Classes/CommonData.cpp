118e82ec7bc891ebc841ed977d50a989498fe9ed
Takayama Fumihiko <tekezo@pqrs.org>
2015-06-18 17:16:00 +0900

    add blank line

--- src/core/kext/Classes/CommonData.cpp
+++ src/core/kext/Classes/CommonData.cpp
@@ -29,6 +29,7 @@ void CommonData::terminate(void) {}
 
 void CommonData::setcurrent_workspaceIds(const uint32_t* ids, size_t count) {
   current_workspaceAppIds_.clear();
+
   for (size_t i = 0; i < count - 1; i += 2) {
     uint32_t type = ids[i];
     uint32_t value = ids[i + 1];
