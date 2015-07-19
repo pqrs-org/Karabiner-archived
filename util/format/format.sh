cbc8cb2f0060308f67791a847801aced0eccdae6
Takayama Fumihiko <tekezo@pqrs.org>
2015-07-19 11:21:41 +0900

    change clang-format path

--- util/format/format.sh
+++ util/format/format.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-clang_format="$HOME/Library/Application Support/Alcatraz/Plug-ins/ClangFormat/bin/clang-format"
-
 find ../../* \
     \( -name '*.[ch]pp' -o -name '*.[mh]' \) \
     -type f \
     ! -ipath '*/Pods/*' \
     ! -ipath '*/build/*' \
     \
-    | xargs "$clang_format" -i
+    | xargs clang-format -i
