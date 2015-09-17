ce529caf6766476b28a3d14c9fab7131d57e5e0a
Takayama Fumihiko <tekezo@pqrs.org>
2015-09-18 02:31:10 +0900

    add diagnostic_macros.hpp

---
+++ src/core/kext/diagnostic_macros.hpp
@@ -0,0 +1,11 @@
+#ifndef DIAGNOSTIC_MACROS_HPP
+#define DIAGNOSTIC_MACROS_HPP
+
+#define BEGIN_IOKIT_INCLUDE      \
+  _Pragma("clang diagnostic push") \
+      _Pragma("clang diagnostic ignored \"-Winconsistent-missing-override\"")
+
+#define END_IOKIT_INCLUDE \
+  _Pragma("clang diagnostic pop")
+
+#endif
