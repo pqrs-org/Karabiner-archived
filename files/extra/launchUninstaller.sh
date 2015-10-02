6c993ca8dfca5dc66a324026167df3004151131e
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-03 03:15:52 +0900

    remove launchUninstaller.sh

--- files/extra/launchUninstaller.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-TMPDIR=`/usr/bin/mktemp -d /tmp/Karabiner_uninstaller.XXXXXX` || exit 1
-/usr/bin/rsync -a /Applications/Karabiner.app/Contents/Applications/KarabinerUninstaller.app "$TMPDIR"
-sh '/Library/Application Support/org.pqrs/Karabiner/setpermissions.sh' "$TMPDIR/KarabinerUninstaller.app"
-/usr/bin/open "$TMPDIR/KarabinerUninstaller.app"
