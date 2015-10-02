b4244414e1e715b5ec6e1125193cad00a77437eb
Takayama Fumihiko <tekezo@pqrs.org>
2015-10-03 03:12:09 +0900

    remove killall

--- files/extra/uninstall.sh
+++ files/extra/uninstall.sh
@@ -6,7 +6,4 @@ sh '/Library/Application Support/org.pqrs/Karabiner/uninstall_core.sh'
 rm -f /var/db/receipts/org.pqrs.driver.Karabiner.*
 rm -f /var/db/receipts/org.pqrs.driver.KeyRemap4MacBook.*
 
-# kill processes
-/usr/bin/killall Karabiner 2>/dev/null
-
 exit 0
