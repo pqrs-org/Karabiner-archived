7372858fbc7032f217e7370fd3f590107e0a0061
Takayama Fumihiko <tekezo@pqrs.org>
2016-01-22 01:46:56 +0900

    call makeFirstResponder

--- src/core/server/Classes/Preferences/CheckboxCellView.m
+++ src/core/server/Classes/Preferences/CheckboxCellView.m
@@ -4,9 +4,12 @@
 @implementation CheckboxCellView
 
 - (IBAction)valueChanged:(id)sender {
+  // select row
   NSInteger row = [self.outlineView rowForView:self];
   [self.outlineView selectRowIndexes:[NSIndexSet indexSetWithIndex:(NSUInteger)(row)] byExtendingSelection:NO];
+  [[self.outlineView window] makeFirstResponder:self.outlineView];
 
+  // toggle setting
   if (self.checkbox.imagePosition != NSNoImage) {
     int value = (self.checkbox.state == NSOnState);
     [self.preferencesManager setValue:value forName:self.settingIdentifier];
