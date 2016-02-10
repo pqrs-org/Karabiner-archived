aa9beeb844ebce288af626db07d9cc26a9626e21
Takayama Fumihiko <tekezo@pqrs.org>
2016-02-10 19:28:28 +0900

    add ParameterOutlineView

---
+++ src/core/server/Classes/Preferences/ParameterOutlineView.m
@@ -0,0 +1,9 @@
+#import "ParameterOutlineView.h"
+
+@implementation ParameterOutlineView
+
+- (BOOL)validateProposedFirstResponder:(NSResponder*)responder forEvent:(NSEvent*)event {
+  return YES;
+}
+
+@end
