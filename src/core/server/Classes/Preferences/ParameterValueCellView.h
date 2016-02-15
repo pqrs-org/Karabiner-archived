9eca553f29844d1a7aabec218be8aaa39d48e159
Takayama Fumihiko <tekezo@pqrs.org>
2016-02-16 01:21:27 +0900

    clean up

--- src/core/server/Classes/Preferences/ParameterValueCellView.h
+++ src/core/server/Classes/Preferences/ParameterValueCellView.h
@@ -8,7 +8,4 @@
 @property(weak) IBOutlet NSStepper* stepper;
 @property(weak) PreferencesManager* preferencesManager;
 @property NSString* settingIdentifier;
-
-- (void)valueChangedByStepper:(id)sender;
-- (void)valueChangedByTextField:(id)sender;
 @end
