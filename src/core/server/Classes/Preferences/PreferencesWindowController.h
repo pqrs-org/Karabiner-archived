63fcf32b50ac18d9e92cab238038ec59387fdd39
Takayama Fumihiko <tekezo@pqrs.org>
2016-02-20 01:47:12 +0900

    clean up

--- src/core/server/Classes/Preferences/PreferencesWindowController.h
+++ src/core/server/Classes/Preferences/PreferencesWindowController.h
@@ -1,19 +1,7 @@
 /* -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*- */
 
-@class ClientForKernelspace;
-@class PreferencesManager;
-@class XMLCompiler;
-
 @interface PreferencesWindowController : NSObject <NSWindowDelegate, NSTabViewDelegate>
 
 - (void)show;
-- (IBAction)openURL:(id)sender;
-- (IBAction)openPrivateXMLUsageURL:(id)sender;
-- (IBAction)refreshKeyRepeatTab:(id)sender;
-- (IBAction)openSystemPreferencesKeyboard:(id)sender;
-- (IBAction)changeDelayUntilRepeat:(id)sender;
-- (IBAction)changeKeyRepeat:(id)sender;
-- (IBAction)openConsoleApp:(id)sender;
-- (IBAction)toggleDebugMode:(id)sender;
 
 @end
