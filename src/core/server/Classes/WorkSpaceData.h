d8094d8eb366a2759502871b6381b30e4868055e
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-01 22:57:23 +0900

    merge WorkSpaceData+InputSource.m into WorkSpaceData.m

--- src/core/server/Classes/WorkSpaceData.h
+++ src/core/server/Classes/WorkSpaceData.h
@@ -1,19 +1,14 @@
 // -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
 
 @import Cocoa;
-#import "InputSource.h"
-#import "XMLCompiler.h"
+
+@class InputSource;
 
 @interface WorkSpaceData : NSObject
 
-@property(weak) IBOutlet XMLCompiler* xmlCompiler;
-
 - (unsigned int)getUIElementRole:(NSString*)name;
 
-@end
-
-@interface WorkSpaceData (InputSource)
-
+// InputSource
 + (void)refreshEnabledInputSources;
 + (InputSource*)getCurrentInputSource;
 - (void)selectInputSource:(unsigned int)vk_keycode;
--- src/core/server/Classes/WorkSpaceData.m
+++ src/core/server/Classes/WorkSpaceData.m
@@ -1,13 +1,142 @@
-// -*- Mode: objc; Coding: utf-8; indent-tabs-mode: nil; -*-
-
+#import "InputSource.h"
 #import "WorkSpaceData.h"
+#import "XMLCompiler.h"
+
+@interface WorkSpaceData ()
+
+@property(weak) IBOutlet XMLCompiler* xmlCompiler;
+
+@end
 
 @implementation WorkSpaceData
 
-- (unsigned int)getUIElementRole:(NSString *)name {
+- (unsigned int)getUIElementRole:(NSString*)name {
   if (!name) return 0;
 
   return [self.xmlCompiler uielementroleid:name];
+}
+
+// InputSource
+
+static NSMutableArray* enabledInputSources_ = nil;
+
++ (void)refreshEnabledInputSources {
+  @synchronized(self) {
+    CFDictionaryRef filter = NULL;
+    CFArrayRef list = NULL;
+
+    enabledInputSources_ = [NSMutableArray new];
+
+    // ----------------------------------------
+    // Making filter
+    const void* keys[] = {
+        kTISPropertyInputSourceIsSelectCapable,
+        kTISPropertyInputSourceCategory,
+    };
+    const void* values[] = {
+        kCFBooleanTrue,
+        kTISCategoryKeyboardInputSource,
+    };
+    filter = CFDictionaryCreate(NULL, keys, values, sizeof(keys) / sizeof(keys[0]), NULL, NULL);
+    if (!filter) goto finish;
+
+    // ----------------------------------------
+    // Making list
+    list = TISCreateInputSourceList(filter, false);
+    if (!list) goto finish;
+
+    // ----------------------------------------
+    for (int i = 0; i < CFArrayGetCount(list); ++i) {
+      TISInputSourceRef source = (TISInputSourceRef)(CFArrayGetValueAtIndex(list, i));
+      if (!source) continue;
+
+      // ----------------------------------------
+      // Skip inappropriate input sources.
+      //
+      // - com.apple.inputmethod.ironwood (Voice Input)
+
+      NSString* sourceID = (__bridge NSString*)(TISGetInputSourceProperty(source, kTISPropertyInputSourceID));
+      if (sourceID) {
+        if ([sourceID isEqualToString:@"com.apple.inputmethod.ironwood"]) {
+          continue;
+        }
+      }
+
+      // ----------------------------------------
+      InputSource* inputSource = [[InputSource alloc] initWithTISInputSourceRef:source];
+      if (inputSource) {
+        [enabledInputSources_ addObject:inputSource];
+      }
+    }
+
+  finish:
+    if (filter) {
+      CFRelease(filter);
+    }
+    if (list) {
+      CFRelease(list);
+    }
+  }
+}
+
++ (InputSource*)getCurrentInputSource {
+  @synchronized(self) {
+    for (InputSource* inputSource in enabledInputSources_) {
+      if ([inputSource selected]) {
+        return inputSource;
+      }
+    }
+  }
+  return nil;
+}
+
+// ----------------------------------------------------------------------
+- (void)selectInputSource:(unsigned int)vk_keycode {
+  // ----------------------------------------------------------------------
+  // Note for languagecode
+  // TISCopyInputSourceForLanguage returns unselectable InputSource.
+  // Therefore we get InputSource by ourself.
+
+  // Note for inputSourceID
+  //   InputSourceID is not unique.
+  //   For example, AquaSKK has 6 Input Sources.
+  //   But all of them have same InputSourceID.
+  //
+  //   Therefore, we need to treat it carefully.
+  //
+  //     * kTISPropertyInputSourceID: jp.sourceforge.inputmethod.aquaskk
+  //     * kTISPropertyInputModeID:   com.apple.inputmethod.Japanese
+  //
+  //     * kTISPropertyInputSourceID: jp.sourceforge.inputmethod.aquaskk
+  //     * kTISPropertyInputModeID:   com.apple.inputmethod.Japanese.HalfWidthKana
+  //
+  //     * kTISPropertyInputSourceID: jp.sourceforge.inputmethod.aquaskk
+  //     * kTISPropertyInputModeID:   com.apple.inputmethod.Roman
+  //
+  //     * kTISPropertyInputSourceID: jp.sourceforge.inputmethod.aquaskk
+  //     * kTISPropertyInputModeID:   com.apple.inputmethod.Japanese.FullWidthRoman
+  //
+  //     * kTISPropertyInputSourceID: jp.sourceforge.inputmethod.aquaskk
+  //     * kTISPropertyInputModeID:   com.apple.inputmethod.Japanese.Hiragana
+  //
+  //     * kTISPropertyInputSourceID: jp.sourceforge.inputmethod.aquaskk
+  //     * kTISPropertyInputModeID:   com.apple.inputmethod.Japanese.Katakana
+
+  InputSource* matched = nil;
+  @synchronized(self) {
+    for (InputSource* inputSource in enabledInputSources_) {
+      if ([self.xmlCompiler is_vk_change_inputsource_matched:vk_keycode
+                                                languagecode:inputSource.languagecode
+                                               inputSourceID:inputSource.inputSourceID
+                                                 inputModeID:inputSource.inputModeID]) {
+        matched = inputSource;
+        break;
+      }
+    }
+  }
+  if (matched) {
+    [matched select];
+  }
 }
 
 @end
