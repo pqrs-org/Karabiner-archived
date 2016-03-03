6dad349f00dd751e012d907e2b0ac6bbc9225676
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-03 12:58:29 +0900

    use /etc/karabiner_kextload_delay

--- files/extra/startup.sh
+++ files/extra/startup.sh
@@ -4,6 +4,12 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 basedir='/Library/Application Support/org.pqrs/Karabiner'
 
 # --------------------
+if [ -f /etc/karabiner_kextload_delay ]; then
+    delay=`ruby -e 'print $stdin.read.to_i' < /etc/karabiner_kextload_delay`
+    sleep $delay
+fi
+
+# --------------------
 argument="$1"
 [ -z "$argument" ] && argument=start
 case "$argument" in
