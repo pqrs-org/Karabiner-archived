e3b88525b436cbe04d95ed8fce37760fb4f8a37b
Takayama Fumihiko <tekezo@pqrs.org>
2016-03-05 00:30:17 +0900

    ignore /etc/karabiner_kextload_delay at stop

--- files/extra/startup.sh
+++ files/extra/startup.sh
@@ -4,16 +4,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 basedir='/Library/Application Support/org.pqrs/Karabiner'
 
 # --------------------
-if [ -f /etc/karabiner_kextload_delay ]; then
-    delay=`ruby -e 'print $stdin.read.to_i' < /etc/karabiner_kextload_delay`
-    sleep $delay
-fi
-
-# --------------------
 argument="$1"
 [ -z "$argument" ] && argument=start
 case "$argument" in
     start)
+        if [ -f /etc/karabiner_kextload_delay ]; then
+            delay=`ruby -e 'print $stdin.read.to_i' < /etc/karabiner_kextload_delay`
+            sleep $delay
+        fi
+
         echo "Starting Karabiner"
         kextload "$basedir/Karabiner.signed.kext"
         ;;
