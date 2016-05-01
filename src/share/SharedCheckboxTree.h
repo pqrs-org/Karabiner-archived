24e3e53d4e56189a77fd999c7be2cef8af822492
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-01 10:07:11 +0900

    remove SharedCheckboxItem

--- src/share/SharedCheckboxTree.h
+++ src/share/SharedCheckboxTree.h
@@ -2,23 +2,6 @@
 
 @import Cocoa;
 
-@interface SharedCheckboxItem : NSObject <NSCoding, NSCopying>
-
-@property(readonly) NSNumber* id;
-@property(copy, readonly) NSString* name;
-@property(copy, readonly) NSString* style;
-@property(copy, readonly) NSString* identifier;
-@property(readonly) NSInteger childrenCount;
-@property(readonly) BOOL needsShowCheckbox;
-
-- (instancetype)initWithName:(NSString*)name
-                       style:(NSString*)style
-                  identifier:(NSString*)identifier
-               childrenCount:(NSInteger)childrenCount
-           needsShowCheckbox:(BOOL)needsShowCheckbox;
-
-@end
-
 @interface SharedCheckboxTree : NSObject <NSCoding, NSCopying>
 
 @property NSNumber* id;
