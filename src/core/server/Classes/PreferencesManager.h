6344720f49e8d2a348437f4ae89e6b13b143e26d
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-09 02:30:08 +0900

    remove unused code

--- src/core/server/Classes/PreferencesManager.h
+++ src/core/server/Classes/PreferencesManager.h
@@ -9,22 +9,8 @@
 - (void)loadPreferencesModel:(PreferencesModel*)preferencesModel;
 - (void)savePreferencesModel:(PreferencesModel*)preferencesModel processIdentifier:(int)processIdentifier;
 
-- (void)load;
+- (void)save;
 
-- (int)value:(NSString*)name;
-- (int)defaultValue:(NSString*)name;
-- (void)setValue:(int)newval forName:(NSString*)name;
-- (void)setValue:(int)newval forName:(NSString*)name tellToKext:(BOOL)tellToKext;
-- (void)setValue:(int)newval forName:(NSString*)name tellToKext:(BOOL)tellToKext notificationUserInfo:(NSDictionary*)notificationUserInfo;
-- (void)clearNotSave;
-
-- (NSArray*)essential_config;
-- (NSDictionary*)changed;
-
-// --------------------------------------------------
-- (NSString*)configlist_selectedIdentifier;
-- (NSDictionary*)configlist_dictionary:(NSInteger)rowIndex;
-- (NSString*)configlist_identifier:(NSInteger)rowIndex;
-- (void)configlist_clear_all_values:(NSInteger)rowIndex;
+- (void)updateKextValue:(NSString*)name;
 
 @end
--- src/core/server/Classes/PreferencesManager.m
+++ src/core/server/Classes/PreferencesManager.m
@@ -11,8 +11,6 @@
 @property(weak) IBOutlet ClientForKernelspace* clientForKernelspace;
 @property(weak) IBOutlet PreferencesModel* preferencesModel;
 @property(weak) IBOutlet XMLCompiler* xmlCompiler;
-@property NSMutableDictionary* defaults;
-@property(copy) NSArray* essential_configuration_identifiers;
 
 @end
 
@@ -20,34 +18,37 @@
 
 // ----------------------------------------
 + (void)initialize {
-  NSDictionary* dict = @{
-    kStatusBarEnabled : @YES,
-    kShowProfileNameInStatusBar : @NO,
-    kProfiles : @[],
-    kCurrentProfileIndex : @0,
-    kCheckForUpdates : @YES,
-    kIsStatusWindowEnabled : @YES,
-    kIsStatusWindowShowCapsLock : @NO,
-    kIsStatusWindowShowStickyModifier : @NO,
-    kIsStatusWindowShowPointingButtonLock : @YES,
-    kStatusWindowType : @0,  // Normal
-    kStatusWindowTheme : @0, // White
-    kStatusWindowOpacity : @80,
-    kStatusWindowFontSize : @0, // Small
-    kStatusWindowPosition : @3, // Bottom right
-    kIsAXNotifierEnabled : @YES,
-    kAXNotifierDisabledInJavaApps : @YES,
-    kAXNotifierDisabledInQtApps : @YES,
-    kAXNotifierDisabledInPreview : @YES,
-    kAXNotifierDisabledInMicrosoftOffice : @YES,
-    kUsePreparedSettings : @YES,
-    kIsMigratedIsOverwriteKeyRepeat : @NO,
-    kIsOverrideKeyRepeat : @NO,
-    kShowIconInDock : @NO,
-    kResumeAtLogin : @YES,
-    kKarabinerPreferencesCheckboxFont : @0,
-  };
-  [[NSUserDefaults standardUserDefaults] registerDefaults:dict];
+  static dispatch_once_t once;
+  dispatch_once(&once, ^{
+    NSDictionary* dict = @{
+      kStatusBarEnabled : @YES,
+      kShowProfileNameInStatusBar : @NO,
+      kProfiles : @[],
+      kCurrentProfileIndex : @0,
+      kCheckForUpdates : @YES,
+      kIsStatusWindowEnabled : @YES,
+      kIsStatusWindowShowCapsLock : @NO,
+      kIsStatusWindowShowStickyModifier : @NO,
+      kIsStatusWindowShowPointingButtonLock : @YES,
+      kStatusWindowType : @0,  // Normal
+      kStatusWindowTheme : @0, // White
+      kStatusWindowOpacity : @80,
+      kStatusWindowFontSize : @0, // Small
+      kStatusWindowPosition : @3, // Bottom right
+      kIsAXNotifierEnabled : @YES,
+      kAXNotifierDisabledInJavaApps : @YES,
+      kAXNotifierDisabledInQtApps : @YES,
+      kAXNotifierDisabledInPreview : @YES,
+      kAXNotifierDisabledInMicrosoftOffice : @YES,
+      kUsePreparedSettings : @YES,
+      kIsMigratedIsOverwriteKeyRepeat : @NO,
+      kIsOverrideKeyRepeat : @NO,
+      kShowIconInDock : @NO,
+      kResumeAtLogin : @YES,
+      kKarabinerPreferencesCheckboxFont : @0,
+    };
+    [[NSUserDefaults standardUserDefaults] registerDefaults:dict];
+  });
 }
 
 - (void)loadPreferencesModel:(PreferencesModel*)preferencesModel {
@@ -122,6 +123,10 @@
       @"identify" : profileModel.identifier ? profileModel.identifier : @"",
       @"appendIndex" : @(profileModel.appendIndex),
     }];
+
+    if (profileModel.identifier && profileModel.values) {
+      [[NSUserDefaults standardUserDefaults] setObject:profileModel.values forKey:profileModel.identifier];
+    }
   }
   [[NSUserDefaults standardUserDefaults] setObject:profiles forKey:kProfiles];
 
@@ -156,289 +161,31 @@
 }
 
 // ----------------------------------------
-- (void)addToDefault:(NSXMLElement*)element {
-  for (NSXMLElement* e in [element elementsForName:@"identifier"]) {
-    NSXMLNode* attr_default = [e attributeForName:@"default"];
-    if (!attr_default) continue;
-
-    self.defaults[[e stringValue]] = @([[attr_default stringValue] intValue]);
-  }
-
-  for (NSXMLElement* e in [element elementsForName:@"list"]) {
-    [self addToDefault:e];
-  }
-  for (NSXMLElement* e in [element elementsForName:@"item"]) {
-    [self addToDefault:e];
-  }
-}
-
-- (void)setDefault {
-  NSURL* xmlurl = [[NSBundle mainBundle] URLForResource:@"number" withExtension:@"xml"];
-  NSXMLDocument* xmldocument = [[NSXMLDocument alloc] initWithContentsOfURL:xmlurl options:0 error:NULL];
-  if (xmldocument) {
-    [self addToDefault:[xmldocument rootElement]];
-  }
-}
-
-// ----------------------------------------
-- (instancetype)init {
-  self = [super init];
-
-  if (self) {
-    self.defaults = [NSMutableDictionary new];
-    [self setDefault];
-
-    self.essential_configuration_identifiers = [NSArray arrayWithObjects:
-#include "../../../bridge/output/include.bridge_essential_configuration_identifiers.m"
-    ];
-  }
-
-  return self;
-}
-
-- (void)load {
-  // ------------------------------------------------------------
-  // scan config_* and detech notsave.*
-  for (NSDictionary* dict in [self configlist_getConfigList]) {
-    if (!dict) continue;
-
-    NSString* identifier = dict[@"identify"];
-    if (!identifier) continue;
-
-    NSDictionary* d = [[NSUserDefaults standardUserDefaults] dictionaryForKey:identifier];
-    if (!d) continue;
-
-    NSMutableDictionary* md = [NSMutableDictionary dictionaryWithDictionary:d];
-
-    for (NSString* name in [md allKeys]) {
-      if ([name hasPrefix:@"notsave."]) {
-        [md removeObjectForKey:name];
-      }
-    }
-
-    [[NSUserDefaults standardUserDefaults] setObject:md forKey:identifier];
-  }
-
-  // ------------------------------------------------------------
-  // migration
-  if (![[NSUserDefaults standardUserDefaults] boolForKey:kIsMigratedIsOverwriteKeyRepeat]) {
-    [[NSUserDefaults standardUserDefaults] setObject:@YES forKey:kIsMigratedIsOverwriteKeyRepeat];
-
-    if ([self valueAsNumber:@"repeat.initial_wait"] || [self valueAsNumber:@"repeat.wait"]) {
-      [[NSUserDefaults standardUserDefaults] setObject:@YES forKey:kIsOverrideKeyRepeat];
-
-      if (![self valueAsNumber:@"repeat.initial_wait"]) {
-        // old defalut value is 500.
-        [self setValue:500 forName:@"repeat.initial_wait"];
-      }
-      if (![self valueAsNumber:@"repeat.wait"]) {
-        // old defalut value is 83.
-        [self setValue:83 forName:@"repeat.wait"];
-      }
-    } else {
-      [[NSUserDefaults standardUserDefaults] setObject:@NO forKey:kIsOverrideKeyRepeat];
-    }
-
-    NSLog(@"Migration: Set kIsOverrideKeyRepeat:%d", [[NSUserDefaults standardUserDefaults] boolForKey:kIsOverrideKeyRepeat]);
-  }
+- (void)save {
+  [self savePreferencesModel:self.preferencesModel processIdentifier:[NSProcessInfo processInfo].processIdentifier];
 }
 
 // ----------------------------------------------------------------------
-- (NSNumber*)valueAsNumber:(NSString*)name {
-  // user setting
-  NSString* identifier = [self configlist_selectedIdentifier];
-  if (identifier) {
-    NSDictionary* dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:identifier];
-    if (dict) {
-      return dict[name];
-    }
-  }
-
-  return nil;
-}
-
-- (int)value:(NSString*)name {
-  NSNumber* number = [self valueAsNumber:name];
-  if (number) {
-    return [number intValue];
-  } else {
-    return [self defaultValue:name];
-  }
-}
-
-- (int)defaultValue:(NSString*)name {
-  NSNumber* number = self.defaults[name];
-  if (number) {
-    return [number intValue];
-  } else {
-    return 0;
-  }
-}
-
-- (void)setValue:(int)newval forName:(NSString*)name {
-  [self setValue:newval forName:name tellToKext:YES];
-}
-
-- (void)setValue:(int)newval forName:(NSString*)name tellToKext:(BOOL)tellToKext {
-  [self setValue:newval forName:name tellToKext:tellToKext notificationUserInfo:nil];
-}
-
-- (void)setValue:(int)newval forName:(NSString*)name tellToKext:(BOOL)tellToKext notificationUserInfo:(NSDictionary*)notificationUserInfo {
-  @synchronized(self) {
-    int oldval = [self value:name];
-
-    NSString* identifier = [self configlist_selectedIdentifier];
-    if (!identifier) {
-      NSLog(@"[ERROR] %s identifier == nil", __FUNCTION__);
-      return;
-    }
-
-    NSMutableDictionary* md = nil;
-
-    NSDictionary* dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:identifier];
-    if (dict) {
-      md = [NSMutableDictionary dictionaryWithDictionary:dict];
-    } else {
-      md = [NSMutableDictionary new];
-    }
-    if (!md) {
-      NSLog(@"[ERROR] %s md == nil", __FUNCTION__);
-      return;
-    }
-
-    int defaultvalue = 0;
-    NSNumber* defaultnumber = self.defaults[name];
-    if (defaultnumber) {
-      defaultvalue = [defaultnumber intValue];
-    }
-
-    if (newval == defaultvalue) {
-      [md removeObjectForKey:name];
-    } else {
-      md[name] = @(newval);
-    }
-
-    [[NSUserDefaults standardUserDefaults] setObject:md forKey:identifier];
-    // [[NSUserDefaults standardUserDefaults] synchronize];
-
-    if (oldval != newval) {
-      // We post notification only when newval is different from oldval in order to avoid infinity loop.
-      // (`setValue` might be called in observer.)
-      [self postPreferencesChangedNotifications:[NSProcessInfo processInfo].processIdentifier];
-      if (tellToKext) {
-        [self callSetConfigOne:name value:newval];
-      }
-    }
-  }
-}
-
-- (void)callSetConfigOne:(NSString*)name value:(int)value {
+- (void)updateKextValue:(NSString*)name {
   struct BridgeSetConfigOne bridgeSetConfigOne;
 
-  for (NSUInteger i = 0, count = [self.essential_configuration_identifiers count]; i < count; ++i) {
-    if ([self.essential_configuration_identifiers[i] isEqualToString:name]) {
-      bridgeSetConfigOne.isEssentialConfig = 1;
-      bridgeSetConfigOne.index = (uint32_t)(i);
-      bridgeSetConfigOne.value = (int32_t)(value);
-      [self.clientForKernelspace set_config_one:&bridgeSetConfigOne];
-      return;
-    }
+  NSInteger essentialConfigurationIndex = [self.preferencesModel essentialConfigurationIndex:name];
+  if (essentialConfigurationIndex >= 0) {
+    bridgeSetConfigOne.isEssentialConfig = 1;
+    bridgeSetConfigOne.index = (uint32_t)(essentialConfigurationIndex);
+    bridgeSetConfigOne.value = (int32_t)([self.preferencesModel value:name]);
+    [self.clientForKernelspace set_config_one:&bridgeSetConfigOne];
+    return;
   }
 
   int config_index = [self.xmlCompiler config_index:name];
   if (config_index >= 0) {
     bridgeSetConfigOne.isEssentialConfig = 0;
     bridgeSetConfigOne.index = (uint32_t)(config_index);
-    bridgeSetConfigOne.value = (int32_t)(value);
+    bridgeSetConfigOne.value = (int32_t)([self.preferencesModel value:name]);
     [self.clientForKernelspace set_config_one:&bridgeSetConfigOne];
     return;
   }
-}
-
-- (void)clearNotSave {
-  @synchronized(self) {
-    // user setting
-    NSString* identifier = [self configlist_selectedIdentifier];
-    if (identifier) {
-      NSDictionary* dict = [[NSUserDefaults standardUserDefaults] dictionaryForKey:identifier];
-      if (dict) {
-        NSMutableDictionary* md = [NSMutableDictionary dictionaryWithDictionary:dict];
-
-        BOOL changed = NO;
-
-        for (NSString* key in dict) {
-          if ([key hasPrefix:@"notsave."]) {
-            [md removeObjectForKey:key];
-            changed = YES;
-          }
-        }
-
-        [[NSUserDefaults standardUserDefaults] setObject:md forKey:identifier];
-
-        if (changed) {
-          [self postPreferencesChangedNotifications:[NSProcessInfo processInfo].processIdentifier];
-          [self.clientForKernelspace send_config_to_kext];
-        }
-      }
-    }
-  }
-}
-
-- (NSArray*)essential_config {
-  NSMutableArray* a = [NSMutableArray new];
-
-  if (self.essential_configuration_identifiers) {
-    for (NSString* identifier in self.essential_configuration_identifiers) {
-      [a addObject:@([self value:identifier])];
-    }
-  }
-
-  return a;
-}
-
-- (NSDictionary*)changed {
-  NSString* identifier = [self configlist_selectedIdentifier];
-  if (!identifier) return nil;
-
-  return [[NSUserDefaults standardUserDefaults] dictionaryForKey:identifier];
-}
-
-// ----------------------------------------------------------------------
-- (NSInteger)configlist_selectedIndex {
-  return [[NSUserDefaults standardUserDefaults] integerForKey:@"selectedIndex"];
-}
-
-- (NSString*)configlist_selectedIdentifier {
-  return [self configlist_identifier:[self configlist_selectedIndex]];
-}
-
-- (NSArray*)configlist_getConfigList {
-  return [[NSUserDefaults standardUserDefaults] arrayForKey:@"configList"];
-}
-
-- (NSDictionary*)configlist_dictionary:(NSInteger)rowIndex {
-  NSArray* list = [self configlist_getConfigList];
-  if (!list) return nil;
-
-  if (rowIndex < 0 || (NSUInteger)(rowIndex) >= [list count]) return nil;
-
-  return list[rowIndex];
-}
-
-- (NSString*)configlist_identifier:(NSInteger)rowIndex {
-  NSDictionary* dict = [self configlist_dictionary:rowIndex];
-  if (!dict) return nil;
-  return dict[@"identify"];
-}
-
-- (void)configlist_clear_all_values:(NSInteger)rowIndex {
-  NSString* identifier = self.preferencesModel.currentProfileIdentifier;
-  if (!identifier) return;
-
-  [[NSUserDefaults standardUserDefaults] setObject:[NSMutableDictionary new] forKey:identifier];
-
-  [self postPreferencesChangedNotifications:[NSProcessInfo processInfo].processIdentifier];
-  [self.clientForKernelspace send_config_to_kext];
 }
 
 @end
