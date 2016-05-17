e7bfae3b93ce1c2aad1003e6a6e59c980d4fdbcd
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-17 23:16:59 +0900

    use catch

--- Tests/lib/file_path/test.cpp
+++ Tests/lib/file_path/test.cpp
@@ -1,127 +1,124 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include "../../include/catch.hpp"
+
 #include <ostream>
 
 #include "pqrs/file_path.hpp"
 
-TEST(pqrs_file_path, dirname) {
-  EXPECT_EQ("/usr/bin", pqrs::file_path::dirname("/usr/bin/ls"));
-  EXPECT_EQ("/usr/bin", pqrs::file_path::dirname("/usr/bin/ls/"));
-  EXPECT_EQ("/usr/bin", pqrs::file_path::dirname("/usr/bin/l"));
-  EXPECT_EQ("/usr/bin", pqrs::file_path::dirname("/usr/bin/l/"));
-  EXPECT_EQ("/", pqrs::file_path::dirname("/usr"));
-  EXPECT_EQ("/", pqrs::file_path::dirname("/"));
-  EXPECT_EQ("usr/bin", pqrs::file_path::dirname("usr/bin/ls"));
-  EXPECT_EQ("usr", pqrs::file_path::dirname("usr/bin/"));
-  EXPECT_EQ(".", pqrs::file_path::dirname("usr"));
-  EXPECT_EQ(".", pqrs::file_path::dirname("usr/"));
-  EXPECT_EQ(".", pqrs::file_path::dirname(""));
+TEST_CASE("dirname", "[pqrs_file_path]") {
+  REQUIRE(pqrs::file_path::dirname("/usr/bin/ls") == "/usr/bin");
+  REQUIRE(pqrs::file_path::dirname("/usr/bin/ls/") == "/usr/bin");
+  REQUIRE(pqrs::file_path::dirname("/usr/bin/l") == "/usr/bin");
+  REQUIRE(pqrs::file_path::dirname("/usr/bin/l/") == "/usr/bin");
+  REQUIRE(pqrs::file_path::dirname("/usr") == "/");
+  REQUIRE(pqrs::file_path::dirname("/") == "/");
+  REQUIRE(pqrs::file_path::dirname("usr/bin/ls") == "usr/bin");
+  REQUIRE(pqrs::file_path::dirname("usr/bin/") == "usr");
+  REQUIRE(pqrs::file_path::dirname("usr") == ".");
+  REQUIRE(pqrs::file_path::dirname("usr/") == ".");
+  REQUIRE(pqrs::file_path::dirname("") == ".");
 }
 
-TEST(pqrs_file_path, normalize) {
+TEST_CASE("normalize", "[pqrs_file_path]") {
   std::string file_path;
 
   file_path = "";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ(".", file_path);
+  REQUIRE(file_path == ".");
 
   file_path = ".";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ(".", file_path);
+  REQUIRE(file_path == ".");
 
   file_path = "./";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ(".", file_path);
+  REQUIRE(file_path == ".");
 
   file_path = "..";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("..", file_path);
+  REQUIRE(file_path == "..");
 
   file_path = "../";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../", file_path);
+  REQUIRE(file_path == "../");
 
   file_path = "..//foo";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../foo", file_path);
+  REQUIRE(file_path == "../foo");
 
   file_path = "abcde";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("abcde", file_path);
+  REQUIRE(file_path == "abcde");
 
   file_path = "abcde/";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("abcde/", file_path);
+  REQUIRE(file_path == "abcde/");
 
   file_path = "/foo//bar/../baz";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("/foo/baz", file_path);
+  REQUIRE(file_path == "/foo/baz");
 
   file_path = "/../foo//bar/../baz";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("/foo/baz", file_path);
+  REQUIRE(file_path == "/foo/baz");
 
   file_path = "foo/../bar";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("bar", file_path);
+  REQUIRE(file_path == "bar");
 
   file_path = "foo/.../bar";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("foo/.../bar", file_path);
+  REQUIRE(file_path == "foo/.../bar");
 
   file_path = "a/../b/../c/d";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("c/d", file_path);
+  REQUIRE(file_path == "c/d");
 
   file_path = "a/./b/./c/d";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("a/b/c/d", file_path);
+  REQUIRE(file_path == "a/b/c/d");
 
   file_path = "foo/bar/..";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("foo", file_path);
+  REQUIRE(file_path == "foo");
 
   file_path = "foo/bar/../";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("foo/", file_path);
+  REQUIRE(file_path == "foo/");
 
   file_path = "foo/bar/.";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("foo/bar", file_path);
+  REQUIRE(file_path == "foo/bar");
 
   file_path = "foo/bar/./";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("foo/bar/", file_path);
+  REQUIRE(file_path == "foo/bar/");
 
   file_path = "../foo/bar";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../foo/bar", file_path);
+  REQUIRE(file_path == "../foo/bar");
 
   file_path = "../../../foo/bar";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../../../foo/bar", file_path);
+  REQUIRE(file_path == "../../../foo/bar");
 
   file_path = "./foo/bar";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("foo/bar", file_path);
+  REQUIRE(file_path == "foo/bar");
 
   file_path = "../foo/bar/..";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../foo", file_path);
+  REQUIRE(file_path == "../foo");
 
   file_path = "../foo/bar///...";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../foo/bar/...", file_path);
+  REQUIRE(file_path == "../foo/bar/...");
 
   file_path = "../a/b/../c/../d///..";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../a", file_path);
+  REQUIRE(file_path == "../a");
 
   file_path = "a/../..////../b/c";
   pqrs::file_path::normalize(file_path);
-  EXPECT_EQ("../../b/c", file_path);
-}
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  REQUIRE(file_path == "../../b/c");
 }
