99359582ef2fbbac6d77c1f81f7609a233bd285f
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-17 23:07:29 +0900

    use catch

--- Tests/kext/List/test.cpp
+++ Tests/kext/List/test.cpp
@@ -1,4 +1,6 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include "../../include/catch.hpp"
+
 #include <ostream>
 
 #include "List.hpp"
@@ -20,131 +22,131 @@ private:
   int value_;
 };
 
-TEST(List, push_back) {
+TEST_CASE("push_back", "[List]") {
   List list;
   TestItem* p = nullptr;
   const int MAXITEM = 10;
 
   // ------------------------------------------------------------
   list.push_back(nullptr);
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
+  REQUIRE(list.size() == static_cast<size_t>(0));
 
   // ------------------------------------------------------------
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
+  REQUIRE(list.size() == static_cast<size_t>(0));
   for (int i = 0; i < MAXITEM; ++i) {
     list.push_back(new TestItem(i));
-    EXPECT_EQ(static_cast<size_t>(i + 1), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(i + 1));
   }
 
   for (int i = 0; i < MAXITEM; ++i) {
     p = dynamic_cast<TestItem*>(list.safe_front());
-    EXPECT_TRUE(p != nullptr);
-    EXPECT_EQ(i, p->v());
+    REQUIRE(p != nullptr);
+    REQUIRE(p->v() == i);
 
     list.pop_front();
-    EXPECT_EQ(static_cast<size_t>(MAXITEM - (i + 1)), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(MAXITEM - (i + 1)));
   }
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
-  EXPECT_EQ(0, allocatecount);
+  REQUIRE(list.size() == static_cast<size_t>(0));
+  REQUIRE(allocatecount == 0);
 
   // ------------------------------------------------------------
   // check prev/next value.
   TestItem* item1 = new TestItem(1);
   TestItem* item2 = new TestItem(2);
-  EXPECT_TRUE(item1->getprev() == nullptr);
-  EXPECT_TRUE(item1->getnext() == nullptr);
-  EXPECT_TRUE(item2->getprev() == nullptr);
-  EXPECT_TRUE(item2->getnext() == nullptr);
+  REQUIRE(item1->getprev() == nullptr);
+  REQUIRE(item1->getnext() == nullptr);
+  REQUIRE(item2->getprev() == nullptr);
+  REQUIRE(item2->getnext() == nullptr);
 
   list.push_back(item1);
-  EXPECT_TRUE(item1->getprev() == nullptr);
-  EXPECT_TRUE(item1->getnext() == nullptr);
+  REQUIRE(item1->getprev() == nullptr);
+  REQUIRE(item1->getnext() == nullptr);
   list.push_back(item2);
-  EXPECT_TRUE(item1->getprev() == nullptr);
-  EXPECT_TRUE(item1->getnext() == item2);
-  EXPECT_TRUE(item2->getprev() == item1);
-  EXPECT_TRUE(item2->getnext() == nullptr);
+  REQUIRE(item1->getprev() == nullptr);
+  REQUIRE(item1->getnext() == item2);
+  REQUIRE(item2->getprev() == item1);
+  REQUIRE(item2->getnext() == nullptr);
 
   list.pop_front();
-  EXPECT_TRUE(item2->getprev() == nullptr);
-  EXPECT_TRUE(item2->getnext() == nullptr);
+  REQUIRE(item2->getprev() == nullptr);
+  REQUIRE(item2->getnext() == nullptr);
 
   list.pop_front();
-  EXPECT_EQ(0, allocatecount);
+  REQUIRE(allocatecount == 0);
 }
 
-TEST(List, push_front) {
+TEST_CASE("push_front", "[List]") {
   List list;
   TestItem* p = nullptr;
   const int MAXITEM = 10;
 
   // ------------------------------------------------------------
   list.push_front(nullptr);
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
+  REQUIRE(list.size() == static_cast<size_t>(0));
 
   // ------------------------------------------------------------
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
+  REQUIRE(list.size() == static_cast<size_t>(0));
   for (int i = 0; i < MAXITEM; ++i) {
     list.push_front(new TestItem(i));
-    EXPECT_EQ(static_cast<size_t>(i + 1), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(i + 1));
   }
 
   for (int i = 0; i < MAXITEM; ++i) {
     p = dynamic_cast<TestItem*>(list.safe_front());
-    EXPECT_TRUE(p != nullptr);
-    EXPECT_EQ(MAXITEM - (i + 1), p->v());
+    REQUIRE(p != nullptr);
+    REQUIRE(p->v() == MAXITEM - (i + 1));
 
     list.pop_front();
-    EXPECT_EQ(static_cast<size_t>(MAXITEM - (i + 1)), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(MAXITEM - (i + 1)));
   }
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
-  EXPECT_EQ(0, allocatecount);
+  REQUIRE(list.size() == static_cast<size_t>(0));
+  REQUIRE(allocatecount == 0);
 
   // ------------------------------------------------------------
   // check prev/next value.
   TestItem* item1 = new TestItem(1);
   TestItem* item2 = new TestItem(2);
-  EXPECT_TRUE(item1->getprev() == nullptr);
-  EXPECT_TRUE(item1->getnext() == nullptr);
-  EXPECT_TRUE(item2->getprev() == nullptr);
-  EXPECT_TRUE(item2->getnext() == nullptr);
+  REQUIRE(item1->getprev() == nullptr);
+  REQUIRE(item1->getnext() == nullptr);
+  REQUIRE(item2->getprev() == nullptr);
+  REQUIRE(item2->getnext() == nullptr);
 
   list.push_front(item1);
-  EXPECT_TRUE(item1->getprev() == nullptr);
-  EXPECT_TRUE(item1->getnext() == nullptr);
+  REQUIRE(item1->getprev() == nullptr);
+  REQUIRE(item1->getnext() == nullptr);
   list.push_front(item2);
-  EXPECT_TRUE(item1->getprev() == item2);
-  EXPECT_TRUE(item1->getnext() == nullptr);
-  EXPECT_TRUE(item2->getprev() == nullptr);
-  EXPECT_TRUE(item2->getnext() == item1);
+  REQUIRE(item1->getprev() == item2);
+  REQUIRE(item1->getnext() == nullptr);
+  REQUIRE(item2->getprev() == nullptr);
+  REQUIRE(item2->getnext() == item1);
 
   list.pop_front();
-  EXPECT_TRUE(item1->getprev() == nullptr);
-  EXPECT_TRUE(item1->getnext() == nullptr);
+  REQUIRE(item1->getprev() == nullptr);
+  REQUIRE(item1->getnext() == nullptr);
 
   list.pop_front();
-  EXPECT_EQ(0, allocatecount);
+  REQUIRE(allocatecount == 0);
 }
 
-TEST(List, safe_front) {
+TEST_CASE("safe_front", "[List]") {
   List list;
   list.safe_front();
   list.safe_front();
   list.safe_front();
 }
-TEST(List, safe_back) {
+TEST_CASE("safe_back", "[List]") {
   List list;
   list.safe_back();
   list.safe_back();
   list.safe_back();
 }
-TEST(List, pop_front) {
+TEST_CASE("pop_front", "[List]") {
   List list;
   list.pop_front();
   list.pop_front();
   list.pop_front();
 }
-TEST(List, pop_back) {
+TEST_CASE("pop_back", "[List]") {
   List list;
   list.pop_back();
   list.pop_back();
@@ -153,13 +155,13 @@ TEST(List, pop_back) {
   list.push_back(new TestItem(1));
   list.push_back(new TestItem(2));
   list.pop_back();
-  EXPECT_EQ(static_cast<size_t>(1), list.size());
+  REQUIRE(list.size() == static_cast<size_t>(1));
 
   TestItem* p = static_cast<TestItem*>(list.safe_back());
-  EXPECT_EQ(static_cast<size_t>(1), p->v());
+  REQUIRE(p->v() == static_cast<size_t>(1));
 }
 
-TEST(List, insert) {
+TEST_CASE("insert", "[List]") {
   List list;
   TestItem* p = nullptr;
 
@@ -169,55 +171,55 @@ TEST(List, insert) {
   p = static_cast<TestItem*>(list.insert(nullptr, new TestItem(1))); // [1]
   p = static_cast<TestItem*>(list.insert(nullptr, new TestItem(2))); // [2,1]
 
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_front())->v());
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_front()->getnext())->v());
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_back())->v());
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_back()->getprev())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 2);
+  REQUIRE(static_cast<TestItem*>(list.safe_front()->getnext())->v() == 1);
+  REQUIRE(static_cast<TestItem*>(list.safe_back())->v() == 1);
+  REQUIRE(static_cast<TestItem*>(list.safe_back()->getprev())->v() == 2);
 
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 2);
   list.pop_front();
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 1);
   list.pop_front();
 
   p = static_cast<TestItem*>(list.insert(nullptr, new TestItem(1))); // [1]
   p = static_cast<TestItem*>(list.insert(p, new TestItem(2)));       // [2,1]
 
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_front())->v());
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_front()->getnext())->v());
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_back())->v());
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_back()->getprev())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 2);
+  REQUIRE(static_cast<TestItem*>(list.safe_front()->getnext())->v() == 1);
+  REQUIRE(static_cast<TestItem*>(list.safe_back())->v() == 1);
+  REQUIRE(static_cast<TestItem*>(list.safe_back()->getprev())->v() == 2);
 
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 2);
   list.pop_front();
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 1);
   list.pop_front();
 
   p = static_cast<TestItem*>(list.insert(nullptr, new TestItem(1)));          // [1]
   p = static_cast<TestItem*>(list.insert(p, new TestItem(2)));                // [2,1]
   p = static_cast<TestItem*>(list.insert(list.safe_back(), new TestItem(3))); // [2,3,1]
 
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_front())->v());
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_back())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 2);
+  REQUIRE(static_cast<TestItem*>(list.safe_back())->v() == 1);
 
-  EXPECT_EQ(2, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 2);
   list.pop_front();
-  EXPECT_EQ(3, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 3);
   list.pop_front();
-  EXPECT_EQ(1, static_cast<TestItem*>(list.safe_front())->v());
+  REQUIRE(static_cast<TestItem*>(list.safe_front())->v() == 1);
   list.pop_front();
 }
 
-TEST(List, erase_and_delete) {
+TEST_CASE("erase_and_delete", "[List]") {
   const int MAXITEM = 10;
 
   // ------------------------------------------------------------
   for (int erase_index = 0; erase_index < MAXITEM; ++erase_index) {
     List list;
 
-    EXPECT_EQ(static_cast<size_t>(0), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(0));
     for (int i = 0; i < MAXITEM; ++i) {
       list.push_back(new TestItem(i));
-      EXPECT_EQ(static_cast<size_t>(i + 1), list.size());
+      REQUIRE(list.size() == static_cast<size_t>(i + 1));
     }
 
     TestItem* p = static_cast<TestItem*>(list.safe_front());
@@ -225,35 +227,30 @@ TEST(List, erase_and_delete) {
       p = static_cast<TestItem*>(p->getnext());
     }
     TestItem* next = static_cast<TestItem*>(p->getnext());
-    EXPECT_EQ(next, list.erase_and_delete(p));
+    REQUIRE(list.erase_and_delete(p) == next);
 
     // check
     p = static_cast<TestItem*>(list.safe_front());
     for (int i = 0; i < MAXITEM; ++i) {
       if (i == erase_index) continue;
-      EXPECT_EQ(i, p->v());
+      REQUIRE(p->v() == i);
       p = static_cast<TestItem*>(p->getnext());
     }
-    EXPECT_EQ(static_cast<size_t>(MAXITEM - 1), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(MAXITEM - 1));
   }
 }
 
-TEST(List, clear) {
+TEST_CASE("clear", "[List]") {
   List list;
   const int MAXITEM = 10;
 
   // ------------------------------------------------------------
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
+  REQUIRE(list.size() == static_cast<size_t>(0));
   for (int i = 0; i < MAXITEM; ++i) {
     list.push_back(new TestItem(i));
-    EXPECT_EQ(static_cast<size_t>(i + 1), list.size());
+    REQUIRE(list.size() == static_cast<size_t>(i + 1));
   }
   list.clear();
-  EXPECT_EQ(static_cast<size_t>(0), list.size());
-  EXPECT_EQ(0, allocatecount);
-}
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  REQUIRE(list.size() == static_cast<size_t>(0));
+  REQUIRE(allocatecount == 0);
 }
