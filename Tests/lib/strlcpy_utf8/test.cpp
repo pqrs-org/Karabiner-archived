f5cdbb162a500e94e6aefed233c3570d9e9bb769
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-17 23:19:24 +0900

    use catch

--- Tests/lib/strlcpy_utf8/test.cpp
+++ Tests/lib/strlcpy_utf8/test.cpp
@@ -1,29 +1,30 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include "../../include/catch.hpp"
 
 #include "strlcpy_utf8.hpp"
 
-TEST(strlcpy_utf8, ascii) {
+TEST_CASE("ascii", "[strlcpy_utf8]") {
   char string[] = "abc";
   char buf[512];
   pqrs::strlcpy_utf8::strlcpy(buf, string, sizeof(buf));
 
-  EXPECT_EQ(buf[0], 'a');
-  EXPECT_EQ(buf[1], 'b');
-  EXPECT_EQ(buf[2], 'c');
-  EXPECT_EQ(buf[3], '\0');
+  REQUIRE(buf[0] == 'a');
+  REQUIRE(buf[1] == 'b');
+  REQUIRE(buf[2] == 'c');
+  REQUIRE(buf[3] == '\0');
 }
 
-TEST(strlcpy_utf8, ascii_shortbuf) {
+TEST_CASE("ascii_shortbuf", "[strlcpy_utf8]") {
   char string[] = "abc";
   char buf[3];
   pqrs::strlcpy_utf8::strlcpy(buf, string, sizeof(buf));
 
-  EXPECT_EQ(buf[0], 'a');
-  EXPECT_EQ(buf[1], 'b');
-  EXPECT_EQ(buf[2], '\0');
+  REQUIRE(buf[0] == 'a');
+  REQUIRE(buf[1] == 'b');
+  REQUIRE(buf[2] == '\0');
 }
 
-TEST(strlcpy_utf8, multibyte) {
+TEST_CASE("multibyte", "[strlcpy_utf8]") {
   uint8_t string[] = {
       0xd5, 0xa3,            // 2byte
       0xe3, 0x81, 0x93,      // 3byte
@@ -37,10 +38,10 @@ TEST(strlcpy_utf8, multibyte) {
   char buf[512];
   pqrs::strlcpy_utf8::strlcpy(buf, reinterpret_cast<char*>(string), sizeof(buf));
 
-  EXPECT_EQ(strcmp(reinterpret_cast<char*>(string), buf), 0);
+  REQUIRE(strcmp(reinterpret_cast<char*>(string), buf) == 0);
 }
 
-TEST(strlcpy_utf8, multibyte_short) {
+TEST_CASE("multibyte_short", "[strlcpy_utf8]") {
   uint8_t string[] = {
       0xd5, 0xa3,            // 2byte
       0xe3, 0x81, 0x93,      // 3byte
@@ -54,15 +55,15 @@ TEST(strlcpy_utf8, multibyte_short) {
   char buf[8];
   pqrs::strlcpy_utf8::strlcpy(buf, reinterpret_cast<char*>(string), sizeof(buf));
 
-  EXPECT_EQ(buf[0], static_cast<char>(0xd5));
-  EXPECT_EQ(buf[1], static_cast<char>(0xa3));
-  EXPECT_EQ(buf[2], static_cast<char>(0xe3));
-  EXPECT_EQ(buf[3], static_cast<char>(0x81));
-  EXPECT_EQ(buf[4], static_cast<char>(0x93));
-  EXPECT_EQ(buf[5], '\0');
+  REQUIRE(buf[0] == static_cast<char>(0xd5));
+  REQUIRE(buf[1] == static_cast<char>(0xa3));
+  REQUIRE(buf[2] == static_cast<char>(0xe3));
+  REQUIRE(buf[3] == static_cast<char>(0x81));
+  REQUIRE(buf[4] == static_cast<char>(0x93));
+  REQUIRE(buf[5] == '\0');
 }
 
-TEST(strlcpy_utf8, invalid_char) {
+TEST_CASE("invalid_char", "[strlcpy_utf8]") {
   uint8_t string[] = {
       0x80, // 0x10000000
   };
@@ -70,10 +71,5 @@ TEST(strlcpy_utf8, invalid_char) {
   char buf[512];
   pqrs::strlcpy_utf8::strlcpy(buf, reinterpret_cast<char*>(string), sizeof(buf));
 
-  EXPECT_EQ(strcmp("", buf), 0);
-}
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  REQUIRE(strcmp("", buf) == 0);
 }
