03c2ded721a1b5c5610aae7fa7ae53e3374471b6
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-17 22:37:33 +0900

    use catch

--- Tests/kext/ButtonStatus/test.cpp
+++ Tests/kext/ButtonStatus/test.cpp
@@ -1,4 +1,6 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN
+#include "../../include/catch.hpp"
+
 #include <ostream>
 
 #include "ButtonStatus.hpp"
@@ -17,58 +19,53 @@ std::ostream& operator<<(std::ostream& os, const ConsumerKeyCode& v) { return os
 std::ostream& operator<<(std::ostream& os, const PointingButton& v) { return os << v.get(); }
 std::ostream& operator<<(std::ostream& os, const Buttons& v) { return os << v.get(); }
 
-TEST(ButtonStatus, all) {
-  ASSERT_TRUE(ButtonStatus::initialize());
+TEST_CASE("all", "[Buttons]") {
+  REQUIRE(ButtonStatus::initialize() == true);
 
-  EXPECT_EQ(Buttons(), ButtonStatus::makeButtons());
+  REQUIRE(Buttons() == ButtonStatus::makeButtons());
 
   // ------------------------------------------------------------
   // set
   {
     ButtonStatus::set(PointingButton(1 << 3), true);
-    EXPECT_EQ(Buttons((1 << 3)), ButtonStatus::makeButtons());
+    REQUIRE(Buttons((1 << 3)) == ButtonStatus::makeButtons());
 
     ButtonStatus::set(PointingButton(1 << 11), false);
-    EXPECT_EQ(Buttons((1 << 3)), ButtonStatus::makeButtons());
+    REQUIRE(Buttons((1 << 3)) == ButtonStatus::makeButtons());
 
     ButtonStatus::set(PointingButton(1 << 11), true);
-    EXPECT_EQ(Buttons((1 << 3)), ButtonStatus::makeButtons());
+    REQUIRE(Buttons((1 << 3)) == ButtonStatus::makeButtons());
 
     ButtonStatus::set(PointingButton(1 << 11), true);
-    EXPECT_EQ(Buttons((1 << 3) | (1 << 11)), ButtonStatus::makeButtons());
+    REQUIRE(Buttons((1 << 3) | (1 << 11)) == ButtonStatus::makeButtons());
 
     ButtonStatus::set(PointingButton(1 << 3), false);
-    EXPECT_EQ(Buttons((1 << 11)), ButtonStatus::makeButtons());
+    REQUIRE(Buttons((1 << 11)) == ButtonStatus::makeButtons());
 
     ButtonStatus::set(PointingButton(1 << 1), true);
-    EXPECT_EQ(Buttons((1 << 1) | (1 << 11)), ButtonStatus::makeButtons());
+    REQUIRE(Buttons((1 << 1) | (1 << 11)) == ButtonStatus::makeButtons());
   }
 
   // ------------------------------------------------------------
   // increase & decrease
   ButtonStatus::increase(Buttons(1 << 9));
-  EXPECT_EQ(Buttons((1 << 1) | (1 << 9) | (1 << 11)), ButtonStatus::makeButtons());
+  REQUIRE(Buttons((1 << 1) | (1 << 9) | (1 << 11)) == ButtonStatus::makeButtons());
 
   ButtonStatus::decrease(Buttons((1 << 4) | (1 << 8)));
-  EXPECT_EQ(Buttons((1 << 1) | (1 << 9) | (1 << 11)), ButtonStatus::makeButtons());
+  REQUIRE(Buttons((1 << 1) | (1 << 9) | (1 << 11)) == ButtonStatus::makeButtons());
 
   // ------------------------------------------------------------
   // reset
   ButtonStatus::reset();
-  EXPECT_EQ(Buttons(), ButtonStatus::makeButtons());
+  REQUIRE(Buttons() == ButtonStatus::makeButtons());
 
   // ------------------------------------------------------------
   // lock
   ButtonStatus::lock_increase(Buttons((1 << 1) | (1 << 3)));
   ButtonStatus::increase(Buttons(1 << 5));
-  EXPECT_EQ(Buttons((1 << 1) | (1 << 3) | (1 << 5)), ButtonStatus::makeButtons());
+  REQUIRE(Buttons((1 << 1) | (1 << 3) | (1 << 5)) == ButtonStatus::makeButtons());
   ButtonStatus::reset();
-  EXPECT_EQ(Buttons((1 << 1) | (1 << 3)), ButtonStatus::makeButtons());
+  REQUIRE(Buttons((1 << 1) | (1 << 3)) == ButtonStatus::makeButtons());
   ButtonStatus::lock_clear();
-  EXPECT_EQ(Buttons(), ButtonStatus::makeButtons());
-}
-
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  REQUIRE(Buttons() == ButtonStatus::makeButtons());
 }
