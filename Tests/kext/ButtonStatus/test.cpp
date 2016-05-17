39bcb9f17bd0c2b68e4fca85328aa31a3aa2ae83
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-17 22:46:19 +0900

    update tests

--- Tests/kext/ButtonStatus/test.cpp
+++ Tests/kext/ButtonStatus/test.cpp
@@ -13,50 +13,50 @@ Config config;
 TEST_CASE("all", "[Buttons]") {
   REQUIRE(ButtonStatus::initialize() == true);
 
-  REQUIRE(Buttons() == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons());
 
   // ------------------------------------------------------------
   // set
   {
     ButtonStatus::set(PointingButton(1 << 3), true);
-    REQUIRE(Buttons((1 << 3)) == ButtonStatus::makeButtons());
+    REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 3)));
 
     ButtonStatus::set(PointingButton(1 << 11), false);
-    REQUIRE(Buttons((1 << 3)) == ButtonStatus::makeButtons());
+    REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 3)));
 
     ButtonStatus::set(PointingButton(1 << 11), true);
-    REQUIRE(Buttons((1 << 3)) == ButtonStatus::makeButtons());
+    REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 3)));
 
     ButtonStatus::set(PointingButton(1 << 11), true);
-    REQUIRE(Buttons((1 << 3) | (1 << 11)) == ButtonStatus::makeButtons());
+    REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 3) | (1 << 11)));
 
     ButtonStatus::set(PointingButton(1 << 3), false);
-    REQUIRE(Buttons((1 << 11)) == ButtonStatus::makeButtons());
+    REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 11)));
 
     ButtonStatus::set(PointingButton(1 << 1), true);
-    REQUIRE(Buttons((1 << 1) | (1 << 11)) == ButtonStatus::makeButtons());
+    REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 1) | (1 << 11)));
   }
 
   // ------------------------------------------------------------
   // increase & decrease
   ButtonStatus::increase(Buttons(1 << 9));
-  REQUIRE(Buttons((1 << 1) | (1 << 9) | (1 << 11)) == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 1) | (1 << 9) | (1 << 11)));
 
   ButtonStatus::decrease(Buttons((1 << 4) | (1 << 8)));
-  REQUIRE(Buttons((1 << 1) | (1 << 9) | (1 << 11)) == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 1) | (1 << 9) | (1 << 11)));
 
   // ------------------------------------------------------------
   // reset
   ButtonStatus::reset();
-  REQUIRE(Buttons() == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons());
 
   // ------------------------------------------------------------
   // lock
   ButtonStatus::lock_increase(Buttons((1 << 1) | (1 << 3)));
   ButtonStatus::increase(Buttons(1 << 5));
-  REQUIRE(Buttons((1 << 1) | (1 << 3) | (1 << 5)) == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 1) | (1 << 3) | (1 << 5)));
   ButtonStatus::reset();
-  REQUIRE(Buttons((1 << 1) | (1 << 3)) == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons((1 << 1) | (1 << 3)));
   ButtonStatus::lock_clear();
-  REQUIRE(Buttons() == ButtonStatus::makeButtons());
+  REQUIRE(ButtonStatus::makeButtons() == Buttons());
 }
