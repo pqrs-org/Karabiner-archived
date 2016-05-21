e6425d0363fe74bdf087eef83dddfe537086b226
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-21 14:55:06 +0900

    add updater

---
+++ src/util/updater/Karabiner updater/AppDelegate.m
@@ -0,0 +1,15 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property(weak) IBOutlet NSWindow *window;
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+
+}
+
+@end
