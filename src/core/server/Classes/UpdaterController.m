f587fae7dfe728e0407947dd4be90a2eeeb334f4
Takayama Fumihiko <tekezo@pqrs.org>
2016-05-23 23:05:43 +0900

    revert terminateUpdaters

--- src/core/server/Classes/UpdaterController.m
+++ src/core/server/Classes/UpdaterController.m
@@ -15,7 +15,16 @@ static dispatch_queue_t queue_;
   return @"/Library/Application Support/org.pqrs/Karabiner/updater/Karabiner.app";
 }
 
++ (void)terminateUpdaters {
+  NSString* bundleIdentifier = [[NSBundle bundleWithPath:self.path] bundleIdentifier];
+  NSArray* applications = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleIdentifier];
+  for (NSRunningApplication* runningApplication in applications) {
+    [runningApplication terminate];
+  }
+}
+
 + (void)launch:(NSString*)argument {
+  [UpdaterController terminateUpdaters];
   [[NSWorkspace sharedWorkspace] launchApplicationAtURL:[NSURL fileURLWithPath:self.path]
                                                 options:0
                                           configuration:@{ NSWorkspaceLaunchConfigurationArguments : @[ argument ] }
